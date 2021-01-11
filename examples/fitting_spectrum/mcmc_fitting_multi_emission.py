8f369cb6eed537f0f5235daca31e3a8e39789e01
Hajime Kawahara <divrot@gmail.com>
2021-01-11 16:30:55 +0900

    sample added

---
+++ examples/fitting_spectrum/mcmc_fitting_multi_emission.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # MCMC Fitting the emission profile to an emission spectrum  
+# 
+# This example conducts HMC-NUTS fitting to a mock absorption line. We use Schwarzchild equation of the absorption model based on Tepper 
+# approximation of Voigt profile, lpf.FAbsVTc in exojax.lpf (line profile functions) module. 
+# 
+# HMC-NUTS: Hamiltonian Monte Carlo No-U-Turn Sample using numpyro
+
+# In[1]:
+
+
+# importing lpf modile in exojax.spec
+from exojax.spec import lpf
+from exojax.spec import rtransfer as rt
+from exojax.spec import planck
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import arviz
+
+import numpy as np
+import jax.numpy as jnp
+from jax import random
+from jax.lax import map, scan
+from jax import vmap, jit
+import numpyro.distributions as dist
+import numpyro
+from numpyro.infer import MCMC, NUTS
+from numpyro.infer import Predictive
+from numpyro.diagnostics import hpdi
+
+plt.style.use('bmh')
+#numpyro.set_platform("cpu")
+numpyro.set_platform("gpu")
+
+# generating mock absorption data
+np.random.seed(38)
+N=300
+nur=300
+nuarr=jnp.linspace(-nur,nur,N)
+sigin=0.01
+sDfix = jnp.array(1.0)
+gLfix = jnp.array(0.5)
+
+Nmol=100
+hatnufix = (np.random.rand(Nmol)-0.5)*nur*2
+Sfix=np.random.rand(Nmol)
+nu0fix = 0.7
+#f = lambda nu: lpf.MultiAbsVTc(nu-nu0fix,sDfix,gLfix,Afix,Sfix,hatnufix)
+
+numatrix=lpf.make_numatrix(nuarr,hatnufix,nu0fix)
+cs=rt.cross(numatrix,sDfix,gLfix,Sfix)
+
+NP=17
+Parr, k=rt.const_p_layer(NP=NP)
+Tarr = 1000.*(Parr/Parr[0])**-0.1
+
+fig=plt.figure(figsize=(10,3))
+ax=fig.add_subplot(121)
+ax.plot(Parr,".")
+ax.plot(Parr/(k+1),"+")
+plt.yscale("log")
+ax=fig.add_subplot(122)
+ax.plot(Tarr,Parr,".")
+#plt.xscale("log")
+plt.yscale("log")
+plt.gca().invert_yaxis()
+plt.savefig("temp.png")
+
+# In[7]:
+
+
+@jit
+def g(xs):
+    """
+    Params: 
+      xs: free parameters
+    """
+    Tarr=xs
+    numic=0.5
+    F0=jnp.ones(len(nuarr))*planck.nB(Tarr[0],numic)
+    #F0=0.0
+    init=[F0,Parr[0],0.7,1.0,0.5]
+    FP,null=scan(add_layer,init,Tarr,NP)
+    return FP[0]*3.e4
+
+
+jaxrt=rt.JaxRT()
+jaxrt.nuarr=nuarr
+jaxrt.hatnufix=hatnufix
+jaxrt.Sfix=Sfix
+jaxrt.Parr=Parr
+jaxrt.NP=NP
+jaxrt.k=k
+
+
+numic=0.5
+F0=jnp.ones(len(nuarr))*planck.nB(Tarr[0],numic)
+init=[F0,Parr[0],0.7,1.0,0.5]
+jaxrt.Tarr=Tarr
+
+
+F0=jnp.zeros(len(nuarr))
+init=[F0,Parr[0],0.7,1.0,0.5]
+FP,tauarr=scan(jaxrt.add_layer,init,Tarr.T,NP)
+
+fig=plt.figure()
+ax=fig.add_subplot(111)
+c=ax.imshow(tauarr)
+plt.colorbar(c,shrink=0.7)
+ax.set_aspect(0.7/ax.get_data_ratio())
+plt.gca().invert_yaxis()
+plt.savefig("temp2.png")
+
+from jax import grad
+F0=jnp.zeros(len(nuarr))
+#F0=0.0
+init=[F0,Parr[0],0.7,1.0,0.5]
+
+sigin=5.0
+data=g(Tarr)+np.random.normal(0,sigin,size=N)
+plt.plot(data,".")
+
+#probabilistic model using numpyro
+def model(nu,y):
+    #A = numpyro.sample('A', dist.Uniform(0.5,1.5))
+    sD = numpyro.sample('sD', dist.Exponential(1.))
+    gL = numpyro.sample('gL', dist.Exponential(1.))
+    sigma = numpyro.sample('sigma', dist.Exponential(1.))
+    nu0 = numpyro.sample('nu0', dist.Uniform(-5,5))
+    numic=0.5
+    nuarr=nu
+    F0=jnp.ones(len(nuarr))*planck.nB(Tarr[0],numic)
+    init=[F0,Parr[0],nu0,sD,gL]
+    jaxrt.Tarr=Tarr
+    mu=jaxrt.layerscan(init)
+    numpyro.sample('y', dist.Normal(mu, sigma), obs=y)
+
+
+# In[35]:
+
+
+rng_key = random.PRNGKey(0)
+rng_key, rng_key_ = random.split(rng_key)
+num_warmup, num_samples = 1000, 2000
+
+kernel = NUTS(model)
+mcmc = MCMC(kernel, num_warmup, num_samples)
+mcmc.run(rng_key_, nu=nuarr, y=data)
+mcmc.print_summary()
+
+
+# In[20]:
+
+
+import arviz
+arviz.plot_trace(mcmc, var_names=["gL", "sD","nu0","sigma"])
+plt.savefig("temp4.png")
+
+
+
+refs={}
+#refs["A"]=Afix
+refs["sD"]=sDfix
+refs["gL"]=gLfix
+refs["sigma"]=sigin
+refs["nu0"]=nu0fix
+arviz.plot_pair(arviz.from_numpyro(mcmc),kind='kde',divergences=False,                marginals=True,
+                reference_values=refs,
+               reference_values_kwargs={'color':"red", "marker":"o", "markersize":12}) 
+plt.savefig("temp5.png")
+
+# generating predictions
+# hpdi is "highest posterior density interval"
+posterior_sample = mcmc.get_samples()
+pred = Predictive(model,posterior_sample)
+nu_ = nuarr
+predictions = pred(rng_key_,nu=nu_,y=None)
+median_mu = jnp.median(predictions["y"],axis=0)
+hpdi_mu = hpdi(predictions["y"], 0.9)
+
+
+# In[23]:
+
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 3))
+ax.plot(nu_,median_mu,color="C0")
+ax.plot(nuarr,data,"+",color="C1",label="data")
+ax.fill_between(nu_, hpdi_mu[0], hpdi_mu[1], alpha=0.3, interpolate=True,color="C0",
+                label="90% area")
+plt.xlabel("$\\nu$")
+plt.legend()
+plt.savefig("temp6.png")
+
+print("SYSEND")
