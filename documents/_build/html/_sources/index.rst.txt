9ae96ae59a2ae970e2ef961c94f74c3ffef6d0fd
Hajime Kawahara <divrot@gmail.com>
2021-01-11 15:09:55 +0900

    dpcs

--- documents/_build/html/_sources/index.rst.txt
+++ documents/_build/html/_sources/index.rst.txt
@@ -10,7 +10,7 @@ Welcome to exojax's documentation!
    :maxdepth: 2
    :caption: Contents:
    	     
-   
+   exojax/modules
 
 Indices and tables
 ==================
--- documents/conf.py
+++ documents/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/exojax/'))
+sys.path.insert(0, os.path.abspath('/home/kawahara/exojax'))
 
 
 # -- Project information -----------------------------------------------------
@@ -46,8 +46,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
