0046da073286013819b8f12165a7f47017abd58f
Hajime Kawahara <divrot@gmail.com>
2021-01-15 12:25:18 +0900

    cumprod

--- examples/fitting_spectrum/cumprod.py
+++ examples/fitting_spectrum/cumprod.py
@@ -30,43 +30,35 @@ from numpyro.infer import MCMC, NUTS
 from numpyro.infer import Predictive
 from numpyro.diagnostics import hpdi
 
-NP=17
+#layers
+NP=30
 Parr, k=rt.const_p_layer(NP=NP)
+dParr = (1.0-k)*Parr
+Parr=Parr[::-1] ### define from upper layer
+dParr=dParr[::-1]
 Tarr = 1000.*(Parr/Parr[0])**-0.1
 
+#nu
 N=50
 nuarr=jnp.linspace(-10,10,N)
 
-def add_layer(carry,x):
-    """
-    Params:
-      carry: F[i], P[i]
-      x: free parameters, T
-      
-    Returns:
-      FP: F[i+1], P[i+1]=k*P[i]
-      dtaui: dtau of this layer
-    """
-    F,Pi,nu0,sigmaD,gammaL = carry
-    Ti = x
-    numic=0.5
-    gi = planck.nB(Ti,numic)
-    dtaui = 1.e-1*lpf.VoigtHjert(nuarr-nu0,sigmaD,gammaL)*(1.0-k)*Pi
-    Trans=(1.0-dtaui)*jnp.exp(-dtaui)
-    F = F*Trans + gi*(1.0-Trans)
-    carry=[F,k*Pi,nu0,sigmaD,gammaL]
-    return carry,dtaui
-
-def g(xs):
-    Tarr=xs
-    numic=0.5
-    F0=jnp.ones(len(nuarr))*planck.nB(Tarr[0],numic)
-    init=[F0,Parr[0],0.7,1.0,0.5]
-    FP,null=scan(add_layer,init,Tarr,NP)
-    return FP[0]
+#data
+numic=0.5
+gi = planck.nB(Tarr,numic)
+dParr = (1.0-k)*Parr
+nu0=0.7
+sigmaD=1.0
+gammaL=0.5
+xsv=1.e-1*lpf.VoigtHjert(nuarr-nu0,sigmaD,gammaL)
+dtauM=dParr[None,:]*xsv[:,None]
+TransM=(1.0-dtauM)*jnp.exp(-dtauM)
+#Q0=jnp.ones(len(nuarr))*planck.nB(Tarr[0],numic)
+Qv=(1-TransM)*gi
+F=(jnp.sum(Qv*jnp.cumprod(TransM,axis=1),axis=1))
+F=F*3.e7
 
 sigin=0.8
-data=g(Tarr)+np.random.normal(0,sigin,size=N)
+data=F+np.random.normal(0,sigin,size=N)
 
 #probabilistic model using numpyro
 def model(nu,y):
@@ -76,14 +68,20 @@ def model(nu,y):
     sigma = numpyro.sample('sigma', dist.Exponential(1.))
     nu0 = numpyro.sample('nu0', dist.Uniform(-5,5))
     alpha = numpyro.sample('alpha', dist.Uniform(-0.3,0.3)) #
+
+    #model
     Tarr = 1000.*(Parr/Parr[0])**alpha #
     numic=0.5
-    nuarr=nu
-    F0=jnp.ones(len(nuarr))*planck.nB(Tarr[0],numic)
-    init=[F0,Parr[0],nu0,sD,gL]
-    FP,null=scan(add_layer,init,Tarr,NP)
-    mu = FP[0]
+    gi = planck.nB(Tarr,numic)
+    xsv=1.e-1*lpf.VoigtHjert(nuarr-nu0,sD,gL)
+    dtauM=dParr[None,:]*xsv[:,None]
+    TransM=(1.0-dtauM)*jnp.exp(-dtauM)
+    Qv=(1-TransM)*gi
+    F=(jnp.sum(Qv*jnp.cumprod(TransM,axis=1),axis=1))
+    mu=F*3.e7
+    
     numpyro.sample('y', dist.Normal(mu, sigma), obs=y)
+
 
 rng_key = random.PRNGKey(0)
 rng_key, rng_key_ = random.split(rng_key)
