1d84a23c322b3740acef16130d3b25e044500319
Hajime Kawahara <divrot@gmail.com>
2021-01-19 21:24:04 +0900

    hitran

---
+++ src/exojax/spec/hitran.py
@@ -0,0 +1,73 @@
+from jax import jit, vmap
+import jax.numpy as jnp
+
+@jit
+def SijT(T,sij0,nu_ij,glower,elower,qT):
+    """Line strength as a function of temperature
+
+    Args:
+       T: temperature (K)
+       sij0: Sij(T=296K)
+       glower: g_lower or gpp
+       elower: elower 
+       qT: Q(Tref)/Q(T)
+
+    Returns:
+       Sij(T): Line strength
+
+    """
+    Tref=296.0 #reference tempearture (K)
+    c_2 = 1.4387773 #hc/k_B (cm K)
+    expow=sij0-c_2*(elower/T-elower/Tref)
+    fac=(1.0-jnp.exp(-c_2*nu_ij/T) )/(1.0-jnp.exp(-c_2*nu_ij/Tref))
+    return jnp.exp(expow)*qT*fac
+
+@jit
+def gamma_hitran(P, T, Pself, n_air, gamma_air_ref, gamma_self_ref):
+    """gamma factor by a pressure broadening
+    
+    Args:
+       P: pressure (atm)
+       T: temperature (K)
+
+    Returns:
+       gamma: pressure gamma factor (cm-1)
+
+    """
+    Tref=296.0 #reference tempearture (K)
+    gamma=(Tref/T)**n_air *(gamma_air_ref*(P-Pself) + gamma_self_ref*(Pself))
+    return gamma
+
+@jit
+def gamma_natural(A):
+    """gamma factor by natural broadning
+    
+    1/(4 pi c) = 2.6544188e-12 (cm-1 s)
+
+    Args:
+       A: Einstein A-factor (1/s)
+
+    Returns:
+       gamma_natural: natural width (cm-1)
+
+    """
+    return 2.6544188e-12*A
+
+@jit
+def doppler_sigma(nu,T,M):
+    """Dopper width (sigma)
+    
+    Note:
+       c3 is sqrt(kB/m_u)/c
+
+    Args:
+       nu: wavenumber (cm-1)
+       T: temperature (K)
+       M: atom/molecular mass
+    
+    Returns:
+       sigma: doppler width (standard deviation) (cm-1)
+
+    """
+    c3=3.0415595e-07
+    return c3*jnp.sqrt(T/M)*nu
