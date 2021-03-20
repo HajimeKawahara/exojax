8bd67538df01ba68aebf7006d724e892621477f8
Hajime Kawahara <divrot@gmail.com>
2021-03-20 13:02:13 +0900

    added xsm in autoXS

--- src/exojax/spec/autospec.py
+++ src/exojax/spec/autospec.py
@@ -4,7 +4,11 @@ from exojax.spec.opacity import xsection
 from exojax.spec.hitran import SijT, doppler_sigma,  gamma_natural, gamma_hitran
 from exojax.spec.exomol import gamma_exomol
 from exojax.spec import molinfo
+from exojax.spec.rtransfer import rtrun, dtaux
+from exojax.spec import make_numatrix0
+from exojax.spec.lpf import xsmatrix
 
+from jax import jit, vmap
 import pathlib
 __all__ = ['AutoXS']
 
@@ -67,15 +71,90 @@ class AutoXS(object):
         nu0=mdb.nu_lines
         xsv=xsection(self.nus,nu0,sigmaD,gammaL,Sij,memory_size=self.memory_size)
         return xsv
+
+    def xsmatrix(self,Tarr,Parr):
+        """cross section matrix
+        Args: 
+           Tarr: temperature layer (K)
+           Parr: pressure layer (bar)
+        Returns:
+           cross section (cm2)
+        """
+        mdb=self.mdb
+        if self.database == "ExoMol":
+            qt=mdb.qr_interp(T)
+            gammaLM = jit(vmap(gamma_exomol,(0,0,None,None)))\
+                              (P,T,mdb.n_Texp,mdb.alpha_ref)\
+                     + gamma_natural(mdb.A)
+            molmass=mdb.molmass
+            SijM=jit(vmap(SijT,(0,None,None,None,None)))\
+                  (Tarr,mdb.logsij0,mdb.nu_lines,mdb.elower,qt)
+
+        elif self.database == "HITRAN" or self.database == "HITEMP":
+            qt=mdb.Qr_layer(Tarr)
+            gammaLM = jit(vmap(gamma_hitran,(0,0,0,None,None,None)))\
+                      (Parr,Tarr,Parr, mdb.n_air, mdb.gamma_air, mdb.gamma_self)\
+                      + gamma_natural(mdb.A)
+            molmass=molinfo.molmass(self.molecules)
+            SijM=jit(vmap(SijT,(0,None,None,None,0)))\
+                  (Tarr,mdb.logsij0,mdb.nu_lines,mdb.elower,qt)
+            
+        sigmaDM=jit(vmap(doppler_sigma,(None,0,None)))\
+                 (mdb.nu_lines,Tarr,molmass)
         
+        nu0=mdb.nu_lines
+        numatrix=make_numatrix0(nus,nu0)
+        xsm=xsmatrix(numatrix,sigmaDM,gammaLM,SijM)
+        return xsm
+
+    
+class AutoRT(object):
+    
+    """exojax auto radiative transfer
+    
+    """
+    def __init__(self,nus,gravity,Tarr,Parr,dParr=None):
+        """
+        Args:
+           nus: wavenumber bin (cm-1)
+           gravity: gravity (cm/s2)
+           Tarr: temperature layer (K)
+           Parr: pressure layer (bar)
+           dParr: delta pressure (bar) optional
+        """
+        self.nus=nus
+        self.gravity=gravity
+        self.nlayer=len(Tarr)        
+        self.Tarr=Tarr
+        self.Parr=Parr
+        if dParr is None:
+            from exojax.utils.chopstacks import buildwall 
+            wParr=buildwall(Parr)
+            self.dParr=wParr[1:]-wParr[0:-1]
+        else:
+            self.dParr=dParr
+            
 if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt
     nus=np.linspace(6101.0,6115.0,3000,dtype=np.float64)
-#    nus=np.linspace(5085.0,5115.0,3000,dtype=np.float64)
-    #autoxs=AutoXS(nus,"HITRAN","CO")
-    autoxs=AutoXS(nus,"ExoMol","NH3")
-    #autoxs=AutoXS(nus,"HITEMP","CO")
-    xsv=autoxs.xsection(1000.0,1.0) #1000K, 1bar
-    plt.plot(nus,xsv)
+
+    #XS
+    autoxs=AutoXS(nus,"HITRAN","CO")
+    Tarr=np.array([1000.0,1500.0])
+    Parr=np.array([1.0,0.1])
+    xsm=autoxs.xsmatrix(Tarr,Parr) #1000K, 1bar
+
+    #xsv=autoxs.xsection(1000.0,1.0) #1000K, 1bar
+    plt.plot(nus,xsm[0,:])
+    plt.plot(nus,xsm[1,:])
+
     plt.show()
+
+    #RT
+#    Parr, dParr, k=rt.pressure_layer(NP=100)
+#    Tarr = 1500.*(Parr/Parr[-1])**-0.02
+    
+#    autort=AutoRT(nus)
+    
+
