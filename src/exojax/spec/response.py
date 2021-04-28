e9857ce85cef3561a516c4daa8d43679fbd6222a
Hajime Kawahara <divrot@gmail.com>
2021-04-28 15:35:01 +0900

    fixed rigitrotm

--- src/exojax/spec/response.py
+++ src/exojax/spec/response.py
@@ -216,9 +216,9 @@ def rigidrotm(nus,F0,vsini,u1=0.0,u2=0.0):
 
     """
     c=299792.458
-    dvmat=jnp.array(c*jnp.log(nusd[None,:]/nus[:,None]))
+    dvmat=jnp.array(c*jnp.log(nus[None,:]/nus[:,None]))
 #    kernel=jnp.exp(-(dvmat+RV)**2/(2.0*beta**2))
-    x=dvmat+RV/vsini
+    x=(dvmat+RV)/vsini
     x2=x*x
     kernel=jnp.where(x2<1.0,jnp.pi/2.0*u1*(1.0 - x2) - 2.0/3.0*jnp.sqrt(1.0-x2)*(-3.0+3.0*u1+u2*2.0*u2*x2),0.0)
     kernel=kernel/jnp.sum(kernel,axis=0) #axis=N
@@ -296,7 +296,7 @@ if __name__ == "__main__":
     Fr=responseRTM(dvmatx,F0,vsini_in,beta,RV,u1,u2,zeta)
 
     #new
-    Frot=rigidrot(nus,F0,vsini_in,u1,u2)
+    Frot=rigidrotm(nus,F0,vsini_in,u1,u2)
     Fg=ipgaussm(nus,nusd,Frot,beta,RV)
 
     plt.plot(wav,F0)
