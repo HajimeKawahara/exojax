0aeff6be9b15ac329260a74ac6148b1c37699f05
Hajime Kawahara <divrot@gmail.com>
2021-05-26 14:15:01 +0900

    Update __init__.py

--- src/exojax/__init__.py
+++ src/exojax/__init__.py
@@ -1,8 +1,8 @@
 __all__ = []
 
 __version__ = "0.5"
-__uri__ = ""
-__author__ = "exojax"
-__email__ = ""
-__license__ = ""
-__description__ = ""
+__uri__ = "http://secondearths.sakura.ne.jp/exojax/"
+__author__ = "Hajime Kawahara and collaborators"
+__email__ = "divrot@gmail.com"
+__license__ = "MIT"
+__description__ = "Auto-differentiable line-by-line spectral modeling of exoplanets/brown dwarfs using JAX."
