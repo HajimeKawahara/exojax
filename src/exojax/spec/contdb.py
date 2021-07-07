42e28c7a0eebb39a0ca70673cc5d0462b130589f
Brett M. Morris <morrisbrettm@gmail.com>
2021-07-07 15:07:29 +0200

    Remove python for loops and replace with lax.scan

--- src/exojax/spec/contdb.py
+++ src/exojax/spec/contdb.py
@@ -90,20 +90,20 @@ def bound_free_absorption(wavelength_um, temperature):
     lambda_0 = 1.6419  # photo-detachment threshold
 
     #   //tabulated constant from John (1988)
-    C_n = [0.0, 152.519, 49.534, -118.858, 92.536, -34.194, 4.982]
-
     def f(wavelength_um):
-        x = 0 
+        C_n = jnp.vstack(
+            [jnp.arange(7), [0.0, 152.519, 49.534, -118.858, 92.536, -34.194, 4.982]]
+        ).T
 
-        for i in range(1, 7):
-            x += C_n[i] * jnp.power((1.0/wavelength_um - 1.0/lambda_0), (i-1)/2.0) 
+        def body_fun(val, x):
+            i, C_n_i = x
+            return val, val + C_n_i * jnp.power(jnp.clip(1.0/wavelength_um - 1.0/lambda_0, a_min=0, a_max=None), (i-1)/2.0)
 
-        return x 
-
+        return lax.scan(body_fun, jnp.zeros_like(wavelength_um), C_n)[-1].sum(0)
 
     # first, we calculate the photo-detachment cross-section (in cm2)
     kappa_bf = (1e-18 * wavelength_um ** 3 * 
-        jnp.power(1.0/wavelength_um - 1.0/lambda_0, 1.5) * f(wavelength_um)
+        jnp.power(jnp.clip(1.0/wavelength_um - 1.0/lambda_0, a_min=0, a_max=None), 1.5) * f(wavelength_um)
     )
 
     kappa_bf = jnp.where(
@@ -116,44 +116,45 @@ def bound_free_absorption(wavelength_um, temperature):
 
 
 def free_free_absorption(wavelength_um, temperature):
-
     # coefficients from John (1988)
     # to follow his notation (which starts at an index of 1), the 0-index components are 0
     # for wavelengths larger than 0.3645 micron
-    A_n1 = [0.0, 0.0, 2483.3460, -3449.8890, 2200.0400, -696.2710, 88.2830]
-    B_n1 = [0.0, 0.0, 285.8270, -1158.3820, 2427.7190, -1841.4000, 444.5170]
-    C_n1 = [0.0, 0.0, -2054.2910, 8746.5230, -13651.1050, 8624.9700, -1863.8650]
-    D_n1 = [0.0, 0.0, 2827.7760, -11485.6320, 16755.5240, -10051.5300, 2095.2880]
-    E_n1 = [0.0, 0.0, -1341.5370, 5303.6090, -7510.4940, 4400.0670, -901.7880]
-    F_n1 = [0.0, 0.0, 208.9520, -812.9390, 1132.7380, -655.0200, 132.9850]
+    A_n1 = jnp.array([0.0, 0.0, 2483.3460, -3449.8890, 2200.0400, -696.2710, 88.2830])
+    B_n1 = jnp.array([0.0, 0.0, 285.8270, -1158.3820, 2427.7190, -1841.4000, 444.5170])
+    C_n1 = jnp.array([0.0, 0.0, -2054.2910, 8746.5230, -13651.1050, 8624.9700, -1863.8650])
+    D_n1 = jnp.array([0.0, 0.0, 2827.7760, -11485.6320, 16755.5240, -10051.5300, 2095.2880])
+    E_n1 = jnp.array([0.0, 0.0, -1341.5370, 5303.6090, -7510.4940, 4400.0670, -901.7880])
+    F_n1 = jnp.array([0.0, 0.0, 208.9520, -812.9390, 1132.7380, -655.0200, 132.9850])
 
     # for wavelengths between 0.1823 micron and 0.3645 micron
-    A_n2 = [0.0, 518.1021, 473.2636, -482.2089, 115.5291, 0.0, 0.0]
-    B_n2 = [0.0, -734.8666, 1443.4137, -737.1616, 169.6374, 0.0, 0.0]
-    C_n2 = [0.0, 1021.1775, -1977.3395, 1096.8827, -245.6490, 0.0, 0.0]
-    D_n2 = [0.0, -479.0721, 922.3575, -521.1341, 114.2430, 0.0, 0.0]
-    E_n2 = [0.0, 93.1373, -178.9275, 101.7963, -21.9972, 0.0, 0.0]
-    F_n2 = [0.0, -6.4285, 12.3600, -7.0571, 1.5097, 0.0, 0.0]
+    A_n2 = jnp.array([0.0, 518.1021, 473.2636, -482.2089, 115.5291, 0.0, 0.0])
+    B_n2 = jnp.array([0.0, -734.8666, 1443.4137, -737.1616, 169.6374, 0.0, 0.0])
+    C_n2 = jnp.array([0.0, 1021.1775, -1977.3395, 1096.8827, -245.6490, 0.0, 0.0])
+    D_n2 = jnp.array([0.0, -479.0721, 922.3575, -521.1341, 114.2430, 0.0, 0.0])
+    E_n2 = jnp.array([0.0, 93.1373, -178.9275, 101.7963, -21.9972, 0.0, 0.0])
+    F_n2 = jnp.array([0.0, -6.4285, 12.3600, -7.0571, 1.5097, 0.0, 0.0])
 
+    coeffs1 = jnp.vstack([
+        jnp.arange(7), A_n1, B_n1, C_n1, D_n1, E_n1, F_n1
+    ]).T
 
-    def ff(wavelength, A_n, B_n, C_n, D_n, E_n, F_n):
-        x = 0 
+    coeffs2 = jnp.vstack([
+        jnp.arange(7), A_n2, B_n2, C_n2, D_n2, E_n2, F_n2
+    ]).T
 
-        for i in range(1, 7):
-            x += (jnp.power(5040.0/temperature, (i+1)/2.0) * 
-                  (wavelength**2 * A_n[i] + B_n[i] + C_n[i]/wavelength + D_n[i]/wavelength**2  + 
-                   E_n[i]/wavelength**3 + F_n[i]/wavelength**4))
-
-        return x*1e-29 
-
+    def body_fun(val, x):
+        i, A_n_i, B_n_i, C_n_i, D_n_i, E_n_i, F_n_i = x
+        return val, val + (jnp.power(5040.0/temperature, (i+1)/2.0) * 
+                  (wavelength_um**2 * A_n_i + B_n_i + C_n_i/wavelength_um + D_n_i/wavelength_um**2  + 
+                   E_n_i/wavelength_um**3 + F_n_i/wavelength_um**4))
 
     kappa_ff = jnp.where(
         wavelength_um > 0.3645, 
-        ff(wavelength_um, A_n1, B_n1, C_n1, D_n1, E_n1, F_n1), 
+        lax.scan(body_fun, jnp.zeros_like(wavelength_um), coeffs)[-1].sum(0) * 1e-29, 
         0
     ) + jnp.where(
         (wavelength_um >= 0.1823) & (wavelength_um <= 0.3645),
-        ff(wavelength_um, A_n2, B_n2, C_n2, D_n2, E_n2, F_n2),
+        lax.scan(body_fun, jnp.zeros_like(wavelength_um), coeffs2)[-1].sum(0) * 1e-29 ,
         0
     )
 
