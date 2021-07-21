2a8603120b485e8dd6196231acb26f2e6e5f4140
Hajime Kawahara <divrot@gmail.com>
2021-07-21 16:07:21 +0900

    started benchmark

---
+++ tests/benchmark/lpf_bm_test.py
@@ -0,0 +1,40 @@
+import pytest
+import time 
+from exojax.spec.lpf import xsvector
+from exojax.spec.make_numatrix import make_numatrix0
+import numpy as np
+
+def xs(Nline=10000):
+    nu0=2000.0
+    nu1=2040.0
+    nus=np.linspace(nu0,nu1,4000,dtype=np.float64)
+    nu_lines=np.random.normal(size=Nline)*(nu1-nu0)+nu0
+    sigmaD=np.random.normal(size=Nline)
+    gammaL=np.random.normal(size=Nline)
+    Sij=np.random.normal(size=Nline)
+    
+    numatrix=make_numatrix0(nus,nu_lines,warning=False)
+    xsv = xsvector(numatrix,sigmaD,gammaL,Sij)
+    xsv.block_until_ready()
+    return True
+
+
+def test_benchmark_a(benchmark):
+    ret = benchmark(xs,10)
+    assert ret
+
+def test_benchmark_b(benchmark):
+    ret = benchmark(xs,100)
+    assert ret
+
+def test_benchmark_c(benchmark):
+    ret = benchmark(xs,1000)
+    assert ret
+
+def test_benchmark_d(benchmark):
+    ret = benchmark(xs,10000)
+    assert ret
+
+def test_benchmark_e(benchmark):
+    ret = benchmark(xs,100000)
+    assert ret
