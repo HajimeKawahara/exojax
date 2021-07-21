b5516b4e23349550d273c0e688988b7454cf5c47
Hajime Kawahara <divrot@gmail.com>
2021-07-21 17:04:38 +0900

    added modit benchmark

--- tests/benchmark/lpf_bm_test.py
+++ tests/benchmark/lpf_bm_test.py
@@ -8,10 +8,10 @@ def xs(Nline=10000):
     nu0=2000.0
     nu1=2040.0
     nus=np.linspace(nu0,nu1,4000,dtype=np.float64)
-    nu_lines=np.random.normal(size=Nline)*(nu1-nu0)+nu0
-    sigmaD=np.random.normal(size=Nline)
-    gammaL=np.random.normal(size=Nline)
-    Sij=np.random.normal(size=Nline)
+    nu_lines=np.random.rand(Nline)*(nu1-nu0)+nu0
+    sigmaD=np.random.rand(Nline)
+    gammaL=np.random.rand(Nline)
+    Sij=np.random.rand(Nline)
     
     numatrix=make_numatrix0(nus,nu_lines,warning=False)
     xsv = xsvector(numatrix,sigmaD,gammaL,Sij)
@@ -38,3 +38,7 @@ def test_benchmark_d(benchmark):
 def test_benchmark_e(benchmark):
     ret = benchmark(xs,100000)
     assert ret
+
+#def test_benchmark_f(benchmark):
+#    ret = benchmark(xs,1000000)
+#    assert ret
