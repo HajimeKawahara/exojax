2e82cdb7ba723afa733e23577be4c745eecadb15
Hajime Kawahara <divrot@gmail.com>
2021-07-25 15:06:26 +0900

    AM01

---
+++ src/exojax/atm/amclouds.py
@@ -0,0 +1,30 @@
+"""Ackerman and Marley 2001 cloud model
+
+   - Ackerman and Marley (2001) ApJ 556, 872
+
+
+"""
+
+def vterminal(drho,eta,Nkn):
+    """terminal velocity of droplets
+
+    Note:
+       Based on equation (B1) in Appendix B of AM01. See also (10-138) p415 in Hans R Pruppacher and James D Klett. Microstructure of atmospheric clouds and precipitation. InMicrophysics of clouds and precipitation, pages 10–73. Springer, 2010
+
+    Args: 
+       drho: density difference between condensates and atmosphere
+       eta: dynamic viscocity of the atmosphere
+       Nkn: Knudsen number (ratio of the molecular mean free path to the droplet radius)
+       
+
+    """
+
+    vf=2.0/9.0*(1.0+1.26*Nkn)*drho/eta
+    
+    return vf
+    
+def eta_Rosner():
+    """dynamic viscocity of the atmosphereby Rosner (2000)
+
+    """
+    return eta
---
+++ src/exojax/atm/simple_clouds.py
@@ -0,0 +1,23 @@
+"""cloud opacity
+
+
+"""
+
+def powerlaw_clouds(nus,kappac0=0.01,nuc0=28571.,alphac=1.):
+    """power-law cloud model
+
+    
+
+    Args:
+       kappac0: opacity (cm2/g) at nuc0
+       nuc0: wavenumber for kappac0
+       alphac: power
+
+    Returns:
+       cross section (cm2)
+
+    Note:
+       alphac = - gamma of the definition in petitRadtrans. Also, default nuc0 corresponds to lambda0=0.35 um.
+
+    """
+    return kappac0*(nus/nuc0)**alphac
