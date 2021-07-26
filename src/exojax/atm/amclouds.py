f8e45db7adb1003481103ec670f8aeeac7afc291
Hajime Kawahara <divrot@gmail.com>
2021-07-26 16:44:12 +0900

    added dynamic viscosity for H2

--- src/exojax/atm/amclouds.py
+++ src/exojax/atm/amclouds.py
@@ -16,6 +16,8 @@ def vterminal(drho,eta,Nkn):
        eta: dynamic viscocity of the atmosphere
        Nkn: Knudsen number (ratio of the molecular mean free path to the droplet radius)
        
+    Returns:
+       terminal velocity
 
     """
 
@@ -23,8 +25,29 @@ def vterminal(drho,eta,Nkn):
     
     return vf
     
-def eta_Rosner():
-    """dynamic viscocity of the atmosphereby Rosner (2000)
+def eta_H2_Rosner(T):
+    """dynamic viscocity of the H2 atmosphere by Rosner (2000)
+    
+    Args:
+       T: temperature (K)
+    
+    Returns:
+       viscosity (g/s/cm)
+
+    Note:
+       (B2) in AM01. epsilon=59.7*k, d (H2) =2.827*e-10(m), m=2*m_u
+
+    Example:
+       >>> #factor in the code is computed by
+       >>> from scipy.constants import m_u, k #SI system
+       >>> d=2.827e-10 #m
+       >>> si2cgs=10.0 #kg/m/s -> g/cm/s (viscosity)
+       >>> 5.0/16.0*np.sqrt(np.pi*k*2*m_u)/(np.pi*d*d)/1.22*(1.0/59.7)**0.16*si2cgs
+       2.0127454503595356e-06
 
     """
+    factor=2.01274545e-06 
+    eta = factor*(T**0.66)
     return eta
+
+
---
+++ src/exojax/atm/psat.py
@@ -0,0 +1,19 @@
+"""Saturation Vapor Pressure
+
+"""
+import jax.numpy as jnp
+
+def Psat_enstatite(T):
+    """Saturation Vapor Pressure for Enstatite (MgSiO3)
+
+    Note:
+       Taken from Ackerman and Marley 2001 Appendix A (A4) see also their errata.
+
+    Args:
+       T: temperature (K)
+    
+    Returns:
+       saturation vapor pressure (bar)
+
+    """
+    return jnp.exp(25.37-58663./T)
