c8fb84c2ceae1c5896194593520395ed29aaff83
Hajime Kawahara <divrot@gmail.com>
2021-07-29 07:40:21 +0900

    import subroutines from AM.ipynb

--- src/exojax/atm/amclouds.py
+++ src/exojax/atm/amclouds.py
@@ -4,6 +4,77 @@
 
 
 """
+from jax import jit
+import jax.numpy as jnp
+from jax import vmap
+
+def VMRcloud(P,Pbase,fsed,VMRbase,kc=1):
+    """VMR of clouds based on AM01
+    
+    Args:
+        P: Pressure (bar)
+        Pbase: base pressure (bar)
+        fsed: fsed
+        VMRbase: VMR of condensate at cloud base
+        kc: constant ratio of condenstates to total mixing ratio
+        
+    Returns:
+        VMR of condensates
+        
+    """
+    VMRc=jnp.where(Pbase>P,VMRbase*(P/Pbase)**(fsed/kc),0.0)
+    return VMRc
+
+@jit
+def get_VMRc(P,Pbase,fsed,VMRbase,kc=1):
+    """VMR array of clouds based on AM01
+    
+    Args:
+        Parr: Pressure array [Nlayer] (bar)
+        Pbase: base pressure (bar)
+        fsed: fsed
+        VMRbase: VMR of condensate at cloud base
+        kc: constant ratio of condenstates to total mixing ratio
+        
+    Returns:
+        VMR array of condensates [Nlayer]
+        
+    """
+    return vmap(VMRcloud,(0,None,None,None),0)(P,Pbase,fsed,VMRbase,kc)
+
+
+@jit
+def get_Pbase(Parr,Psat,VMR):
+    """get Pbase from an intersection of a T-P profile and Psat(T) curves
+    Args:
+        Parr: pressure array
+        Psat: saturation pressure arrau
+        VMR: VMR for vapor
+        
+    Returns:
+        Pbase: base pressure
+    """
+    #ibase=jnp.searchsorted((Psat/VMR)-Parr,0.0) # 231 +- 9.2 us
+    ibase=jnp.argmin(jnp.abs(jnp.log(Parr)-jnp.log(Psat)+jnp.log(VMR))) # 73.8 +- 2.9 us
+    return Parr[ibase]
+
+def vf_stokes(r,g,eta,Nkn=0.):
+    """terminal velocity of Stokes flow (Reynolds number << 1)
+    
+    Args:
+        r: particle size
+        g: gravity
+        eta: dynamic viscosity
+        Nkn: Knudsen number
+        
+    Note:
+        (1.0+1.255*Nkn) is the Cunningham factor
+    
+    Return:
+        terminal velocity
+        
+    """
+    return 2.0*g*r*r*(1.0+1.255*Nkn)/(9.0*eta)
 
 def vterminal(drho,eta,Nkn):
     """terminal velocity of droplets
--- src/exojax/atm/psat.py
+++ src/exojax/atm/psat.py
@@ -18,3 +18,19 @@ def Psat_enstatite_AM01(T):
 
     """
     return jnp.exp(25.37-58663./T)
+
+
+def Psat_Fe_solid(T):
+    """Saturation Vapor Pressure for Solid Fe (Fe)
+
+    Note:
+       Taken from Ackerman and Marley 2001 Appendix A (A4) see also their errata.
+
+    Args:
+       T: temperature (K)
+    
+    Returns:
+       saturation vapor pressure (bar)
+
+    """
+    return jnp.exp(15.71-47664./T)
