3c39c437d16dd1c465a3b731b55d975f2ef15962
Hajime Kawahara <divrot@gmail.com>
2021-08-10 16:00:56 +0900

    modit_test checked

--- tests/auto/modit_test.py
+++ tests/auto/modit_test.py
@@ -48,10 +48,10 @@ F1=autort.rtrun()
 #F1o=autort.spectrum(nusobs,100000.0,20.0,0.0)
 
 xsmode="DIT"
-nus,wav,res=nugrid(1900.0,2300.0,160000,"cm-1",xsmode=xsmode)
+nus_,wav,res=nugrid(1900.0,2300.0,160000,"cm-1",xsmode=xsmode)
 Parr=numpy.logspace(-8,2,100) #100 layers from 10^-8 bar to 10^2 bar
 Tarr = 500.*(Parr/Parr[-1])**0.02    
-autort=AutoRT(nus,1.e5,2.33,Tarr,Parr,xsmode=xsmode) #g=1.e5 cm/s2, mmw=2.33
+autort=AutoRT(nus_,1.e5,2.33,Tarr,Parr,xsmode=xsmode) #g=1.e5 cm/s2, mmw=2.33
 autort.addcia("H2-H2",0.74,0.74)       #CIA, mmr(H)=0.74
 autort.addmol("ExoMol","CO",0.01)      #CO line, mmr(CO)=0.01
 F2=autort.rtrun()
@@ -71,23 +71,13 @@ fig=plt.figure()
 ax=fig.add_subplot(211)
 plt.plot(nus,F0,label="LPF",color="C0",alpha=0.4)
 plt.plot(nus,F1,label="MODIT",color="C1",alpha=0.4)
+plt.plot(nus_,F2,label="DIT",color="C2",ls="dotted",alpha=0.4)
+
 plt.plot(nus,F1-F0,color="C1",alpha=0.4)
 plt.legend(loc="upper right")
-
 
 ax=fig.add_subplot(212)
 plt.plot(nus,F1-F0,label="MODIT-LPF",color="C1",alpha=0.4)
 plt.legend(loc="upper right")
-
-
-#ax=fig.add_subplot(413)
-#plt.plot(nusobs,F0o,label="LPF (obs)",color="C0",alpha=0.4,ls="dashed",lw=2)
-#plt.plot(nusobs,F1o,label="MODIT (obs)",color="C1",alpha=0.4,ls="dashed",lw=2)
-#plt.plot(nusobs,F1o-F0o,color="C1",alpha=0.4)
-#plt.legend(loc="upper right")
-
-#ax=fig.add_subplot(414)
-#plt.plot(nusobs,F1o-F0o,color="C1",alpha=0.4,lw=2,label="MODIT-LPF (obs)")
-#plt.legend(loc="upper right")
 
 plt.show()
