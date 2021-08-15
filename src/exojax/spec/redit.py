bf74ff6441a64e1fdcfc712f5ba589dac00d07ee
Hajime Kawahara <divrot@gmail.com>
2021-08-15 14:52:51 +0900

    added test

--- src/exojax/spec/redit.py
+++ src/exojax/spec/redit.py
@@ -12,6 +12,7 @@ from jax.ops import index_add
 from jax.ops import index as joi
 from exojax.spec.dit import getix
 from exojax.spec.modit import inc2D_givenx
+from exojax.spec.lpf import voigt
 
 @jit
 def xsvector(cnu,indexnu,R,nsigmaD,ngammaL,S,nu_grid,ngammaL_grid):
@@ -45,9 +46,22 @@ def xsvector(cnu,indexnu,R,nsigmaD,ngammaL,S,nu_grid,ngammaL_grid):
     k = jnp.fft.rfftfreq(2*Ng_nu,1)    
     lsda=jnp.zeros((len(nu_grid),len(ngammaL_grid))) #LSD array init
     Slsd=inc2D_givenx(lsda, S,cnu,indexnu,log_ngammaL,log_ngammaL_grid) #LSD
+
+    qvector=
     
+    Mat=jnp.hstack([log_ngammaL_grid,Slsd])
+    def seqconv(x,arr):        
+        carry=0.0
+        log_ngammaL_each=arr[0]
+        se=arr[1:]        
+        kernel=voigt(qvector,log_nstbeta,log_ngammaL_each)
+        arr=jnp.convolve(se,kernel,mode="same")  
+        return carry, arr
     
-    return xs
+    val,xsmm=scan(seqconv,0.0,Mat)
+    xsm=jnp.sum(xsmm,axis=0)
+    
+    return xsm
 
 if __name__=="__main__":
     print("test")
