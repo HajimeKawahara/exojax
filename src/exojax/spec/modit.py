ab6f31a97528bf2449cbf887eb83e30923652561
Hajime Kawahara <divrot@gmail.com>
2021-08-15 15:35:58 +0900

    added err DIT

--- src/exojax/spec/modit.py
+++ src/exojax/spec/modit.py
@@ -82,10 +82,12 @@ def xsvector(cnu,indexnu,R,dLarray,nsigmaD,ngammaL,S,nu_grid,ngammaL_grid):
     log_ngammaL=jnp.log(ngammaL)
     log_ngammaL_grid = jnp.log(ngammaL_grid)
 
-    k = jnp.fft.rfftfreq(2*Ng_nu,1)    
+    k = jnp.fft.rfftfreq(2*Ng_nu,1)
+#    k = jnp.fft.rfftfreq(4*Ng_nu,1) #Nv=4   
     lsda=jnp.zeros((len(nu_grid),len(ngammaL_grid))) #LSD array
     Slsd=inc2D_givenx(lsda, S,cnu,indexnu,log_ngammaL,log_ngammaL_grid) #Lineshape Density
     Sbuf=jnp.vstack([Slsd,jnp.zeros_like(Slsd)])
+    #Sbuf=jnp.vstack([Slsd,jnp.zeros_like(Slsd),jnp.zeros_like(Slsd),jnp.zeros_like(Slsd)]) #Nv=4
     til_Slsd = jnp.fft.rfft(Sbuf,axis=0)
     
     til_Voigt=folded_voigt_kernel_logst(k, log_nstbeta,log_ngammaL_grid,dLarray)
--- tests/comparison/errCO_DIT.py
+++ tests/comparison/errCO_DIT.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 plt.style.use('bmh')
 from exojax.spec import make_numatrix0
 from exojax.spec.lpf import xsvector as lpf_xsvector
-from exojax.spec.modit import xsvector as modit_xsvector
+from exojax.spec.dit import xsvector as dit_xsvector
 from exojax.spec import initspec
 from exojax.spec import xsection as lpf_xsection
 from exojax.spec.hitran import SijT, doppler_sigma, gamma_hitran, gamma_natural
@@ -18,9 +18,8 @@ from exojax.spec.hitran import normalized_doppler_sigma
 
 def comperr(Nnu,plotfig=False):
 
-    nus=np.logspace(np.log10(1.e7/2700),np.log10(1.e7/2100.),Nnu,dtype=np.float64)
+    nus=np.linspace(1.e7/2700,1.e7/2100.,Nnu,dtype=np.float64)
 
-#    nus=np.logspace(np.log10(3000),np.log10(6000.0),Nnu,dtype=np.float64)
     mdbCO=moldb.MdbHit('/home/kawahara/exojax/data/CO/05_hit12.par',nus)
     
     Mmol=28.010446441149536
@@ -44,14 +43,14 @@ def comperr(Nnu,plotfig=False):
     gammaL = gamma_hitran(Pfix,Tfix,Pfix, mdbCO.n_air, mdbCO.gamma_air, mdbCO.gamma_self)
     #+ gamma_natural(A) #uncomment if you inclide a natural width
     sigmaD=doppler_sigma(mdbCO.nu_lines,Tfix,Mmol)
+    sigmaD_grid=set_ditgrid(sigmaD)
+    gammaL_grid=set_ditgrid(gammaL)
+
     
-    cnu,indexnu,R,dLarray=initspec.init_modit(mdbCO.nu_lines,nus)
-    nsigmaD=normalized_doppler_sigma(Tfix,Mmol,R)
-    ngammaL=gammaL/(mdbCO.nu_lines/R)
-    ngammaL_grid=set_ditgrid(ngammaL)
+    cnu,indexnu,dLarray=initspec.init_dit(mdbCO.nu_lines,nus)
     
-    xs_modit_lp=modit_xsvector(cnu,indexnu,R,dLarray,nsigmaD,ngammaL,Sij,nus,ngammaL_grid)
-    wls_modit = 100000000/nus
+    xs_dit_lp=dit_xsvector(cnu,indexnu,dLarray,sigmaD,gammaL,Sij,nus,sigmaD_grid,gammaL_grid)
+    wls_dit = 100000000/nus
     
     #ref (direct)
     d=10000
@@ -59,23 +58,23 @@ def comperr(Nnu,plotfig=False):
     xsv_lpf_lp=lpf_xsection(nus,ll,sigmaD,gammaL,Sij,memory_size=30)
 
 
-    dif=xs_modit_lp/xsv_lpf_lp-1.
+    dif=xs_dit_lp/xsv_lpf_lp-1.
     med=np.median(dif)
     iju=22940.
     ijd=26400.
-    limu,limd=np.searchsorted(wls_modit[::-1],[iju,ijd])
+    limu,limd=np.searchsorted(wls_dit[::-1],[iju,ijd])
     std=np.std(dif[::-1][limu:limd])
     
-    return med,std,R,ijd,iju,wls_modit,xs_modit_lp,xsv_lpf_lp,dif
+    return med,std,ijd,iju,wls_dit,xs_dit_lp,xsv_lpf_lp,dif
 
 
 if __name__=="__main__":
     import matplotlib
-    m,std,R,ijd,iju,wls_modit,xs_modit_lp,xsv_lpf_lp,dif=comperr(100000)
-    m1,std1,R1,ijd1,iju1,wls_modit1,xs_modit_lp1,xsv_lpf_lp1,dif1=comperr(3000000)
+    m,std,ijd,iju,wls_dit,xs_dit_lp,xsv_lpf_lp,dif=comperr(100000)
+    m1,std1,ijd1,iju1,wls_dit1,xs_dit_lp1,xsv_lpf_lp1,dif1=comperr(3000000)
 
-    print(m,std,R)
-    print(m1,std1,R1)
+    print(m,std)
+    print(m1,std1)
 
 
 
@@ -89,9 +88,9 @@ if __name__=="__main__":
         tip=2.0
         fig=plt.figure(figsize=(12,3))
         ax=plt.subplot2grid((12, 1), (0, 0),rowspan=8)
-        plt.plot(wls_modit1,xsv_lpf_lp1,label="Direct",color="C0",alpha=0.3,markersize=3)
-        plt.plot(wls_modit,xs_modit_lp,color="C1",lw=1,alpha=0.3,label="R="+str(R))
-        plt.plot(wls_modit1,xs_modit_lp1,color="C2",lw=1,alpha=0.5,label="R="+str(R1),ls="dashed")
+        plt.plot(wls_dit1,xsv_lpf_lp1,label="Direct",color="C0",alpha=0.3,markersize=3)
+        plt.plot(wls_dit,xs_dit_lp,color="C1",lw=1,alpha=0.3)
+        plt.plot(wls_dit1,xs_dit_lp1,color="C2",lw=1,alpha=0.5,ls="dashed")
 
         plt.xlim(ijd,iju)    
 #        plt.xlim(llow*10-tip,lhigh*10+tip)    
@@ -107,8 +106,8 @@ if __name__=="__main__":
         plt.xlabel('wavelength [$\AA$]')
         
         ax=plt.subplot2grid((12, 1), (8, 0),rowspan=4)
-        plt.plot(wls_modit,np.abs((dif)),alpha=0.2,color="C1",label="R="+str(R))
-        plt.plot(wls_modit1,np.abs((dif1)),alpha=0.5,color="C2",label="R="+str(R1))
+        plt.plot(wls_dit,np.abs((dif)),alpha=0.2,color="C1")
+        plt.plot(wls_dit1,np.abs((dif1)),alpha=0.5,color="C2")
         
         plt.ylabel("difference (%)",fontsize=10)
         plt.xlim(ijd,iju)
@@ -120,6 +119,6 @@ if __name__=="__main__":
         plt.xlabel('wavelength [$\AA$]')
         plt.legend(loc="upper left")
         
-        plt.savefig("fig/comparison_modit.png", bbox_inches="tight", pad_inches=0.0)
-        plt.savefig("fig/comparison_modit.pdf", bbox_inches="tight", pad_inches=0.0)
+        plt.savefig("fig/comparison_dit.png", bbox_inches="tight", pad_inches=0.0)
+        plt.savefig("fig/comparison_dit.pdf", bbox_inches="tight", pad_inches=0.0)
         plt.show()
