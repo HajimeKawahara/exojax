014586451107b1ec6f3586758c8151a0a0028268
Tako <hiro.tako.ishikawa@icloud.com>
2021-08-15 23:46:36 +0900

    Make atomic-line-list of VALD3 available

---
+++ src/exojax/spec/vald3api.py
@@ -0,0 +1,125 @@
+"""API for VALD 3
+
+"""
+
+import numpy as np
+import pandas as pd
+
+def read_ExStellar(stellarf):
+    """VALD IO for "Extract stellar" file (long format)
+    Note:
+        =
+        =
+        See https://www.astro.uu.se/valdwiki/select_output
+
+    Args:
+        
+    Returns:
+        line data in pandas DataFrame
+        
+    """
+    dat = pd.read_csv(stellarf, sep=",",skiprows=3,\
+    names=("species","wav_lines","elowereV","vmic","loggf","rad_damping","stark_damping","waals_damping","lande","depth","reference"))
+    return dat
+    
+    
+def read_ExAll(allf):
+    """VALD IO for "Extract all" file (long format)
+    Note:
+        =
+        =
+        See https://www.astro.uu.se/valdwiki/presformat_output
+
+    Args:
+        Elm Ion
+        WL_vac(AA)
+        log gf*
+        E_low(eV):  lower excitation potential
+        J lo:  lower rotational quantum number
+        E_up(eV):  upper excitation potential
+        J up:  upper rotational quantum number
+        Lande lower
+        Lande upper
+        Lande mean
+        Damping Rad.
+        Damping Stark
+        Damping Waals
+
+    Returns:
+        line data in pandas DataFrame
+        
+    """
+    dat = pd.read_csv(allf, sep=",", skiprows=1, \
+    names=("species","wav_lines","loggf","elowereV","jlower","euppereV", "jupper", "landelower", "landeupper", "landemean", "rad_damping","stark_damping","waals_damping")\
+                     )
+
+    dat = dat[dat.species.str.startswith("'")] #Remove rows not starting with "'"
+    dat = dat[dat.species.str.startswith("' ").map({False: True, True: False})] #Remove rows of Reference
+    dat = dat[dat.species.str.startswith("'_").map({False: True, True: False})] #Remove rows of Reference
+    
+    dat = dat[ dat.species.str.len()<7 ] #Remove long name (molecules e.g., TiO)
+    dat = dat[dat.species.str[1:3].str.isupper().map({False: True, True: False})] #Remove names starting with successive uppercase letters (molecules e.g., CO, OH, CN)
+    #dat = dat[dat.species.str.startswith("'CO").map({False: True, True: False})]
+    dat = dat.reset_index(drop=True)
+    
+    dat = dat.astype({'wav_lines': 'float64', 'loggf': 'float64', 'elowereV': 'float64', 'jlower': 'float64', 'euppereV': 'float64'})
+
+    return dat
+    
+
+def pickup_param(ExAll):
+    """ extract transition parameters from VALD3 line list and insert the same DataFrame.
+    
+    Args:
+        ExAll: VALD3 line list (DataFrame): Output of read_ExAll
+    
+    Returns:
+        A:  Einstein coefficient in [s-1]
+        nu_lines:  transition waveNUMBER in [cm-1] (NOT frequency in [s-1])
+        elower: lower excitation potential [eV]
+        gupper: upper statistical weight
+        jlower: lower rotational quantum number (total angular momentum)
+        jupper
+        ielem:  atomic number (e.g., Fe=26)
+        iion:  ionized level (e.g., neutral=1, singly)
+        vdWdamp:  van der Waals damping parameters
+        gamRad: gamma(HWHM) of radiation damping
+       
+    Note:
+    
+    """
+    ccgs = 2.99792458e10 #[cm/s]
+    ecgs = 4.80320450e-10 #[esu]=[dyn^0.5*cm] !elementary charge
+    mecgs  = 9.10938356e-28 #[g] !electron mass
+    
+    # Correspondence between Atomic Number and Element Symbol
+    PeriodicTable = np.zeros([119], dtype=object) #PeriodicTable = np.empty([100], dtype=object)
+    PeriodicTable[:] = [' 0', 'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+
+    # insert new columns in VALD line list
+    ExAll["nu_lines"] = 1.e8 / ExAll["wav_lines"] #[cm-1]<-[AA]
+    ExAll["elower"] = ExAll["elowereV"] * 8065.541
+    ExAll["eupper"] = ExAll["euppereV"] * 8065.541
+    ExAll["gupper"] = ExAll["jupper"]*2+1
+    ExAll["glower"] = ExAll["jlower"]*2+1
+    #notes4Tako#どうせ比をとるので電子の縮退度等の係数は落ちる.(MyLog2017.rtf)
+    ExAll["A"] = 10**ExAll["loggf"] / ExAll["gupper"] * (ccgs*ExAll["nu_lines"])**2 \
+        *8*np.pi**2*ecgs**2 / (mecgs*ccgs**3)
+    
+    A=ExAll["A"].to_numpy()
+    nu_lines=ExAll["nu_lines"].to_numpy()
+    elower=ExAll["elower"].to_numpy()
+    gupper=ExAll["gupper"].to_numpy()
+    jlower=ExAll["jlower"].to_numpy()
+    jupper=ExAll["jupper"].to_numpy()
+    vdWdamp=ExAll["waals_damping"].to_numpy()
+    gamRad=ExAll["rad_damping"].to_numpy()
+
+    ielem = np.zeros(len(ExAll), dtype='int') #atomic number (e.g., Fe=26)
+    iion = np.zeros(len(ExAll), dtype='int') #e.g., neutral=1, singly ionized=2, ...
+    for i, sp in enumerate(ExAll['species']):
+        symbol = sp.strip("'").split(' ')[0]
+        ielem[i] = np.where(PeriodicTable==symbol)[0][0] if (symbol in PeriodicTable) else 0
+        iion[i] = int(sp.strip("'").split(' ')[-1])
+
+    return A, nu_lines, elower, gupper, jlower, jupper, ielem, iion, vdWdamp, gamRad
---
+++ src/exojax/spec/vald3db.py
@@ -0,0 +1,233 @@
+"""Atomic database (ADB) class
+    created by mimicking "moldb.py"
+"""
+import numpy as np
+import jax.numpy as jnp
+import pathlib
+from exojax.spec import vald3api, vald3
+import pandas as pd
+
+__all__ = ['AdbVald',]
+
+class AdbVald(object):
+    """ molecular database of ExoMol
+    
+    AdbVald is a class for VALD3.
+    
+    Attributes:
+        ...
+        elower (jnp array): the lower state energy (cm-1)
+        ...
+        #\\\\
+    
+    """
+    def __init__(self,path,nurange=[-np.inf,np.inf],margin=1.0,crit=-np.inf, bkgdatm="H2", broadf=True): #tako210721
+    
+        """Atomic database for VALD3 "Long format"
+
+        Args:
+           #\\\\
+           
+        Note:
+           #\\\\
+
+        """
+        #explanation="
+        
+        self.path = pathlib.Path(path)
+        #t0=self.path.parents[0].stem
+        #molec=t0+"__"+str(self.path.stem)
+        self.bkgdatm=bkgdatm
+        print("Background atmosphere: ",self.bkgdatm)
+        #molecbroad=t0+"__"+self.bkgdatm
+        
+        self.crit = crit
+        self.margin = margin
+        self.nurange=[np.min(nurange),np.max(nurange)]
+        self.broadf=broadf
+        #VALD3 output
+        self.vald3_file = self.path #tako210721
+        #Where exomol files are
+        #self.states_file = self.path/pathlib.Path(molec+".states.bz2")
+        #self.pf_file = self.path/pathlib.Path(molec+".pf")
+        #self.def_file = self.path/pathlib.Path(molec+".def")
+        #self.broad_file = self.path/pathlib.Path(molecbroad+".broad")
+
+
+
+
+        #load partition function (for 284 atomic species) from Barklem et al. (2016)
+        pff = "/home/tako/work/.database/Barklem_2016/J_A+A_588_A96/table8.dat"
+        pfTf = "/home/tako/work/.database/Barklem_2016/J_A+A_588_A96/table8_T.dat"
+
+        pfTdat = pd.read_csv(pfTf, sep="\s+")
+        self.T_gQT = jnp.array(pfTdat.columns[1:].to_numpy(dtype=float)) #T for grid QT    #self.
+        self.pfdat = pd.read_csv(pff, sep="\s+", comment="#", names=pfTdat.columns)
+        self.gQT_284species = jnp.array(self.pfdat.iloc[:, 1:].to_numpy(dtype=float)) #grid Q vs T vs Species
+
+        
+
+
+        #load vald file ("Extract Stellar" request)
+        print("Reading VALD file")
+        valdd=vald3api.read_ExAll(self.vald3_file)
+        #valdd=vald3api.read_ExStellar(self.vald3_file)
+        #better to use .feather format? #\\\\
+        #valdd.to_feather(self.vald3_file.with_suffix(".feather"))
+        
+        #compute additional transition parameters
+        self._A, self.nu_lines, self._elower, self._gupper, self._jlower, self._jupper, self._ielem, self._iion, self._vdWdamp, self._gamRad = vald3api.pickup_param(valdd)
+        
+        
+        
+        self.Tref=296.0 #\\\\
+        self.QTref_284 = np.array(self.QT_interp_284(self.Tref))
+        self.QTmask = self.make_QTmask(valdd) #identify species for each line
+
+
+
+        ##Line strength: input shoud be ndarray not jnp array
+        self.Sij0 = vald3.Sij0(self._A, self._gupper, self.nu_lines, self._elower, self.QTref_284, self.QTmask)
+
+
+
+        ### MASKING ###
+        mask=(self.nu_lines>self.nurange[0]-self.margin)\
+        *(self.nu_lines<self.nurange[1]+self.margin)\
+        *(self.Sij0>self.crit)
+        
+        self.masking(mask)
+        
+
+
+        #Compile atomic-specific data for each line
+        ipccf = "/home/tako/ghR/exojax_0/src/exojax/spec/ipcc_Asplund2009_pre.dat" #tako210804
+        ipccc = ('ielem', 'ionizationE1', 'dam1', 'dam2', 'solarA', 'mass', 'ionizationE2')
+        self.ipccd = pd.read_csv(ipccf, sep="\s+", skiprows=1, usecols=[1,2,3,4,5,6,7], names=ipccc)
+        
+        ionE = jnp.array(list(map(lambda x: self.ipccd[self.ipccd['ielem']==x].iat[0, 1], self.ielem)))
+        ionE2 = jnp.array(list(map(lambda x: self.ipccd[self.ipccd['ielem']==x].iat[0, 6], self.ielem)))
+        self.ionE = ionE * np.where(self.iion==1, 1, 0) + ionE2 * np.where(self.iion==2, 1, 0)
+        self.solarA = jnp.array(list(map(lambda x: self.ipccd[self.ipccd['ielem']==x].iat[0, 4], self.ielem)))
+        self.atomicmass = jnp.array(list(map(lambda x: self.ipccd[self.ipccd['ielem']==x].iat[0, 5], self.ielem)))
+        
+            
+    #End of the CONSTRUCTOR definition ↑
+
+
+
+    #Defining METHODS ↓
+    
+    
+    def masking(self,mask):
+        """applying mask and (re)generate jnp.arrays
+        
+        Args:
+           mask: mask to be applied. self.mask is updated.
+
+        Note:
+           We have nd arrays and jnp arrays. We apply the mask to nd arrays and generate jnp array from the corresponding nd array. For instance, self._A is nd array and self.A is jnp array.
+
+        """
+        #numpy float 64 Do not convert them jnp array
+        self.nu_lines = self.nu_lines[mask]
+        self.Sij0 = self.Sij0[mask]
+        self._A=self._A[mask]
+        self._elower=self._elower[mask]
+        self._gupper=self._gupper[mask]
+        self._jlower=self._jlower[mask]
+        self._jupper=self._jupper[mask]
+        self.QTmask=self.QTmask[mask]
+        self._ielem=self._ielem[mask]
+        self._iion=self._iion[mask]
+        self._vdWdamp=self._vdWdamp[mask]
+        self._gamRad=self._gamRad[mask]
+        
+        #jnp arrays
+        self.dev_nu_lines=jnp.array(self.nu_lines)
+        self.logsij0=jnp.array(np.log(self.Sij0))
+        self.A=jnp.array(self._A)
+        self.gamma_natural=vald3.gamma_natural(self.A) #gamma_natural [cm-1] #natural broadeningの原理はmoleculeと同様のはず(tako210802)
+        self.elower=jnp.array(self._elower)
+        self.gupper=jnp.array(self._gupper)
+        self.jlower=jnp.array(self._jlower,dtype=int)
+        self.jupper=jnp.array(self._jupper,dtype=int)
+        
+        self.QTmask=jnp.array(self.QTmask,dtype=int)
+        self.ielem=jnp.array(self._ielem,dtype=int)
+        self.iion=jnp.array(self._iion,dtype=int)
+        self.vdWdamp=jnp.array(self._vdWdamp)
+        self.gamRad=jnp.array(self._gamRad)
+
+    def Atomic_gQT(self, atomspecies):
+        """Select grid of partition function especially for the species of interest
+        
+        Args:
+            atomspecies: species e.g., "Fe 1"
+            
+        Returns:
+            gQT: grid Q(T) for the species
+        
+        """
+        if len(atomspecies.split(' '))==2:
+            atomspecies_Roman = atomspecies.split(' ')[0] + '_' + 'I'*int(atomspecies.split(' ')[-1])
+        gQT = self.gQT_284species[ np.where(self.pfdat['T[K]']==atomspecies_Roman) ][0]
+        return gQT
+    
+    def QT_interp(self, atomspecies, T):
+        """interpolated partition function
+
+        Args:
+           T: temperature
+           atomspecies: species e.g., "Fe 1"
+
+        Returns:
+           Q(T): interpolated in jnp.array for the Atomic Species
+
+        """
+        gQT = self.Atomic_gQT(atomspecies)
+        return jnp.interp(T, self.T_gQT, gQT)
+
+    def qr_interp(self, atomspecies, T):
+        """interpolated partition function ratio
+
+        Args:
+           T: temperature
+           atomspecies: species e.g., "Fe 1"
+
+        Returns:
+           qr(T)=Q(T)/Q(Tref): interpolated in jnp.array
+
+        """
+        return self.QT_interp(atomspecies,T)/self.QT_interp(atomspecies,self.Tref)
+
+    def QT_interp_284(self, T):
+        """interpolated partition function of all 284 species
+
+        Args:
+           T: temperature
+
+        Returns:
+           Q(T)*284: interpolated in jnp.array for all 284 Atomic Species
+
+        """
+        #self.T_gQT.shape -> (42,)
+        #self.gQT_284species.shape -> (284, 42)
+        list_gQT_eachspecies = self.gQT_284species.tolist()
+        listofDA_gQT_eachspecies = list(map(lambda x: jnp.array(x), list_gQT_eachspecies))
+        listofQT = list(map(lambda x: jnp.interp(T, self.T_gQT, x), listofDA_gQT_eachspecies))
+        QT_284 = jnp.array(listofQT)
+        return QT_284
+
+    def make_QTmask(self, ExAll): #identify species for each line
+        """Make array for assigning identifier of Q(Tref) to each line
+        
+        Input: ExAll: Dataframe outputted from read_ExAll
+        """
+        QTmask_sp = np.zeros(len(ExAll))
+        for i, sp in enumerate(ExAll['species']):
+            sp_Roman = sp.strip("'").split(' ')[0] + '_' + 'I'*int(sp.strip("'").split(' ')[-1])
+            QTmask_sp[i] = np.where(self.pfdat['T[K]']==sp_Roman)[0][0]
+
+        QTmask_sp = QTmask_sp.astype('int')
+        return(QTmask_sp)
