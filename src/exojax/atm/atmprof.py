2c2b777fc0b7ebb8ee03357b0f019661e587baf1
Hajime Kawahara <divrot@gmail.com>
2021-08-26 11:41:35 +0900

    added Guillot model

--- src/exojax/atm/atmprof.py
+++ src/exojax/atm/atmprof.py
@@ -20,3 +20,60 @@ def Hatm(g,T,mu):
     """
     
     return kB*T/(m_u*mu*g)
+
+def atmprof_gray(Parr,g,kappa,Tint):
+    """
+
+    Args:
+        Parr: pressure array (bar)
+        g: gravity (cm/s2)
+        kappa: infrared opacity 
+        Tint: temperature equivalence of the intrinsic energy flow
+    
+    """
+
+    tau = Parr*1.e6*kappa/g
+    Tarr = (0.75*Tint**4*(2.0/3.0+tau))**0.25
+    return Tarr
+
+def atmprof_Guillot(Parr,g,kappa,Tint,Tirr,f=0.25):
+    """
+
+    Notes:
+        Guillot (2010) Equation (29)
+
+    Args:
+        Parr: pressure array (bar)
+        g: gravity (cm/s2)
+        kappa: thermal/IR opacity (kappa_th in Guillot 2010)
+        Tint: temperature equivalence of the intrinsic energy flow
+        Tirr: temperature equivalence of the irradiation
+        f: 
+
+    Returns:
+        Tarr
+    
+    """
+    tau = Parr*1.e6*kappa/g #Equation (51)
+    invsq3=1.0/jnp.sqrt(3.0)
+    fac=2.0/3.0 + invsq3*(1.0/gamma + (gamma - 1.0/gamma)*jnp.exp(-gamma*tau*sqrt3))
+    Tarr = (0.75*Tint**4*(2.0/3.0+tau) + 0.75*Tirr**4*f*fac)**0.25
+
+    return Tarr
+
+
+def get_Tirr(Teq,Tint):
+    """Tirr from equilibrium temperature and intrinsic temperature
+
+    Args:
+       Teq: radiative equilibrium temperature
+       Tint: intrinsic temperature
+
+    Return:
+       Tirr: iradiation temperature
+    
+    Note:
+       Here we assume A=0 (albedo) and beta=1 (fully-energy distributed)
+
+    """
+    return (4.0*Teq**4 - Tint**4)**0.25
