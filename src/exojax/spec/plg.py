4d74cc2bc4e31dedcd9b9bcabbd5efd534263ad8
Hajime Kawahara <divrot@gmail.com>
2021-09-11 13:16:32 +0900

    new plg_exomol

--- src/exojax/spec/plg.py
+++ src/exojax/spec/plg.py
@@ -4,17 +4,30 @@
 import numpy as np
 from exojax.spec.dit import npgetix
 
-def plg_exomol(logsij0,elower,Nelower=10):
+
+
+
+def plg_exomol(cnu,indexnu,logsij0,elower,elower_grid=None,Nelower=10):
 #(nu_grid,nu_lines,elower,alpha_ref,n_Texp,Nlimit=30):
     """PLG for exomol
 
     """
-    get_qlogsij0(logsij0,elower,Nelower=10)
-    return None
+    
+    kT0=10000.0
+    expme=np.exp(-elower/kT0)
+    if elower_grid is None:
+        margin=1.0
+        min_expme=np.min(expme)*np.exp(-margin/kT0)
+        max_expme=np.max(expme)*np.exp(margin/kT0)
+        expme_grid=np.linspace(min_expme,max_expme,Nelower)
+        elower_grid=-np.log(expme_grid)*kT0
+    else:
+        expme_grid=np.exp(-elower_grid/kT0)
+    qlogsij0,qcnu,frozen_mask=get_qlogsij0(cnu,indexnu,logsij0,expme,expme_grid,Nelower=10)
+    
+    return qlogsij0,qcnu,elower_grid,frozen_mask
 
-
-
-def get_qlogsij0(logsij0,elower,Nelower=10,Ncrit=10):
+def get_qlogsij0(cnu,indexnu,logsij0,expme,expme_grid,Nelower=10,Ncrit=10):
     """gether (freeze) lines 
 
     Args:
@@ -22,47 +35,124 @@ def get_qlogsij0(logsij0,elower,Nelower=10,Ncrit=10):
        Ncrit: if # of lines in indices exceeds Ncrit
 
     """
+
     
-    kT0=10000.0    
+    cont,index=npgetix(expme,expme_grid) #elower contribution and elower index of lines
+    m=np.max(index)+2
+    eindex=index+m*indexnu #extended index
+    num_unique=np.bincount(eindex)
+    Sij=np.exp(logsij0)
+
+    #qlogsij0
+    qlogsij0=np.append(np.bincount(eindex,weights=Sij*(1.0-cont)),0.0)
+    qlogsij0=(qlogsij0+np.bincount(eindex+1,weights=Sij*cont))
+    qlogsij0=np.log(qlogsij0)
+
+    #qcnu
+    qcnu_den=np.append(np.bincount(eindex,weights=Sij),0.0)
+    qcnu_den=qcnu_den+np.bincount(eindex+1,weights=Sij)
+    qcnu_num=np.append(np.bincount(eindex,weights=Sij*cnu),0.0)
+    qcnu_num=qcnu_num+np.bincount(eindex+1,weights=Sij*cnu)
+    qcnu=qcnu_num/qcnu_den
+    
+
+    frozen_mask=0.0
+    qlogsij0=qlogsij0.reshape((int(len(qlogsij0)/m),m))
+    return qlogsij0,qcnu,frozen_mask
+
+
+def plg_exomol_slow(cnu,indexnu,logsij0,elower,elower_grid=None,Nelower=10):
+#(nu_grid,nu_lines,elower,alpha_ref,n_Texp,Nlimit=30):
+    """PLG for exomol
+
+    """
+    
+    kT0=10000.0
     expme=np.exp(-elower/kT0)
-    margin=1.0
-    min_expme=np.min(expme)*np.exp(-margin/kT0)
-    max_expme=np.max(expme)*np.exp(margin/kT0)
-    expme_grid=np.linspace(min_expme,max_expme,Nelower)
-    elower_grid=-np.log(expme_grid)*kT0
-    cont,index=npgetix(expme,expme_grid)
-    
-    unindex,numunique=np.unique(index,return_counts=True)
-    mask=numunique>=Ncrit
-    
-    frozen_index=unindex[mask] #frozen index
-    plg_index=np.sort(np.unique(np.concatenate([frozen_index,frozen_index+1])))
-    Ng=len(plg_index)
-    range_index=np.array(range(0,Ng),dtype=int)
-    qsij0=np.zeros(Ng,dtype=np.float64)
+    if elower_grid is None:
+        margin=1.0
+        min_expme=np.min(expme)*np.exp(-margin/kT0)
+        max_expme=np.max(expme)*np.exp(margin/kT0)
+        expme_grid=np.linspace(min_expme,max_expme,Nelower)
+        elower_grid=-np.log(expme_grid)*kT0
+    else:
+        expme_grid=np.exp(-elower_grid/kT0)
 
-    frozen_mask=np.zeros(len(logsij0),dtype=bool)
-    for fi in frozen_index:
-        mask=(index==fi)
-        frozen_mask[mask]=True
-        j=range_index[plg_index==fi][0]
-        qsij0[j]=qsij0[j]+np.sum((1.0-cont[mask])*np.exp(logsij0[mask]))
-        mask=(index==fi+1)
-        k=range_index[plg_index==fi+1][0]
-        qsij0[k]=qsij0[k]+np.sum(cont[mask]*np.exp(logsij0[mask]))
+    qlogsij0=[]
+    qcnu=[]
+    for inu in np.unique(indexnu):
+        mask=indexnu==inu
+        cnu_=cnu[mask]
+        logsij0_=logsij0[mask]
+        expme_=expme[mask]
+        each_qlogsij0,each_qcnu,frozen_mask=get_qlogsij0_slow(cnu_,logsij0_,expme_,expme_grid,Nelower=10)
+        qlogsij0.append(each_qlogsij0)
+        qcnu.append(each_qcnu)
+    qlogsij0=np.array(qlogsij0)
+    qcnu=np.array(qcnu)
+    
+    return qlogsij0,qcnu,elower_grid,frozen_mask
 
-    qlogsij0=np.log(qsij0)
-        
-    return qlogsij0,elower_grid,frozen_mask
+def get_qlogsij0_slow(cnu,logsij0,expme,expme_grid,Nelower=10,Ncrit=10):
+    """gether (freeze) lines 
+
+    Args:
+       Nelower: # of division of elower between min to max values
+       Ncrit: if # of lines in indices exceeds Ncrit
+
+    """
+
+    
+    cont,index=npgetix(expme,expme_grid) #elower contribution and elower index of lines
+    num_unique=np.bincount(index)
+    Sij=np.exp(logsij0)
+
+    #qlogsij0
+    qlogsij0=np.append(np.bincount(index,weights=Sij*(1.0-cont)),0.0)
+    qlogsij0=(qlogsij0+np.bincount(index+1,weights=Sij*cont))
+    qlogsij0=np.log(qlogsij0)
+
+    #qcnu
+    qcnu_den=np.append(np.bincount(index,weights=Sij),0.0)
+    qcnu_den=qcnu_den+np.bincount(index+1,weights=Sij)
+    qcnu_num=np.append(np.bincount(index,weights=Sij*cnu),0.0)
+    qcnu_num=qcnu_num+np.bincount(index+1,weights=Sij*cnu)
+    qcnu=qcnu_num/qcnu_den
+    
+
+    frozen_mask=0.0
+    
+    return qlogsij0,qcnu,frozen_mask
+
 
 
 if __name__ == "__main__":
     import numpy as np
-    Nline=100
+    from exojax.spec.initspec import init_modit
+    import time
+    Nline=10000000
     logsij0=np.random.rand(Nline)
     elower=np.linspace(2000.0,7000.0,Nline)
-    qlogsij0,elower_grid,frozen_mask=get_qlogsij0(logsij0,elower)
-    print(qlogsij0)
-    print(elower_grid)
-    print(frozen_mask)
-    print(elower[~frozen_mask])
+    nus=np.random.rand(Nline)*10.0
+
+    #init modit
+    Nnus=10001
+    nu_grid=np.linspace(0,10,Nnus)
+    cnu,indexnu,R,pmarray=init_modit(nus,nu_grid)
+
+
+    ts=time.time()
+    qlogsij0,qcn,elower_grid,frozen_mask=plg_exomol(cnu,indexnu,logsij0,elower)
+    te=time.time()
+    #    print(qcn)
+    print(te-ts,"sec")
+
+    print("==========================")
+    if Nline<1000000:
+        ts=time.time()
+        qlogsij0_slow,qcn_slow,elower_grid,frozen_mask=plg_exomol_slow(cnu,indexnu,logsij0,elower)
+        te=time.time()
+        #    print(qcn)
+        print(te-ts,"sec")
+        print(np.sum((qlogsij0-qlogsij0_slow)**2))
+        
