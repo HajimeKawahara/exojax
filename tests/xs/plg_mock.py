348bd83fb71a8bd848923ea10ffeceee97acc29c
Hajime Kawahara <divrot@gmail.com>
2021-09-13 15:14:12 +0900

    test methane and mock checked

--- tests/xs/plg_mock.py
+++ tests/xs/plg_mock.py
@@ -11,24 +11,24 @@ from exojax.spec import plg
 
 np.random.seed(1)
 Nline=10000001
-Ngamma=20
+Ngamma=10
 
 logsij0=np.random.rand(Nline)
 elower=(np.random.rand(Nline))**0.2*5000.+2000.0
-nus=np.random.rand(Nline)*10.0
+nu_lines=np.random.rand(Nline)*10.0
 index_gamma=np.random.randint(Ngamma,size=Nline)
 
 #init modit
-Nnus=101
-nu_grid=np.linspace(0,10,Nnus)
-cnu,indexnu,R,pmarray=init_modit(nus,nu_grid)
+Nnus=1001
+nus=np.linspace(0,10,Nnus)
+cnu,indexnu,R,pmarray=init_modit(nu_lines,nus)
 
 #elower setting
-Ncrit=30
+Ncrit=3
 Nelower=11
     
 ts=time.time()
-qlogsij0,qcnu,num_unique,elower_grid=plg.plg_elower_addcon(index_gamma,Ngamma,cnu,indexnu,Nnus,logsij0,elower,Ncrit=Ncrit,Nelower=Nelower)
+qlogsij0,qcnu,num_unique,elower_grid,frozen_mask,nonzeropl_mask=plg.plg_elower_addcon(index_gamma,Ngamma,cnu,indexnu,nus,logsij0,elower,Ncrit=Ncrit,Nelower=Nelower,reshape=True)    
 te=time.time()
 print(te-ts,"sec")
 num_unique=np.array(num_unique,dtype=float)
@@ -36,10 +36,12 @@ num_unique[num_unique<Ncrit]=None
 
 fig=plt.figure(figsize=(10,3))
 ax=fig.add_subplot(211)
-c=plt.imshow(num_unique[0,:,:].T)
+c=plt.imshow(num_unique[5,:,:].T)
 plt.colorbar(c,shrink=0.2)
-ax=fig.add_subplot(212)
+ax.set_aspect(0.1/ax.get_data_ratio())
 
-c=plt.imshow(qlogsij0[0,:,:].T)
+ax=fig.add_subplot(212)
+c=plt.imshow(qlogsij0[5,:,:].T)
 plt.colorbar(c,shrink=0.2)
+ax.set_aspect(0.1/ax.get_data_ratio())
 plt.show()
