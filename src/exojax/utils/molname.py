7df3152945ab3fd3b4e90181d7f80eac982925bd
Hajime Kawahara <divrot@gmail.com>
2021-10-10 09:24:53 +0900

    added stable isotope and auto s2e for stableisotopes

--- src/exojax/utils/molname.py
+++ src/exojax/utils/molname.py
@@ -1,4 +1,5 @@
 import re
+
 def e2s(molname_exact):
     """convert the exact molname (used in ExoMol) to the simple molname
 
@@ -36,9 +37,100 @@ def e2s(molname_exact):
     except:
         print("Warning: Exact molname ",molname_exact,"cannot be converted to simple molname")
         return molname_exact
-        
+
+def split_simple(molname_simple):
+    """split simple molname
+
+    Args: 
+       molname_simple: simple molname
+
+    Return: 
+       atom list
+       number list
+
+    Example:
+
+       >>> split_simple("Fe2O3")
+       >>> (['Fe', 'O'], ['2', '3'])
+
+    """
+
+    atom_list=[]
+    num_list=[]
+    tmp=None
+    num=""
+    for ch in molname_simple:
+        if ch.isupper():
+            if tmp is not None:
+                atom_list.append(tmp)
+                num_list.append(num)
+                num=""
+            tmp=ch
+        elif ch.islower():
+            tmp=tmp+ch
+        elif ch.isdigit():
+            num=ch
+            
+    atom_list.append(tmp)                
+    num_list.append(num)
+    
+    return atom_list,num_list
+
+    
+def s2e_stable(molname_simple):
+    """convert the simple molname to the exact molname (used in ExoMol) using stable isotopes 
+
+    Args: 
+       molname_simple: simple molname
+
+    Return: 
+       exact molname
+
+
+    """
+    from exojax.utils import isotopes, isodata
+    isolist=isodata.read_mnlist()
+
+    atom_list,num_list=split_simple(molname_simple)
+    molname_exact=""
+    for j,atm in enumerate(atom_list):
+        iso=isotopes.get_stable_isotope(atm,isolist)
+        molname_exact=molname_exact+iso[0]+num_list[j]
+        if j < len(atom_list)-1:
+            molname_exact=molname_exact+"-"
+    return  molname_exact
+
 if __name__ == "__main__":
+
+    print(s2e_stable("Fe2O3"))
+    print(s2e_stable("CH4"))
+    print(s2e_stable("NaOH"))
+
     print(e2s("12C-1H4"))
     print(e2s("23Na-16O-1H"))
     print(e2s("HeH_p"))
-    print(e2s("trans-31P2-1H-2H"))#not working 
+    print(e2s("trans-31P2-1H-2H"))#not working
+    
+    EXOMOL_SIMPLE2EXACT= \
+        {\
+         "CO":"12C-16O",\
+         "OH":"16O-1H",\
+         "NH3":"14N-1H3",\
+         "NO":"14N-16O",
+         "FeH":"56Fe-1H",
+         "H2S":"1H2-32S",
+         "SiO":"28Si-16O2",
+         "CH4":"12C-1H4",
+         "HCN":"1H-12C-14N",
+         "C2H2":"12C2-1H2",
+         "TiO":"48Ti-16O",
+         "CO2":"12C-16O2",
+         "CrH":"52Cr-1H",
+         "H2O":"1H2-16O",
+         "VO":"51V-16O",
+         "CN":"12C-14N",
+         "PN":"31P-14N",
+        }
+    for i in EXOMOL_SIMPLE2EXACT:
+        print(s2e_stable(i),EXOMOL_SIMPLE2EXACT[i])
+
