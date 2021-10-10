acb4876c85fb53bb0d8173750e5b00a988524dbc
Hajime Kawahara <divrot@gmail.com>
2021-10-10 09:31:33 +0900

    test_added

--- src/exojax/utils/molname.py
+++ src/exojax/utils/molname.py
@@ -111,26 +111,4 @@ if __name__ == "__main__":
     print(e2s("HeH_p"))
     print(e2s("trans-31P2-1H-2H"))#not working
     
-    EXOMOL_SIMPLE2EXACT= \
-        {\
-         "CO":"12C-16O",\
-         "OH":"16O-1H",\
-         "NH3":"14N-1H3",\
-         "NO":"14N-16O",
-         "FeH":"56Fe-1H",
-         "H2S":"1H2-32S",
-         "SiO":"28Si-16O2",
-         "CH4":"12C-1H4",
-         "HCN":"1H-12C-14N",
-         "C2H2":"12C2-1H2",
-         "TiO":"48Ti-16O",
-         "CO2":"12C-16O2",
-         "CrH":"52Cr-1H",
-         "H2O":"1H2-16O",
-         "VO":"51V-16O",
-         "CN":"12C-14N",
-         "PN":"31P-14N",
-        }
-    for i in EXOMOL_SIMPLE2EXACT:
-        print(s2e_stable(i),EXOMOL_SIMPLE2EXACT[i])
 
