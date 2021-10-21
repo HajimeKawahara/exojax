5dcfd778a272d8b1a897ae2ea9e48c4eb74e1adf
Tako <hiro.tako.ishikawa@icloud.com>
2021-10-21 12:22:18 +0900

    testing

--- src/exojax/spec/vald3.py
+++ src/exojax/spec/vald3.py
@@ -30,9 +30,9 @@ def Sij0(A, gupper, nu_lines, elower, QTref_284, QTmask, Irwin=False):
         QTref[i] = QTref_284[mask]
         
     #Use Irwin_1981 for Fe I (mask==76)  #test211013Tako
-    if Irwin==True:
-        from exojax.spec import vald3api
-        QTref[np.where(QTmask == 76)[0][0]] = vald3api.partfn_Fe(Tref)
+    #if Irwin==True:
+    #    from exojax.spec import vald3api
+    #    QTref[np.where(QTmask == 76)[0][0]] = vald3api.partfn_Fe(Tref)
 
     S0 = -A*gupper*np.exp(-hcperk*elower/Tref)*np.expm1(-hcperk*nu_lines/Tref)\
         /(8.0*np.pi*ccgs*nu_lines**2*QTref)
