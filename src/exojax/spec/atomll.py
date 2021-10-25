4e7d62153a34bd0b94777370f84f65ad9eb40e26
Tako <hiro.tako.ishikawa@icloud.com>
2021-10-25 17:05:01 +0900

    testing

--- src/exojax/spec/atomll.py
+++ src/exojax/spec/atomll.py
@@ -1,4 +1,5 @@
 import numpy as np
+from exojax.spec import atomllapi
 #import jax.numpy as jnp
 
 def Sij0(A, gupper, nu_lines, elower, QTref_284, QTmask, Irwin=False):
@@ -31,8 +32,7 @@ def Sij0(A, gupper, nu_lines, elower, QTref_284, QTmask, Irwin=False):
         
     #Use Irwin_1981 for Fe I (mask==76)  #test211013Tako
     if Irwin==True:
-        from exojax.spec import atomllapi
-        QTref[np.where(QTmask == 76)[0][0]] = atomllapi.partfn_Fe(Tref)
+        QTref[np.where(QTmask == 76)[0]] = atomllapi.partfn_Fe(Tref)
 
     S0 = -A*gupper*np.exp(-hcperk*elower/Tref)*np.expm1(-hcperk*nu_lines/Tref)\
         /(8.0*np.pi*ccgs*nu_lines**2*QTref)
