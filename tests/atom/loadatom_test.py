7205ca99969873a6608e896e2604291ac06c90f7
Hajime Kawahara <divrot@gmail.com>
2021-11-30 08:55:51 +0900

    test done

--- tests/atom/loadatom_test.py
+++ tests/atom/loadatom_test.py
@@ -5,13 +5,16 @@
 """
 
 import pytest
-from exojax.spec.atomllapi import load_atomicdata
+from exojax.spec.atomllapi import load_atomicdata,load_pf_Barklem2016
 
 def test_loadatom():
     data=load_atomicdata()
     assert data["ionizationE1"][0]==13.595
 
-def test_
+def test_barklem():
+    data=load_pf_Barklem2016()
+    assert data[1]["1.00000e-05"][0]==2.0
     
 if __name__ == "__main__":
     test_loadatom()
+    test_barklem()
