cbd1bce67a948adfbf3d1ff3f58efb487ebfb8c0
Hajime Kawahara <divrot@gmail.com>
2021-11-30 10:22:39 +0900

    adde test for nsol

--- tests/atom/loadatom_test.py
+++ tests/atom/loadatom_test.py
@@ -1,11 +1,11 @@
 """ test for loading atomic data
     
 
-
 """
 
 import pytest
 from exojax.spec.atomllapi import load_atomicdata,load_pf_Barklem2016
+from exojax.utils.zsol import nsol
 
 def test_loadatom():
     data=load_atomicdata()
@@ -14,7 +14,12 @@ def test_loadatom():
 def test_barklem():
     data=load_pf_Barklem2016()
     assert data[1]["1.00000e-05"][0]==2.0
+
+def test_nsol():
+    nsun=nsol()
+    assert nsun["Mg"]==3.272837539539687e-05
     
 if __name__ == "__main__":
     test_loadatom()
     test_barklem()
+    test_nsol()
