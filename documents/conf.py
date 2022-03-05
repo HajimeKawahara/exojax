6d5072007f54420c9528d2ef3242c7f8877e1b47
Hajime Kawahara <divrot@gmail.com>
2022-03-05 21:22:59 +0900

    readthedocs test

--- documents/conf.py
+++ documents/conf.py
@@ -31,7 +31,7 @@ release = '1.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.ext.napoleon', 'sphinxemoji.sphinxemoji',
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinxemoji.sphinxemoji',
               ]
 
 # Add any paths that contain templates here, relative to this directory.
