c28259054ff43abf050ba876ce7d8d50db8f0c8b
Hajime Kawahara <divrot@gmail.com>
2022-04-10 09:15:00 +0900

    pytest.approx

--- tests/utils/mnlist_test.py
+++ tests/utils/mnlist_test.py
@@ -4,7 +4,7 @@ from exojax.utils.isodata import read_mnlist
 
 def test_mnlist():
     arr=read_mnlist()
-    assert arr["abundance"][0]==99.9885
+    assert arr["abundance"][0]==pytest.approx(99.9885)
 
 if __name__ == '__main__':
     test_mnlist()
