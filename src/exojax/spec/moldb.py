f80380f839026a4e943eaf2eae62a674becbf76a
Yui Kawashima <yui.kawashima.721@gmail.com>
2022-04-14 17:05:57 +0900

    added a feature to remove .bz2 for HITRAN just in case

--- src/exojax/spec/moldb.py
+++ src/exojax/spec/moldb.py
@@ -456,6 +456,9 @@ class MdbHit(object):
            extract: If True, it extracts the opacity having the wavenumber between nurange +- margin. Use when you want to reduce the memory use.
         """
         from exojax.spec.hitran import SijT
+        if ("hit" in path and path[-4:] == ".bz2"):
+            path = path[:-4]
+            print('Warning: path changed (.bz2 removed):', path)
         if ("HITEMP" in path and path[-4:] == ".par"):
             path = path + '.bz2'
             print('Warning: path changed (.bz2 added):', path)
