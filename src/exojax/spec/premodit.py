c5bb57125f3660c76533f65d81293a1a91e66d2c
Hajime Kawahara <divrot@gmail.com>
2022-04-25 11:21:51 +0900

    Elower gird

--- src/exojax/spec/premodit.py
+++ src/exojax/spec/premodit.py
@@ -1,7 +1,10 @@
 """Line profile computation using PremoDIT = Precomputation of LSD version of MODIT
 
 """
+import numpy as np
 from exojax.spec.lsd import npgetix
+from exojax.utils.constants import hcperk
+
 def check_elower_grid_density(nu_lines, elower, elower_grid, blur_scale):
     """
 
@@ -11,13 +14,34 @@ def check_elower_grid_density(nu_lines, elower, elower_grid, blur_scale):
     """
     c_elower, i_elower = npgetix(elower, elower_grid)
 
-    initial_nu_grid = determine_initial_nugrid(nu_lines, blur_scale)
-    c_nu_lines, i_nu_lines = npgetix(nu_lines, intial_nu_grid)
+    q_lines=np.log(nu_lines)
+    initial_q_grid = determine_initial_qgrid(q_lines, blur_scale)
+    c_q_lines, i_q_lines = npgetix(q_lines, intial_q_grid)
     
     
-def determine_initial_nugrid(nu_lines, blur_scale):
-    print("test")
-    
+def determine_initial_qgrid(q_lines, blur_scale):
+    """ detemine the interval of the inital q grid
+
+    Args:
+        q_lines: the line center in the form of q = ln(nu [cm-1]), 
+        blur_scale: 
+
+    """
+    print()
+
+def compute_dElower(T,interval_contrast=0.1):
+    """ compute a grid interval of Elower given the grid interval of line strength
+
+    Args: 
+        T: temperature in Kelvin
+        interval_contrast: putting c = grid_interval_line_strength, then, the contrast of line strength between the upper and lower of the grid becomes c-order of magnitude.
+
+    Returns:
+        Required dElower
+    """
+    return interval_contrast*np.log(10.0)*T/hcperk
+
+
     
 def test_elower_grid_density():
     print("test")
@@ -27,3 +51,4 @@ def test_determine_initial_nugrid():
     
 if __name__ == "__main__":
     print("premodit")
+    test_compute_dElower()
