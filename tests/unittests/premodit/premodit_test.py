bff252281f77a6afe32db2dd9e4d43ae8caf27bf
Hajime Kawahara <divrot@gmail.com>
2022-04-25 11:35:07 +0900

    Elower gird

--- tests/unittests/premodit/premodit_test.py
+++ tests/unittests/premodit/premodit_test.py
@@ -1,5 +1,11 @@
 import pytest
-from exojax.spec.premodit import compute_dElower
+from exojax.spec.premodit import compute_dElower, test_make_elower_grid()
 
 def test_compute_dElower():
     assert compute_dElower(1000.0,interval_contrast=0.1)==pytest.approx(160.03762408883165)
+
+def test_make_elower_grid():
+    maxe=12001.0
+    mine=99.01
+    eg=make_elower_grid(1000, [mine,maxe], 1.0)
+    assert eg[-1]>=maxe and eg[0]<=mine
