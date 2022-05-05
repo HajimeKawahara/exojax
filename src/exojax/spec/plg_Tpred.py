1f5766a601048e834c12c0dea9a7a5c12c048692
Tako <hiro.tako.ishikawa@icloud.com>
2022-05-05 23:04:02 +0900

    Arranging functions to optimize Tpred value used for making elower_grid

--- src/exojax/spec/plg_Tpred.py
+++ src/exojax/spec/plg_Tpred.py
@@ -86,7 +86,7 @@ def plg_elower_addcon(indexa,Na,cnu,indexnu,nu_grid,mdb,elower_grid=None,Nelower
     return qlogsij0,qcnu,num_unique,elower_grid,frozen_mask,nonzeropl_mask
 
 def get_qlogsij0_addcon(indexa,Na,cnu,indexnu,Nnugrid,mdb,expme,expme_grid, Ncrit=0, Tpred=296.):
-    """gether (freeze) lines w/ additional indexing
+    """gather (freeze) lines w/ additional indexing
 
     Args:
        indexa: the indexing of the additional condition
@@ -101,6 +101,7 @@ def get_qlogsij0_addcon(indexa,Na,cnu,indexnu,Nnugrid,mdb,expme,expme_grid, Ncri
        Tpred: guess of typical temperature in the atmosphere
 
     """
+    threshold_persist_freezing = 10000
     m=len(expme_grid)
     n=m*Nnugrid
     Ng=n*Na
@@ -119,7 +120,7 @@ def get_qlogsij0_addcon(indexa,Na,cnu,indexnu,Nnugrid,mdb,expme,expme_grid, Ncri
     SijTpred_frozen = SijT(Tpred, \
                     mdb.logsij0[frozen_mask], mdb.nu_lines[frozen_mask], mdb.elower[frozen_mask], \
                     qT=mdb.qr_interp(Tpred))
-    persist_freezing = SijTpred_frozen < max(SijTpred_frozen)/1000
+    persist_freezing = SijTpred_frozen < max(SijTpred_frozen) / threshold_persist_freezing
     index_persist_freezing = np.where(frozen_mask)[0][persist_freezing]
     frozen_mask = np.isin(np.arange(len(frozen_mask)), index_persist_freezing)
         
@@ -195,7 +196,7 @@ def plg_elower(cnu,indexnu,Nnugrid,logsij0,elower,elower_grid=None,Nelower=10,Nc
     return qlogsij0,qcnu,num_unique,elower_grid
 
 def get_qlogsij0(cnu,indexnu,Nnugrid,logsij0,expme,expme_grid,Nelower=10,Ncrit=0):
-    """gether (freeze) lines 
+    """gather (freeze) lines
 
     Args:
        cnu: contribution of wavenumber for LSD
@@ -237,6 +238,171 @@ def get_qlogsij0(cnu,indexnu,Nnugrid,logsij0,expme,expme_grid,Nelower=10,Ncrit=0
     return qlogsij0,qcnu,num_unique,frozen_mask
 
 
+def diff_frozen_vs_pseudo(coefTpred_array, Tpred):
+#if __name__ == "__main__":
+    coefTpred = coefTpred_array[0]
+    import numpy as np
+    import jax.numpy as jnp
+    from exojax.spec import moldb, contdb
+    from exojax.spec.rtransfer import nugrid
+    from exojax.spec import initspec
+    import matplotlib.pyplot as plt
+    import time
+    import tqdm
+    import pandas as pd
+    import copy
+    #    dats=pd.read_csv("Gl229B_spectrum_H2O.dat",names=("wav","flux"),delimiter="\s")
+    #    wavmic=dats["wav"].values*1.e4
+    #    ccgs=29979245800.0
+    #    flux=dats["flux"].values*ccgs
+
+    #plg_h2o_Tpred.py
+    import sys
+    sys.path.append('/home/tako/ghR/exojax/src/exojax/spec/')
+    import plg_Tpred as plg
+
+
+    #wls,wll = 15541, 15551
+    #nus, wav, reso = nugrid(wls, wll, int((wll-wls)/nugrid_res), unit="AA", xsmode="modit")
+    #nus,wav,reso = nugrid(15540.0,15560.0,2000,unit="AA",xsmode="modit")
+    #nus,wav,reso = nugrid(16300.0,16600.0,10000,unit="AA",xsmode="modit") #test220409
+
+    #mdb = moldb.MdbExomol('.database/H2O/1H2-16O/POKAZATEL/', nus, crit=1.e-40) #, Ttyp=4000)
+    #mdb_orig = copy.deepcopy(mdb)
+    mdb = copy.deepcopy(mdb_orig)
+    print("Nline=",len(mdb.A))
+    cdbH2H2 = contdb.CdbCIA('.database/H2-H2_2011.cia', nus)
+
+    cnu, indexnu, R, pmarray = initspec.init_modit(mdb.nu_lines, nus)
+    cnu_orig = copy.deepcopy(cnu)
+    indexnu_orig = copy.deepcopy(indexnu)
+
+
+
+
+    #make index_gamma
+    gammaL_set=mdb.alpha_ref+mdb.n_Texp*(1j) #complex value
+    gammaL_set_unique=np.unique(gammaL_set)
+    Ngamma=np.shape(gammaL_set_unique)[0]
+    index_gamma=np.zeros_like(mdb.alpha_ref,dtype=int)
+    alpha_ref_grid=gammaL_set_unique.real
+    n_Texp_grid=gammaL_set_unique.imag
+    for j,a in tqdm.tqdm(enumerate(gammaL_set_unique)):
+        index_gamma=np.where(gammaL_set==a,j,index_gamma)
+    print("done.")
+    #-------------------------------------------------------
+
+    reshape=False
+    ts=time.time()
+    qlogsij0,qcnu,num_unique,elower_grid,frozen_mask,nonzeropl_mask=plg.plg_elower_addcon(index_gamma,Ngamma,cnu,indexnu,nus,
+                                                                                              mdb,Ncrit=Ncrit,Nelower=Nelower,reshape=reshape,
+                                                                                              Tpred=Tpred, coefTpred=coefTpred)
+    te=time.time()
+    print(te-ts,"sec")
+    print("elower_grid",elower_grid)
+
+    if reshape:
+        num_unique=np.array(num_unique,dtype=float)
+        num_unique[num_unique<Ncrit]=None
+        fig=plt.figure(figsize=(10,4.5))
+        ax=fig.add_subplot(311)
+        c=plt.imshow(num_unique[0,:,:].T)
+        #    c=plt.imshow(np.sum(num_unique[:,:,:],axis=0).T)
+        plt.colorbar(c,shrink=0.2)
+        ax.set_aspect(0.1/ax.get_data_ratio())
+        ax=fig.add_subplot(312)
+        c=plt.imshow(qlogsij0[0,:,:].T)
+        plt.colorbar(c,shrink=0.2)
+        ax.set_aspect(0.1/ax.get_data_ratio())
+        plt.show()
+        import sys
+        sys.exit()
+    Nnugrid=len(nus)
+
+    mdb, cnu, indexnu=plg.gather_lines(mdb,Ngamma,Nnugrid,Nelower,nus,cnu,indexnu,qlogsij0,qcnu,elower_grid,alpha_ref_grid,n_Texp_grid,frozen_mask,nonzeropl_mask)
+
+    ##
+    from exojax.spec import rtransfer as rt
+    from exojax.spec import dit, modit
+    from exojax.spec.rtransfer import rtrun, dtauM, dtauCIA, nugrid
+    from exojax.spec import planck, response
+    from exojax.spec import molinfo
+
+
+    #atm
+    NP=100
+    Parr, dParr, k=rt.pressure_layer(NP=NP)
+    molmassH2O=molinfo.molmass("H2O")
+    mmw=2.33 #mean molecular weight
+    mmrH2=0.74
+    molmassH2=molinfo.molmass("H2")
+    vmrH2=(mmrH2*mmw/molmassH2) #VMR
+
+    Pref=1.0 #bar
+    ONEARR=np.ones_like(Parr)
+
+
+    # Precomputing gdm_ngammaL
+    from exojax.spec.modit import setdgm_exomol
+    from jax import jit, vmap
+
+
+    fT = lambda T0,alpha: T0[:,None]*(Parr[None,:]/Pref)**alpha[:,None]
+    T0_test=np.array([Tpred-300.,Tpred+300.,Tpred-300.,Tpred+300.])
+    alpha_test=np.array([0.2,0.2,0.05,0.05])
+    res=0.2
+    dgm_ngammaL=setdgm_exomol(mdb,fT,Parr,R,molmassH2O,res,T0_test,alpha_test)
+    dgm_ngammaL_orig = setdgm_exomol(mdb_orig, fT, Parr, R, molmassH2O, res, T0_test, alpha_test)
+
+    u1=0.0
+    u2=0.0
+    vsini=5.0
+
+    def frun(Tarr,MMR_,Mp,Rp,u1,u2,RV,vsini):
+        g=2478.57730044555*Mp/Rp**2
+        SijM_,ngammaLM_,nsigmaDl_=modit.exomol(mdb,Tarr,Parr,R,molmassH2O)
+        xsm_=modit.xsmatrix(cnu,indexnu,R,pmarray,nsigmaDl_,ngammaLM_,SijM_,nus,dgm_ngammaL)
+        #abs is used to remove negative values in xsv
+        dtaum=dtauM(dParr,jnp.abs(xsm_),MMR_*ONEARR,molmassH2O,g)
+        #CIA
+        dtaucH2H2=dtauCIA(nus,Tarr,Parr,dParr,vmrH2,vmrH2,mmw,g,cdbH2H2.nucia,cdbH2H2.tcia,cdbH2H2.logac)
+        dtau=dtaum+dtaucH2H2
+        sourcef = planck.piBarr(Tarr,nus)
+        F0=rtrun(dtau,sourcef)
+        Frot=response.rigidrot(nus,F0,vsini,u1,u2)
+        #mu=response.ipgauss_sampling(nusd,nus,Frot,beta_inst,RV)
+        mu=Frot
+        return mu
+    def frun_orig(Tarr, MMR_, Mp, Rp, u1, u2, RV, vsini):
+        g = 2478.57730044555*Mp/Rp**2
+        SijM_, ngammaLM_, nsigmaDl_ = modit.exomol(mdb_orig, Tarr, Parr, R, molmassH2O)
+        xsm_ = modit.xsmatrix(cnu_orig, indexnu_orig, R, pmarray, nsigmaDl_, ngammaLM_, SijM_, nus, dgm_ngammaL_orig)
+        #abs is used to remove negative values in xsv
+        dtaum = dtauM(dParr, jnp.abs(xsm_), MMR_*ONEARR, molmassH2O, g)
+        #CIA
+        dtaucH2H2 = dtauCIA(nus, Tarr, Parr, dParr, vmrH2, vmrH2, mmw, g, cdbH2H2.nucia, cdbH2H2.tcia, cdbH2H2.logac)
+        dtau = dtaum+dtaucH2H2
+        sourcef  =  planck.piBarr(Tarr, nus)
+        F0 = rtrun(dtau, sourcef)
+        Frot = response.rigidrot(nus, F0, vsini, u1, u2)
+        #mu = response.ipgauss_sampling(nusd, nus, Frot, beta_inst, RV)
+        mu = Frot
+        return mu
+        
+    Tarr = Tpred*(Parr/Pref)**0.1
+    mu=frun(Tarr,MMR_=0.0059,Mp=33.2,Rp=0.88,u1=0.0,u2=0.0,RV=10.0,vsini=0.1)
+    mu_orig = frun_orig(Tarr,MMR_=0.0059,Mp=33.2,Rp=0.88,u1=0.0,u2=0.0,RV=10.0,vsini=0.1)
+                
+        return abs(np.mean(mu - mu_orig))
+        
+        
+def optimize_coefTpred(Tpred):
+    import scipy.optimize as opt
+    coefTpred_opt = opt.least_squares(\
+    fun=sct.diff_frozen_pseudo, x0=[1.,], bounds=(0.5, 1.5), args=[Tpred,])
+    return coefTpred_opt.x[0]
+
+    
 def gather_lines(mdb,Na,Nnugrid,Nelower,nu_grid,cnu,indexnu,qlogsij0,qcnu,elower_grid,alpha_ref_grid,n_Texp_grid,frozen_mask,nonzeropl_mask):
     """gather pseudo lines and unfrozen lines into lines for exomol
 
