b5eb9ffed61ce8042e4868ca94f08e826f940383
Hajime Kawahara <divrot@gmail.com>
2022-05-21 11:28:39 +0900

    compare (3D) done

--- src/exojax/spec/premodit.py
+++ src/exojax/spec/premodit.py
@@ -168,7 +168,8 @@ def unbiased_lsd_lowpass(FT_Slsd_biased,T,nu_grid,elower_grid, qr):
     eunbias_Slsd = jnp.fft.irfft(eunbias_FTSbuf)
     return g_bias(nu_grid,T)*eunbias_Slsd/qr(T)
 
-def compare_with_direct3d(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0):
+
+def compare_with_direct3d_(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0):
     """ compare the premodit LSD with the direct computation of LSD
 
     """
@@ -180,7 +181,7 @@ def compare_with_direct3d(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0):
     elower_grid=make_elower_grid(Ttyp, mdb._elower, interval_contrast=interval_contrast)
     lbd=make_lbd3D_uniqidx(mdb.Sij0, mdb.nu_lines, nus, mdb._elower, elower_grid, uidx_broadpar, Ttyp)
     Slsd=unbiased_lsd(lbd,Ttest,nus,elower_grid,mdb.qr_interp)
-
+    Slsd=np.sum(Slsd,axis=1)
     
     cont_inilsd_nu, index_inilsd_nu = npgetix(mdb.nu_lines, nus)
     qT = mdb.qr_interp(Ttest)
@@ -192,7 +193,6 @@ def compare_with_direct3d(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0):
     print("max deviation=",np.max(np.abs(Slsd/Slsd_direct-1.0)))
     return Slsd, Slsd_direct
 
-
 if __name__ == "__main__":
     import jax.numpy as jnp
     from exojax.spec import moldb
@@ -202,24 +202,7 @@ if __name__ == "__main__":
     nus=np.logspace(np.log10(6020.0), np.log10(6080.0), 40000, dtype=np.float64)
     mdb = moldb.MdbExomol('.database/CH4/12C-1H4/YT10to10/', nus, gpu_transfer=False)
 
-    from exojax.spec.lsd import uniqidx_2D
-    broadpar=np.array([mdb._n_Texp,mdb._alpha_ref]).T
-    uidx_broadpar=uniqidx_2D(broadpar)
-
-    Ttyp=2000.0
-    interval_contrast=0.1
-    elower_grid=make_elower_grid(Ttyp, mdb._elower, interval_contrast=interval_contrast)
-    lbd=make_lbd3D_uniqidx(mdb.Sij0, mdb.nu_lines, nus, mdb._elower, elower_grid, uidx_broadpar, Ttyp)
-    Ttest=1000.0
-
-    print(np.shape(lbd))
-    Slsd=unbiased_lsd(lbd,Ttest,nus,elower_grid,mdb.qr_interp)
-    print(np.shape(Slsd))
-    import sys
-    sys.exit()
-
-    
-    Slsd,Slsd_direct=compare_with_direct2D(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0)    
+    Slsd,Slsd_direct=compare_with_direct3d(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0)    
 
     fig=plt.figure()
     ax=fig.add_subplot(211)
--- tests/manualtest/premodit/compare_with_direct3D.py
+++ tests/manualtest/premodit/compare_with_direct3D.py
@@ -3,17 +3,20 @@
 """
 import numpy as np
 
-def compare_with_direct2D(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0):
-    """ compare the premodit LSD with the direct computation of LSD
+def compare_with_direct3D(mdb,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0):
+    """ compare the premodit LSD with the direct computation of LSD 3D version
 
     """
-    from exojax.spec.lsd import npadd1D, npgetix
-    from exojax.spec.hitran import SijT        
-    from exojax.spec.premodit import make_elower_grid, make_lbd2D, unbiased_lsd
+    from exojax.spec.lsd import npadd1D, npgetix, uniqidx_2D
+    from exojax.spec.hitran import SijT
+    from exojax.spec.premodit import make_elower_grid, make_lbd3D_uniqidx, unbiased_lsd
 
+    broadpar=np.array([mdb._n_Texp,mdb._alpha_ref]).T
+    uidx_broadpar=uniqidx_2D(broadpar)
     elower_grid=make_elower_grid(Ttyp, mdb._elower, interval_contrast=interval_contrast)
-    lbd=make_lbd2D(mdb.Sij0, mdb.nu_lines, nus, mdb._elower, elower_grid, Ttyp)    
+    lbd=make_lbd3D_uniqidx(mdb.Sij0, mdb.nu_lines, nus, mdb._elower, elower_grid, uidx_broadpar, Ttyp)
     Slsd=unbiased_lsd(lbd,Ttest,nus,elower_grid,mdb.qr_interp)
+    Slsd=np.sum(Slsd,axis=1)
     
     cont_inilsd_nu, index_inilsd_nu = npgetix(mdb.nu_lines, nus)
     qT = mdb.qr_interp(Ttest)
@@ -32,7 +35,7 @@ if __name__ == "__main__":
 
     nus=np.logspace(np.log10(6020.0), np.log10(6080.0), 40000, dtype=np.float64)
     mdbCH4 = moldb.MdbExomol('.database/CH4/12C-1H4/YT10to10/', nus, gpu_transfer=False)
-    Slsd,Slsd_direct=compare_with_direct2D(mdbCH4,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0)    
+    Slsd,Slsd_direct=compare_with_direct3D(mdbCH4,Ttest=1000.0,interval_contrast=0.1,Ttyp=2000.0)    
 
     fig=plt.figure()
     ax=fig.add_subplot(211)
