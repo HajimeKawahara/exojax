afa25d1fbb7ee6aedf00705470b5b8b081011dc7
Hajime Kawahara <divrot@gmail.com>
2022-05-29 08:32:54 +0900

    added a unittest for setrt.gen_wavenumber_grid (successor of nugrid)

--- src/exojax/test/generate.py
+++ src/exojax/test/generate.py
@@ -2,12 +2,12 @@
 
 """
 from exojax.spec import moldb
-from exojax.spec.rtransfer import rtrun, dtauM, dtauCIA, gen_wavenumber_grid
+from exojax.spec.rtransfer import rtrun, dtauM, dtauCIA
+from exojax.spec.setrt import gen_wavenumber_grid
 
 def gendata_exomol():
     Nx=1500
     nus, wav, res = gen_wavenumber_grid(29200.0,30000., Nx, unit='AA')
-    nus, wav, res = nugrid(29200.0,30000., Nx, unit='AA')
 
     mdb = moldb.MdbExomol('.database/CO/12C-16O/Li2015', nus, crit=1.e-30)
     print(len(mdb.A))
