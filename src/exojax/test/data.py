61565bca21af18df26ce86c21752cdb2bc769bd6
Hajime Kawahara <divrot@gmail.com>
2022-05-29 09:42:03 +0900

    clean manualtest

--- src/exojax/test/data.py
+++ src/exojax/test/data.py
@@ -5,3 +5,4 @@
 """
 
 TESTDATA_moldb_CO_EXOMOL="moldb_co_exomol.pickle"
+TESTDATA_moldb_CH4_EXOMOL_NOGPU="moldb_ch4_exomol_nogpu.pickle"
--- src/exojax/test/generate.py
+++ src/exojax/test/generate.py
@@ -4,16 +4,18 @@
 from exojax.spec import moldb
 from exojax.spec.setrt import gen_wavenumber_grid
 import pickle
+import numpy as np
 
 def gendata_moldb_exomol():
     """generate test data for CO exomol
     """
     from exojax.test.data import TESTDATA_moldb_CO_EXOMOL as filename
-    Nx=1500
+    Nx=10000
     nus, wav, res = gen_wavenumber_grid(22920.0,24000.0, Nx, unit='AA')
-    mdb = moldb.MdbExomol('.database/CO/12C-16O/Li2015', nus, crit=1e-35, Ttyp=296.0)
+    mdbCO = moldb.MdbExomol('.database/CO/12C-16O/Li2015', nus, crit=1e-35, Ttyp=296.0)
     with open(filename, 'wb') as f:
-        pickle.dump(mdb, f)
+        pickle.dump(mdbCO, f)
+
         
 if __name__ == "__main__":
     gendata_moldb_exomol()
