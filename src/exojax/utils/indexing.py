8b2a6031d96b77e104fb207f29c8cc9cdd6beb14
Hajime Kawahara <divrot@gmail.com>
2022-06-02 14:05:44 +0900

    find_or_add_index

--- src/exojax/utils/indexing.py
+++ src/exojax/utils/indexing.py
@@ -1,11 +1,38 @@
 import numpy as np
 import tqdm
 
-def uniqidx_2D(a):
-    """ compute indices based on uniq values of the input array (2D).                                                   
+
+def uniqidx(input_array):
+    """ compute indices based on uniq values of the input M-dimensional array.                                                   
                                                                                                                         
     Args:                                                                                                               
-        a: input array (N,M), will use unique M-dim vectors                                                             
+        input_array: input array (N,M), will use unique M-dim vectors                                                             
+                                                                                                                        
+    Returns:                                                                                                            
+        unique index, unique value                                                                                      
+                                                                                                                        
+    Examples:                                                                                                           
+                                                                                                                        
+        >>> a=np.array([[4,1],[7,1],[7,2],[7,1],[8,0],[4,1]])                                                           
+        >>> uidx, uval=uniqidx(a) #->[0,1,2,1,3,0], [[4,1],[7,1],[7,2],[8,0]]                                        
+                                                                                                                        
+    """
+    N, _ = np.shape(input_array)
+    uniqvals = np.unique(input_array, axis=0)
+    uidx = np.zeros(N, dtype=int)
+    uidx_p = np.where(input_array == uniqvals[0], True, False)
+    uidx[np.array(np.prod(uidx_p, axis=1), dtype=bool)] = 0
+    for i, uv in enumerate(tqdm.tqdm(uniqvals[1:], desc="uniqidx")):
+        uidx_p = np.where(input_array == uv, True, False)
+        uidx[np.array(np.prod(uidx_p, axis=1), dtype=bool)] = i + 1
+    return uidx, uniqvals
+
+
+def uniqidx_plus_one(index_array):
+    """ compute indices based on uniq values of the input index array and input index + one vector  
+                                                                                                                        
+    Args:                                                                                                               
+        index_array: input index array (N,M), will use unique M-dim vectors                                                             
                                                                                                                         
     Returns:                                                                                                            
         unique index, unique value                                                                                      
@@ -16,12 +43,38 @@ def uniqidx_2D(a):
         >>> uidx, uval=uniqidx_2D(a) #->[0,1,2,1,3,0], [[4,1],[7,1],[7,2],[8,0]]                                        
                                                                                                                         
     """
-    N,_=np.shape(a)
-    uniqvals=np.unique(a,axis=0)
-    uidx=np.zeros(N,dtype=int)
-    uidx_p=np.where(a==uniqvals[0], True, False)
-    uidx[np.array(np.prod(uidx_p,axis=1),dtype=bool)]=0
-    for i,uv in enumerate(tqdm.tqdm(uniqvals[1:],desc="uniqidx")):
-        uidx_p=np.where(a==uv, True, False)
-        uidx[np.array(np.prod(uidx_p,axis=1),dtype=bool)]=i+1
-    return uidx, uniqvals
+    uidx, uniqval = uniqidx(index_array)
+    uniqval_new=np.copy(uniqval)
+    Nuidx = np.max(uidx)
+    neighbor_indices=np.zeros((Nuidx,3))
+    for i in range(0, Nuidx):
+        neighbor_indices[i,0], uniqval_new = find_or_add_index(uniqval[i,:]+np.array([1,0]), uniqval_new)
+        neighbor_indices[i,1], uniqval_new = find_or_add_index(uniqval[i,:]+np.array([0,1]), uniqval_new)
+        neighbor_indices[i,2], uniqval_new = find_or_add_index(uniqval[i,:]+np.array([1,1]), uniqval_new)
+        
+    return False
+
+def find_or_add_index(new_index, index_array):
+    """find a position of a new index in index_array, if not exisited add the new index in index_array
+    
+    Args: 
+        new_index: new index investigated
+        index_array: index array
+   
+    Returns:
+        position, index_array updated
+        
+    """
+    uidx_p = np.where(index_array == new_index, True, False)
+    mask = np.array(np.prod(uidx_p, axis=1), dtype=bool)
+    ni = np.where(mask == True)[0]
+    if len(ni) == 0:
+        index_array = np.vstack([index_array,new_index])
+        return len(index_array) - 1, index_array
+    else:
+        return ni[0], index_array
+
+if __name__ == "__main__":
+    a = np.array([[4, 1], [7, 1], [7, 2], [7, 1], [8, 0], [5, 1]])
+    uniqidx_plus_one(a)
+
--- tests/unittests/utils/indexing_test.py
+++ tests/unittests/utils/indexing_test.py
@@ -1,13 +1,38 @@
-from exojax.utils.indexing import uniqidx_2D
+from exojax.utils.indexing import uniqidx, find_or_add_index
 import numpy as np
 
 def test_uniqidx_2D():
     a = np.array([[4, 1], [7, 1], [7, 2], [7, 1], [8, 0], [4, 1]])
-    uidx, val = uniqidx_2D(a)
+    uidx, val = uniqidx(a)
     ref = np.array([0, 1, 2, 1, 3, 0])
     assert np.all(uidx - ref == 0.0)
     refval = np.array([[4, 1], [7, 1], [7, 2], [8, 0]])
     assert np.all(val - refval == 0.0)
 
+def test_uniqidx_3D():
+    a = np.array([[4, 1, 2], [7, 1, 2], [7, 2, 2], [7, 1, 2], [8, 0, 2], [4, 1, 1]])
+    uidx, val = uniqidx(a)
+    ref = np.array([1, 2, 3, 2, 4, 0])
+    assert np.all(uidx - ref == 0.0)
+    refval = np.array([[4, 1, 1], [4, 1, 2], [7, 1, 2], [7, 2, 2], [8, 0, 2]])
+    assert np.all(val - refval == 0.0)
+
+def test_find_or_add_index():
+    # case for existed in index_array
+    a = np.array([[4, 1], [7, 1], [7, 2], [8, 0]])
+    index_found, b = find_or_add_index([4,1], a)
+    assert index_found == 0
+    assert np.all(a - b == 0)
+    
+    # case for not existed in index_array
+    index_found, c = find_or_add_index([4,4], a)
+    assert index_found == 4
+    ref=np.array([[4, 1], [7, 1], [7, 2], [8, 0],[4, 4]])
+    assert np.all(a - c == 0)
+
 if __name__ == "__main__":
     test_uniqidx_2D()
+    test_uniqidx_3D()
+    test_find_or_add_index()
+    
+    
