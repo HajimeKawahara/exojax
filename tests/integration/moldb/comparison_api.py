ff3f09252f9ddbf94ec97d49532b74d46856de99
Hajime Kawahara <divrot@gmail.com>
2022-07-22 11:55:53 +0900

    made a comparison test and noticed that we need to implement Ttyp stuff in api

---
+++ tests/integration/moldb/comparison_api.py
@@ -0,0 +1,13 @@
+import pytest
+from exojax.spec.api import MdbExomol as MdbExomol_api
+from exojax.spec.moldb import MdbExomol as MdbExomol_orig
+
+
+def comparison_moldb_exomol():
+    crit=1.e-30
+    mapi = MdbExomol_api(".database/CO/12C-16O/Li2015",nurange=[4200.0,4300.0],crit=crit)
+    morig = MdbExomol_orig(".database/CO/12C-16O/Li2015",nurange=[4200.0,4300.0],crit=crit)
+    print(mapi.A - morig._A)
+
+if __name__ == "__main__":
+    comparison_moldb_exomol()
