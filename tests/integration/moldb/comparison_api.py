2fc2ecc8ae244e41d26ca619f12e2eb0f47bcd63
Hajime Kawahara <divrot@gmail.com>
2022-07-28 11:10:36 +0900

    different directory for comparison of old and new moldb with each other

--- tests/integration/moldb/comparison_api.py
+++ tests/integration/moldb/comparison_api.py
@@ -5,9 +5,10 @@ from exojax.spec.moldb import MdbExomol as MdbExomol_orig
 
 def comparison_moldb_exomol():
     crit=1.e-30
+    morig = MdbExomol_orig(".database_/CO/12C-16O/Li2015",nurange=[4200.0,4300.0],crit=crit,margin=0.0)
     mapi = MdbExomol_api(".database/CO/12C-16O/Li2015",nurange=[4200.0,4300.0],crit=crit,margin=0.0)
-    morig = MdbExomol_orig(".database/CO/12C-16O/Li2015",nurange=[4200.0,4300.0],crit=crit,margin=0.0)
-    assert np.all(mapi.A - morig._A) == 0.0
+    
+    assert np.all(mapi.A - morig.A) == 0.0
     
     T=1000.0
     qr_api = mapi.qr_interp(T)
