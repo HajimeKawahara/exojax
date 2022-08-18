df74ac1a7402b16e48f5d2d6dfd68624e36c8a4a
Yui Kawashima@higuma <yui.kawashima.721@gmail.com>
2022-08-18 21:16:08 +0900

    prepared test_molmass()

--- tests/unittests/atom/molmass_test.py
+++ tests/unittests/atom/molmass_test.py
@@ -1,11 +1,16 @@
 """test for molmass calculation"""
 
+import pytest
 from exojax.spec.molinfo import molmass
 
 
-if __name__ == '__main__':
+def test_molmass():
     print(molmass('air'))
     print(molmass('CO2'))
     print(molmass('He'))
     print(molmass('CO2',db_HIT=True))
     print(molmass('He',db_HIT=True))
+
+
+if __name__ == '__main__':
+    test_molmass()
