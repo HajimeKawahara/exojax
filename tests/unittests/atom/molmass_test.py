f42ea7fae54d58ee3f683a59763013efcb6b3566
Yui Kawashima@higuma <yui.kawashima.721@gmail.com>
2022-08-18 21:20:37 +0900

    excluded print statment in tests/unittests/atom/molmass_test.py

--- tests/unittests/atom/molmass_test.py
+++ tests/unittests/atom/molmass_test.py
@@ -5,11 +5,11 @@ from exojax.spec.molinfo import molmass
 
 
 def test_molmass():
-    print(molmass('air'))
-    print(molmass('CO2'))
-    print(molmass('He'))
-    print(molmass('CO2',db_HIT=True))
-    print(molmass('He',db_HIT=True))
+    molmass('air')
+    molmass('CO2')
+    molmass('He')
+    molmass('CO2',db_HIT=True)
+    molmass('He',db_HIT=True)
 
 
 if __name__ == '__main__':
