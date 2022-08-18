40bfa2680b1b7bdaa41c59506ccd02ed7f95065a
Yui Kawashima@higuma <yui.kawashima.721@gmail.com>
2022-08-18 16:10:01 +0900

    modified to use HITRAN mass if db_HIT = True

--- src/exojax/spec/molinfo.py
+++ src/exojax/spec/molinfo.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import warnings
 EachMass = {'H': 1.00794, 'He': 4.002602, 'Li': 6.941, 'Be': 9.012182, 'B': 10.811, 'C': 12.0107, 'N': 14.0067, 'O': 15.9994, 'F': 18.9984032, 'Ne': 20.1797, 'Na': 22.98976928, 'Mg': 24.305, 'Al': 26.9815386, 'Si': 28.0855, 'P': 30.973762, 'S': 32.065, 'Cl': 35.453, 'Ar': 39.948, 'K': 39.0983, 'Ca': 40.078, 'Sc': 44.955912, 'Ti': 47.867, 'V': 50.9415, 'Cr': 51.9961, 'Mn': 54.938045, 'Fe': 55.845, 'Co': 58.933195, 'Ni': 58.6934, 'Cu': 63.546, 'Zn': 65.409, 'Ga': 69.723, 'Ge': 72.64, 'As': 74.9216, 'Se': 78.96, 'Br': 79.904, 'Kr': 83.798, 'Rb': 85.4678, 'Sr': 87.62, 'Y': 88.90585, 'Zr': 91.224, 'Nb': 92.90638, 'Mo': 95.94, 'Tc': 98.9063, 'Ru': 101.07, 'Rh': 102.9055, 'Pd': 106.42, 'Ag': 107.8682, 'Cd': 112.411, 'In': 114.818, 'Sn': 118.71, 'Sb': 121.760, 'Te': 127.6, 'I': 126.90447, 'Xe': 131.293, 'Cs': 132.9054519, 'Ba': 137.327, 'La': 138.90547, 'Ce': 140.116,
             'Pr': 140.90465, 'Nd': 144.242, 'Pm': 146.9151, 'Sm': 150.36, 'Eu': 151.964, 'Gd': 157.25, 'Tb': 158.92535, 'Dy': 162.5, 'Ho': 164.93032, 'Er': 167.259, 'Tm': 168.93421, 'Yb': 173.04, 'Lu': 174.967, 'Hf': 178.49, 'Ta': 180.9479, 'W': 183.84, 'Re': 186.207, 'Os': 190.23, 'Ir': 192.217, 'Pt': 195.084, 'Au': 196.966569, 'Hg': 200.59, 'Tl': 204.3833, 'Pb': 207.2, 'Bi': 208.9804, 'Po': 208.9824, 'At': 209.9871, 'Rn': 222.0176, 'Fr': 223.0197, 'Ra': 226.0254, 'Ac': 227.0278, 'Th': 232.03806, 'Pa': 231.03588, 'U': 238.02891, 'Np': 237.0482, 'Pu': 244.0642, 'Am': 243.0614, 'Cm': 247.0703, 'Bk': 247.0703, 'Cf': 251.0796, 'Es': 252.0829, 'Fm': 257.0951, 'Md': 258.0951, 'No': 259.1009, 'Lr': 262, 'Rf': 267, 'Db': 268, 'Sg': 271, 'Bh': 270, 'Hs': 269, 'Mt': 278, 'Ds': 281, 'Rg': 281, 'Cn': 285, 'Nh': 284, 'Fl': 289, 'Mc': 289, 'Lv': 292, 'Ts': 294, 'Og': 294}
 
 
-def molmass(molecule):
+def molmass(molecule, db_HIT=False):
     """provide molecular mass from the molecular name.
 
     Args:
        molecule: molecular name e.g. CO2, He
+       db_HIT: if True, use the molecular mass considering the abundance and mass of each isotopologues provided by HITRAN (https://hitran.org/docs/iso-meta/)
 
     Returns: 
        molecular mass
@@ -25,7 +27,7 @@ def molmass(molecule):
     """
     df = pd.read_csv("../data/atom/HITRAN_molparam.txt", sep="\s{2,}", engine="python", skiprows=1, \
                      names=["# Iso","Abundance","Q(296K)","gj","Molar Mass(g)"])
-    molmass = {}
+    molmass_hi = {}
     for i in range(len(df)):
         if("(" in df["# Iso"][i]):
             molname = df["# Iso"][i].split()[0]
@@ -36,32 +38,43 @@ def molmass(molecule):
             tot_abd = tot_abd + df["Abundance"][i]
 
         if(i == len(df)-1 or "(" in df["# Iso"][i+1]):
-            molmass[molname] = tot/tot_abd
+            molmass_hi[molname] = tot/tot_abd
+
 
     if molecule == 'air' or molecule == 'Air':
         return 28.97
 
-    em = 0.0
-    tot = 0.0
-    listmol = list(molecule)
-    ignore = False
-    for k, i in enumerate(listmol):
-        if ignore:
-            ignore = False
-        elif i.isdigit():
-            tot = tot+em*(int(i)-1)
-        else:
-            if k+1 < len(listmol):
-                if listmol[k+1].islower():
-                    em = EachMass[listmol[k]+listmol[k+1]]
-                    ignore = True
+    if molecule in molmass_hi and db_HIT:
+        molmass = molmass_hi[molecule]
+    else:
+        if(db_HIT):
+            warn_msg = "db_HIT is set as True, but the molecular name '%s' does not exist in the HITRAN database. So set db_HIT as False. For reference, all the available molecules in the HITRAN databse are as follows:" % molecule
+            warnings.warn(warn_msg, UserWarning)
+            print(list(molmass_hi.keys()))
+
+        em = 0.0
+        tot = 0.0
+        listmol = list(molecule)
+        ignore = False
+        for k, i in enumerate(listmol):
+            if ignore:
+                ignore = False
+            elif i.isdigit():
+                tot = tot+em*(int(i)-1)
+            else:
+                if k+1 < len(listmol):
+                    if listmol[k+1].islower():
+                        em = EachMass[listmol[k]+listmol[k+1]]
+                        ignore = True
+                    else:
+                        em = EachMass[i]
                 else:
                     em = EachMass[i]
-            else:
-                em = EachMass[i]
 
-            tot = tot+em
-    return tot
+                tot = tot+em
+        molmass = tot
+
+    return molmass
 
 
 if __name__ == '__main__':
@@ -69,3 +82,5 @@ if __name__ == '__main__':
     print(molmass('CO2'))
     print(molmass('He'))
     print(molmass('air'))
+    print(molmass('CO2',db_HIT=True))
+    print(molmass('He',db_HIT=True))
