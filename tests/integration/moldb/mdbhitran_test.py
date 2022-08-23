2b48b51d2b9fcc5a43ac9de551e886d7e00240a2
Hajime Kawahara <divrot@gmail.com>
2022-08-23 17:46:02 +0900

    HITRAN test completed

--- tests/integration/moldb/mdbhitran_test.py
+++ tests/integration/moldb/mdbhitran_test.py
@@ -27,10 +27,11 @@ def test_moldb_hitran_interp():
                  nurange=[4200.0, 4300.0],
                  crit=1.e-30)
     T = 1000.0
+    
     assert mdb.qr_interp(1, T) == pytest.approx(3.5526853)
     qr = mdb.qr_interp_lines(T)
     ref = [3.5402815,3.5472596,3.5526853,3.5537634,3.5603657]
-    uniq=np.unique(qr)
+    uniq=np.unique(qr)    
     assert uniq[0] == pytest.approx(ref[0])
     assert uniq[1] == pytest.approx(ref[1])
     assert uniq[2] == pytest.approx(ref[2])
@@ -42,4 +43,4 @@ if __name__ == "__main__":
     test_moldb_hitran()
     test_moldb_hitran_direct_name()
     test_moldb_hitran_direct_molecid()
-#    test_moldb_hitran_interp()
+    test_moldb_hitran_interp()
