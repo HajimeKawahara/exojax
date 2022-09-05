2c5ba275f64d942c9e155003d586cfcc7abe58fc
Hajime Kawahara <divrot@gmail.com>
2022-09-05 20:27:08 +0900

    refactored

--- src/exojax/special/erfcx.py
+++ src/exojax/special/erfcx.py
@@ -1,6 +1,5 @@
 import jax.numpy as jnp
 from jax import jit
-from jax.lax import scan
 
 
 @jit
@@ -10,17 +9,16 @@ def erfcx(x):
     Scaled complementary error function exp(-x*x) erfc(x)
 
     Args:
-         x: should be larger than -9.3
+    x: should be larger than -9.3
 
     Returns:
-         jnp.array: erfcx(x)
+        jnp.array: erfcx(x)
 
     Note:
-       We acknowledge the post in stack overflow (https://stackoverflow.com/questions/39777360/accurate-computation-of-scaled-complementary-error-function-erfcx).
+          We acknowledge the post in stack overflow 
+          (https://stackoverflow.com/questions/39777360/accurate-computation-of-scaled-complementary-error-function-erfcx).
     """
     a = jnp.abs(x)
-    #q = (-a*(a-2.0)/(a+2.0)-2.0*((a-2.0)/(a+2.0)+1.0)+a) / \
-    #    (a+2.0) + (a-2.0)/(a+2.0)
     b = (a - 2.0) / (a + 2.0)
     q = (-a * b - 2.0 * (b + 1.0) + a) / (a + 2.0) + b
     p = ((((((((((
