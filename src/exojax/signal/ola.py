692595b560c1bbc4f09af90e189971738a8e938a
Hajime Kawahara <divrot@gmail.com>
2022-09-07 14:46:58 +0900

    renamed inputs

--- src/exojax/signal/ola.py
+++ src/exojax/signal/ola.py
@@ -5,62 +5,70 @@ from jax.numpy import index_exp
 from jax.lax import dynamic_update_slice
 from jax import jit
 
+
 @jit
-def olaconv(xarr, f):
+def olaconv(reshaped_input_matrix, fir_filter):
     """Overlap and Add convolve (jax.numpy version)
 
     Args:
-        xarr (jax/ndarray): reshaped matrix of a long real vector (ndiv, L)
-        f (jax/ndarray): real FIR filter, length should be odd
+        reshaped_input_matrix (jax.ndarray): reshaped matrix of a long real input vector (ndiv, div_length)
+        fir_filter (jax.ndarray): real FIR filter. The length should be odd.
+        
+    Note:
+        ndiv is the number of the divided input vectors.
+        div_length is the length of the divided input vectors. 
         
     Returns:
-        convolved vector w/ length of (len(x) + len(f) - 1)
+        convolved vector w/ the length of (len(input) + len(f) - 1)
     """
-    ndiv, L = jnp.shape(xarr)
-    M = len(f)
-    N = L + M - 1
-    Nx = ndiv * L
-    xzeropad = jnp.zeros((ndiv, N))
-    xzeropad = xzeropad.at[index_exp[:, 0:L]].add(xarr)
-    fzeropad = jnp.zeros(N)
-    fzeropad = fzeropad.at[index_exp[0:M]].add(f)
+    ndiv, div_length = jnp.shape(reshaped_input_matrix)
+    filter_length = len(fir_filter)
+    fft_length = div_length + filter_length - 1
+    input_length = ndiv * div_length
+    xzeropad = jnp.zeros((ndiv, fft_length))
+    xzeropad = xzeropad.at[index_exp[:,
+                                     0:div_length]].add(reshaped_input_matrix)
+    fzeropad = jnp.zeros(fft_length)
+    fzeropad = fzeropad.at[index_exp[0:filter_length]].add(fir_filter)
     ftilde = jnp.fft.rfft(fzeropad)
     xtilde = jnp.fft.rfft(xzeropad, axis=1)
     ytilde = xtilde * ftilde[jnp.newaxis, :]
     ftarr = jnp.fft.irfft(ytilde, axis=1)
 
-    def f(idiv, ft):
+    def fir_filter(idiv, ft):
         idiv = idiv + 1
-        return idiv, dynamic_update_slice(jnp.zeros(Nx + M - 1), ft,
-                                          ((idiv - 1) * L, ))
+        return idiv, dynamic_update_slice(
+            jnp.zeros(input_length + filter_length - 1), ft,
+            ((idiv - 1) * div_length, ))
 
-    nscan, fftvalarr = scan(f, 0, ftarr)
+    nscan, fftvalarr = scan(fir_filter, 0, ftarr)
     return jnp.sum(fftvalarr, axis=0)
 
 
-def np_olaconv(xarr, f):
+def np_olaconv(reshaped_input_matrix, fir_filter):
     """Overlap and Add convolve (numpy version)
 
     Args:
-        xarr (ndarray): reshaped matrix of a long real vector (ndiv, L)
-        f (ndarray): real FIR filter, length should be odd
+        reshaped_input_matrix (ndarray): reshaped matrix of a long real vector (ndiv, L)
+        fir_filter (ndarray): real FIR filter, length should be odd
         
     Returns:
-        convolved vector w/ length of (len(x) + len(f) - 1)
+        convolved vector w/ length of (len(input) + len(fir_filter) - 1)
     """
-    ndiv, L = np.shape(xarr)
-    M = len(f)
-    N = L + M - 1
-    Nx = ndiv * L
-    xzeropad = np.zeros((ndiv, N))
-    xzeropad[:, 0:L] = xarr
-    fzeropad = np.zeros(N)
-    fzeropad[0:M] = f
+    ndiv, div_length = np.shape(reshaped_input_matrix)
+    filter_length = len(fir_filter)
+    fft_length = div_length + filter_length - 1
+    input_length = ndiv * div_length
+    xzeropad = np.zeros((ndiv, fft_length))
+    xzeropad[:, 0:div_length] = reshaped_input_matrix
+    fzeropad = np.zeros(fft_length)
+    fzeropad[0:filter_length] = fir_filter
     ftilde = np.fft.rfft(fzeropad)
     xtilde = np.fft.rfft(xzeropad, axis=1)
     ytilde = xtilde * ftilde[np.newaxis, :]
     ftarr = np.fft.irfft(ytilde, axis=1)
-    y = np.zeros(Nx + M - 1)
+    y = np.zeros(input_length + filter_length - 1)
     for idiv in range(ndiv):
-        y[int(idiv * L):int(idiv * L + N)] += ftarr[idiv, :]
+        y[int(idiv * div_length):int(idiv * div_length +
+                                     fft_length)] += ftarr[idiv, :]
     return y
