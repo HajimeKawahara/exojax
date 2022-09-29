048789944ac635687cfec8c55c49f1314b556ded
Hajime Kawahara <divrot@gmail.com>
2022-09-29 11:10:20 +0900

    tmp

--- tests/unittests/spec/presolar/presolar_test.py
+++ tests/unittests/spec/presolar/presolar_test.py
@@ -5,25 +5,42 @@ from exojax.utils.instfunc import resolution_eslog
 from exojax.spec.setrt import gen_wavenumber_grid
 
 
-def reshape_line_density(lbd, nu_grid, filter_length):
-    #spectral_resolution = resolution_eslog(nu_grid)
+def mini_batches_number(nu_grid, filter_length):
     input_length = len(nu_grid)
     opt_div_length = optimal_div_length(filter_length)
     n_block = int(input_length / opt_div_length) + 1
-    
-    return 
+    return n_block
+
+def reshape_line_density(lbd, nu_grid, filter_length):
+    #spectral_resolution = resolution_eslog(nu_grid)
+    n_mini_batches = mini_batches_number(nu_grid, filter_length)
+
+    return
+
+def example_filter():
+    filter_length = 50001
+    N = 2000000
+    nu_grid, wav, resolution = gen_wavenumber_grid(3000.0,
+                                                   5000.0,
+                                                   N,
+                                                   unit="cm-1",
+                                                   xsmode="presolar")
+    return filter_length, nu_grid
+
+def test_mini_batches_number():
+    filter_length, nu_grid = example_filter()
+    n_mini_batches = mini_batches_number(nu_grid, filter_length)
+    assert n_mini_batches == 3
+
 
 def test_reshape_line_density():
-
-    filter_length = 25001
-    N = 1000000
-    nu_grid = gen_wavenumber_grid(22000.0,
-                                  23000.0,
-                                  N,
-                                  unit="AA",
-                                  xsmode="premodit")
-    reshape_line_density(nu_grid, filter_length)
-
-
+    filter_length, nu_grid = example_filter()
+    n_E_h = 10
+    n_broadening_k = 19
+    lbd = np.zeros((nu_grid,n_broadening_k,n_E_h))
+    reshaped_lbd = reshape_line_density(lbd, nu_grid, filter_length)
+    
+    
 if __name__ == "__main__":
+    test_mini_batches_number()
     test_reshape_line_density()
