ef838098d6395614b67f09e634c9a32562706051
Hajime Kawahara <divrot@gmail.com>
2022-10-01 08:02:42 +0900

    lbd reshaping done

--- src/exojax/spec/presolar.py
+++ src/exojax/spec/presolar.py
@@ -4,6 +4,24 @@
 import numpy as np
 import jax.numpy as jnp
 from exojax.signal.ola import optimal_fft_length
+from exojax.signal.ola import generate_padding_matrix
+
+
+def lbd_olaform(lbd, ndiv, div_length, filter_length):
+    """convert LBD to match the form of OLA, i.e. generate hat LBD 
+
+    Args:
+        lbd (3D array): line basis density (input length,:,:)
+        ndiv (int): number of mini batches
+        div_length (int): mini batch length 
+        filter_length (int): filter length
+
+    Returns:
+        4D array: hat(LBD) (ndiv, fft_length, :, :)
+    """
+    rlbd = reshape_lbd(lbd, ndiv, div_length)
+    hat_lbd = generate_padding_matrix(-np.inf, rlbd, filter_length)
+    return hat_lbd
 
 
 def optimal_mini_batch(input_length, filter_length):
@@ -20,3 +38,29 @@ def optimal_mini_batch(input_length, filter_length):
     ndiv = int(input_length / opt_div_length) + 1
     return ndiv, opt_div_length
 
+
+def reshape_lbd(lbd, ndiv, div_length, padding_value=-np.inf):
+    """reshaping LBD (input length,:,:) to (ndiv, div_length, :, :) w/ padding_value
+    Args:
+        lbd (3D array): line basis density (input length,:,:)
+        ndiv (int): number of mini batches
+        div_length (int): mini batch length 
+        padding_value (optional): padding value. Defaults to -np.inf.
+
+    Raises:
+        ValueError: ndiv*div_length should be larger than input length = shape(lbd)[0]
+
+    Returns:
+        4D array: reshaped LBD (ndiv, div_length, :, :) w/ padding_value
+    """
+
+    input_length, n_broadening_k, n_E_h = np.shape(lbd)
+    if ndiv * div_length < input_length:
+        raise ValueError(
+            "ndiv*div_length should be larger than input length = shape(lbd)[0]"
+        )
+    residual = ndiv * div_length - input_length
+    padding_shape = (residual, n_broadening_k, n_E_h)
+    padding_matrix = np.full(padding_shape, padding_value)
+    rlbd = np.vstack((lbd, padding_matrix))
+    return rlbd.reshape((ndiv, div_length, n_broadening_k, n_E_h))
--- tests/unittests/spec/presolar/presolar_test.py
+++ tests/unittests/spec/presolar/presolar_test.py
@@ -2,6 +2,8 @@ import numpy as np
 from exojax.signal.ola import _input_length
 from exojax.spec.setrt import gen_wavenumber_grid
 from exojax.spec.presolar import optimal_mini_batch
+from exojax.spec.presolar import lbd_olaform
+from exojax.spec.presolar import reshape_lbd
 
 
 def _example_filter(N, filter_length):
@@ -39,34 +41,30 @@ def test_optimal_mini_batch():
     assert len(nu_grid) < ndiv * opt_div_length
 
 
-from exojax.signal.ola import generate_padding_matrix
-
-
-def reshape_lbd(lbd, ndiv, div_length, padding_value=-np.inf):
-    """reshaping lbd to (ndiv, div_length, :, :) array w/ padding_value
-    Args:
-        lbd (3D array): line basis density
-        ndiv (int): number of mini batches
-        div_length (int): mini batch length 
-        padding_value (optional): padding value. Defaults to -np.inf.
-
-    Raises:
-        ValueError: ndiv*div_length should be larger than input length = shape(lbd)[0]
-
-    Returns:
-        4D array: _description_
-    """
-    
-    input_length, n_broadening_k, n_E_h = np.shape(lbd)
-    if ndiv * div_length < input_length:
-        raise ValueError(
-            "ndiv*div_length should be larger than input length = shape(lbd)[0]"
-        )
+def test_lbd_olaform_simple():
+    lbd = _simple_example_lbd()
+    input_length = np.shape(lbd)[0]
+    ndiv, div_length = 3, 5
+    filter_length = 3
+    hat_lbd = lbd_olaform(lbd, ndiv, div_length, filter_length)
     residual = ndiv * div_length - input_length
-    padding_shape = (residual, n_broadening_k, n_E_h)
-    padding_matrix = np.full(padding_shape, padding_value)
-    rlbd = np.vstack((lbd, padding_matrix))
-    return rlbd.reshape((ndiv, div_length,n_broadening_k, n_E_h))
+    res = np.sum(1.0 /
+                 hat_lbd[-1, div_length -
+                         residual:, :, :])  #summation of 1/padding elements
+    assert res == 0.0
+    margin = filter_length - 1
+    res2 = np.sum(1.0 /
+                  hat_lbd[:, -margin:, :, :])  #summation of 1/padding elements
+    assert res2 == 0.0
+
+
+def test_lbd_olaform():
+    lbd, filter_length = _example_lbd()
+    input_length = np.shape(lbd)[0]
+    ndiv, div_length = optimal_mini_batch(input_length, filter_length)
+    hat_lbd = lbd_olaform(lbd, ndiv, div_length, filter_length)
+    assert np.shape(hat_lbd) == (3, 762048, 19, 10)
+
 
 def test_reshape_lbd_simple():
     lbd = _simple_example_lbd()
@@ -74,7 +72,8 @@ def test_reshape_lbd_simple():
     ndiv, div_length = 3, 5
     rlbd = reshape_lbd(lbd, ndiv, div_length)
     residual = ndiv * div_length - input_length
-    res = np.sum(1.0/rlbd[-1,div_length-residual:,:,:]) #summation of 1/padding elements
+    res = np.sum(1.0 / rlbd[-1, div_length -
+                            residual:, :, :])  #summation of 1/padding elements
     assert res == 0.0
 
 
@@ -84,23 +83,11 @@ def test_reshape_lbd():
     ndiv, div_length = optimal_mini_batch(input_length, filter_length)
     rlbd = reshape_lbd(lbd, ndiv, div_length)
     assert np.shape(rlbd) == (3, 712048, 19, 10)
-    
-
-def generate_hat_lbd(lbd, filter_length):
-    input_length = np.shape(lbd)[0]
-    ndiv, div_length = optimal_mini_batch(input_length, filter_length)
-    rlbd = reshape_lbd(lbd, ndiv, div_length)
-    hat_lbd = generate_padding_matrix(-np.inf, rlbd, filter_length)
-    return hat_lbd
-
-
-def test_generate_hat_lbd():
-    lbd = _example_lbd()
-    #hat_lbd = generate_hat_lbd(reshaped_lbd, filter_length)
 
 
 if __name__ == "__main__":
-    test_optimal_mini_batch()
-    #test_generate_hat_lbd()
-    test_reshape_lbd_simple()
-    test_reshape_lbd()
+    #test_lbd_olaform_simple()
+    test_lbd_olaform()
+    #test_optimal_mini_batch()
+    #test_reshape_lbd_simple()
+    #test_reshape_lbd()
