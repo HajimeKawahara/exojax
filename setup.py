757c2bc58c71bb2fdc7c8a31ce56f84880bcca89
Hajime Kawahara <divrot@gmail.com>
2022-10-10 21:49:37 +0900

    numpy requirement < 1.22.3 to match to radis

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
 ]
 INSTALL_REQUIRES = [
-    'numpy', 'pandas', 'tqdm', 'scipy', 'jax>=0.2.22', 'numpyro', 'pyarrow',
+    'numpy<=1.22.3', 'pandas', 'tqdm', 'scipy', 'jax>=0.2.22', 'numpyro', 'pyarrow',
     'hitran-api', 'bs4', 'vaex', 'radis', 'jaxopt'
 ]
 
