0f0a7451fd38ecc630fd3c32678764d921420f94
Hajime Kawahara <divrot@gmail.com>
2022-10-13 16:47:45 +0900

    1 and 2 widths

--- tests/unittests/spec/response/spin_rotation_test.py
+++ tests/unittests/spec/response/spin_rotation_test.py
@@ -3,15 +3,21 @@ from exojax.spec.spin_rotation import rotkernel
 import jax.numpy as jnp
 
 def test_rotkernel(fig=False):
-    N = 101
-    x = jnp.linspace(-1.0, 1.0, N)
+    N = 201
+    x_1 = jnp.linspace(-2.0, 2.0, N)
     u1 = 0.1
     u2 = 0.1
-    kernel = rotkernel(x, u1, u2)
-    assert jnp.sum(kernel) == 143.85559
+    kernel_1 = rotkernel(x_1, u1, u2)
+    N = 101
+    x_2 = jnp.linspace(-1.0, 1.0, N)
+    kernel_2 = rotkernel(x_2, u1, u2)
+    assert jnp.sum(kernel_1) == pytest.approx(143.85559)
+    assert jnp.sum(kernel_2) == pytest.approx(143.85559)
+    
     if fig:
         import matplotlib.pyplot as plt
-        plt.plot(x, kernel)
+        plt.plot(x_1, kernel_1)
+        plt.plot(x_2, kernel_2)
         plt.show()
 
 
