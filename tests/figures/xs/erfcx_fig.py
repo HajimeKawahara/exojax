a2948d28daa5a1bfdf273bad824e24b656e235d4
Hajime Kawahara <divrot@gmail.com>
2022-10-17 16:20:07 +0900

    remove erfcx_scan

--- tests/figures/xs/erfcx_fig.py
+++ tests/figures/xs/erfcx_fig.py
@@ -9,7 +9,6 @@ from jax import jit, vmap
 import numpy as np
 from scipy.special import erfcx as sc_erfcx
 from exojax.special import erfcx
-from exojax.special._special import erfcx_scan
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.pyplot as plt
 #from jax.config import config                                                 #
@@ -19,7 +18,6 @@ N = 10000
 xv = jnp.logspace(-5, 5, N)
 xvc = np.logspace(-5, 5, N)
 verfcx = vmap(erfcx)
-verfcx_scan = vmap(erfcx_scan)
 ref = sc_erfcx(xvc)
 
 
@@ -42,27 +40,11 @@ def test_comparison_erfcx_scipy():
     assert np.max(d) < 2.e-6
 
 
-def test_comparison_erfcx_scan_scipy():
-    d = (verfcx_scan(xv) - ref) / ref
-    print('erfcx_scan: MEDIAN=', np.median(d),
-          ' MAX=', np.max(d), ' MEAN=', np.mean(d))
-    fig = plt.figure(figsize=(7, 2.3))
-    ax = fig.add_subplot(111)
-    ax.plot(xvc, d, '.', alpha=0.1, rasterized=True)
-    ax.yaxis.set_major_formatter(FormatStrFormatter('%1.e'))
-    plt.ylabel('(our erfcx - scipy)/scipy')
-    plt.xscale('log')
-    plt.xlabel('$x$')
-    plt.ylim(-3.e-6, 3.e-6)
-    plt.savefig('erfcx_scan.pdf', bbox_inches='tight', pad_inches=0.0)
-
-    assert np.max(d) < 2.e-6
 
 
 if __name__ == '__main__':
     import time
     test_comparison_erfcx_scipy()
-    test_comparison_erfcx_scan_scipy()
 
     if False:
         # comparison
@@ -73,8 +55,3 @@ if __name__ == '__main__':
             te = time.time()
             print('direct', te-ts)
 
-            ts = time.time()
-            for i in range(0, 10000):
-                verfcx_scan(xv)
-            te = time.time()
-            print('scan', te-ts)
--- tests/unittests/spec/xs/f64/erfcx_f64_test.py
+++ tests/unittests/spec/xs/f64/erfcx_f64_test.py
@@ -11,7 +11,6 @@ from jax import jit, vmap
 import numpy as np
 from scipy.special import erfcx as sc_erfcx
 from exojax.special import erfcx
-from exojax.special._special import erfcx_scan
 from jax.config import config                                                  
 config.update("jax_enable_x64", True)
 
@@ -19,22 +18,17 @@ N=10000
 xv=jnp.logspace(-5,5,N)
 xvc=np.logspace(-5,5,N)
 verfcx=vmap(erfcx)
-verfcx_scan=vmap(erfcx_scan)
 ref=sc_erfcx(xvc)
 
 def test_comparison_erfcx_scipy():
     d=(verfcx(xv) - ref)/ ref
     assert np.max(d)<2.e-6
 
-def test_comparison_erfcx_scan_scipy():
-    d=(verfcx_scan(xv) - ref)/ ref
-    assert np.max(d)<2.e-6
 
     
 if __name__ == "__main__":
     import time
     test_comparison_erfcx_scipy()
-    test_comparison_erfcx_scan_scipy()
 
     if False:
     #comparison 
@@ -44,9 +38,3 @@ if __name__ == "__main__":
                 verfcx(xv)
             te=time.time()
             print("direct",te-ts)
-            
-            ts=time.time()
-            for i in range(0,10000):
-                verfcx_scan(xv)
-            te=time.time()
-            print("scan",te-ts)
