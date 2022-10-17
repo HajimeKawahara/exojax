69fcc6d5ee993b72adc026cd68670f6fb17a04d9
Hajime Kawahara <divrot@gmail.com>
2022-10-17 23:13:33 +0900

    code climate

--- src/exojax/utils/grids.py
+++ src/exojax/utils/grids.py
@@ -25,38 +25,53 @@ def wavenumber_grid(x0, x1, N, unit='cm-1', xsmode='lpf'):
         resolution
     """
     
+    _check_even_number(N)
+    grid_mode = check_scale_xsmode(xsmode) 
+    grid = _set_grid(x0, x1, N, unit, grid_mode)
+    nus = _set_nus(unit, grid)
+    wav = nu2wav(nus, unit="AA")
+    resolution = _set_resolution(grid_mode, nus)
+
+    return nus, wav, resolution
+
+def _set_grid(x0, x1, N, unit, grid_mode):
+    if grid_mode == 'ESLOG':
+        grid = np.logspace(np.log10(x0), np.log10(x1), N, dtype=np.float64)
+    elif grid_mode == 'ESLIN':
+        grid = _set_grid_eslin(unit,x0,x1,N)
+    else:
+        raise ValueError("unavailable xsmode/unit.")
+    return grid
+
+def _check_even_number(N):
     if np.mod(N,2)==1:
         msg = "Currently, only even number is allowed as N. "
         msg += "response.convolve_rigid_rotation requires this condition."       
         raise ValueError(msg)
-        
-    if check_scale_xsmode(xsmode) == 'ESLOG':
-        grid = np.logspace(np.log10(x0), np.log10(x1), N, dtype=np.float64)
-    elif check_scale_xsmode(xsmode) == 'ESLIN' and unit == 'cm-1':
-        warnings.warn("ESLIN is not recommended. Consider to use ESLOG instead.", UserWarning)
-        grid = np.linspace((x0), (x1), N, dtype=np.float64)
-    else:
-        raise ValueError("unavailable xsmode/unit.")
-        
+
+def _set_nus(unit, grid):
     if unit == 'cm-1':
         nus = grid
     elif unit == 'nm' or unit == 'AA':
         nus = wav2nu(grid, unit)
-        
-    wav = nu2wav(nus, unit="AA")
-        
-    if check_scale_xsmode(xsmode) == 'ESLOG':
+    return nus
+
+def _set_resolution(grid_mode, nus):
+    if grid_mode == 'ESLOG':
         resolution = resolution_eslog(nus)
         minr = resolution
-    elif check_scale_xsmode(xsmode) == 'ESLIN':
+    elif grid_mode == 'ESLIN':
         minr, resolution, maxr = resolution_eslin(nus)
-        
     warn_resolution(minr)
+    return resolution
 
-    return nus, wav, resolution
-
-
-
+def _set_grid_eslin(unit,x0,x1,N):
+    if unit == "cm-1":
+        return np.linspace((x0), (x1), N, dtype=np.float64)
+    else: 
+        cx1, cx0 = wav2nu(np.array([x0, x1]), unit)
+        return np.linspace((cx0), (cx1), N, dtype=np.float64)
+    
 def velocity_grid(resolution, vmax):
     """generate velocity grid for a rigid rotation
 
