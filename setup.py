3c8f13c9cb3f5fbd3583e0960f47866a2cecf362
Hajime Kawahara <divrot@gmail.com>
2022-10-22 07:06:50 +0900

    install error from seaborn (matplotlib 3.6.1)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ CLASSIFIERS = [
     'Programming Language :: Python',
 ]
 INSTALL_REQUIRES = [
-    'numpy<=1.22.3', 'pandas', 'tqdm', 'scipy', 'jax>=0.2.22', 'numpyro', 'pyarrow',
+    'numpy<=1.22.3', 'pandas', 'tqdm', 'scipy', 'jax>=0.2.22', 'numpyro', 'pyarrow','matplotlib <= 3.6.0',
     'hitran-api', 'bs4', 'vaex', 'radis @ git+https://github.com/radis/radis.git@develop#egg=radis', 'jaxopt'
     ]
 
