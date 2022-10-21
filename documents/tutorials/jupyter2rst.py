b7aaf83e720df84d8be10ea815a00cdba877945d
Hajime Kawahara <divrot@gmail.com>
2022-10-22 07:01:37 +0900

    list

--- documents/tutorials/jupyter2rst.py
+++ documents/tutorials/jupyter2rst.py
@@ -5,8 +5,8 @@ dat = pd.read_csv("list.dat", names=("ipynb", ))
 
 for ipynb_name in dat["ipynb"]:
     print(ipynb_name)
-    #subprocess.run(
-    #    "jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=600  --execute "
-    #    + '"'+ipynb_name+'"',
-    #    shell=True)
+    subprocess.run(
+        "jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=600  --execute "
+        + '"'+ipynb_name+'"',
+        shell=True)
     subprocess.run("jupyter nbconvert --to rst "+ '"'+ipynb_name+'"',shell=True)
