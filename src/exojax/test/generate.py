f23f52ea6bfb25435336b7bf3bd9380c55a7c495
Hajime Kawahara <divrot@gmail.com>
2022-10-30 12:54:14 +0900

    api gpu_transfer in generate.py

--- src/exojax/test/generate.py
+++ src/exojax/test/generate.py
@@ -1,21 +1,25 @@
 """generate test data
 
 """
-from exojax.spec import moldb
+from exojax.spec import api
 from exojax.utils.grids import wavenumber_grid
 import pickle
-import numpy as np
+
 
 def gendata_moldb_exomol():
     """generate test data for CO exomol
     """
     from exojax.test.data import TESTDATA_moldb_CO_EXOMOL as filename
-    Nx=10000
-    nus, wav, res = wavenumber_grid(22920.0,24000.0, Nx, unit='AA')
-    mdbCO = moldb.MdbExomol('.database/CO/12C-16O/Li2015', nus, crit=1e-35, Ttyp=296.0)
+    Nx = 10000
+    nus, wav, res = wavenumber_grid(22920.0, 24000.0, Nx, unit='AA')
+    mdbCO = api.MdbExomol('.database/CO/12C-16O/Li2015',
+                          nus,
+                          crit=1e-35,
+                          Ttyp=296.0,
+                          gpu_transfer=True)
     with open(filename, 'wb') as f:
         pickle.dump(mdbCO, f)
 
-        
+
 if __name__ == "__main__":
     gendata_moldb_exomol()
---
+++ tests/unittests/test/emulate_mdb_test.py
@@ -0,0 +1,9 @@
+import pytest
+from exojax.test.emulate_mdb import mock_mdbExoMol
+
+def test_mock_mdbExoMol():
+    mdb = mock_mdbExoMol()
+    
+    
+if __name__ == "__main__":
+    test_mock_mdbExoMol()
