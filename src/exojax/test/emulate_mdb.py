9308bba5929be6ddf8a5191df0b1a8f8b10421a3
Hajime Kawahara <divrot@gmail.com>
2022-10-31 13:22:12 +0900

    include single isotope in emulate_mdb

--- src/exojax/test/emulate_mdb.py
+++ src/exojax/test/emulate_mdb.py
@@ -4,24 +4,38 @@ import pickle
 import pkg_resources
 from exojax.test.data import TESTDATA_moldb_CO_EXOMOL
 from exojax.test.data import TESTDATA_moldb_CO_HITEMP
+from exojax.test.data import TESTDATA_moldb_CO_HITEMP_SINGLE_ISOTOPE
 from exojax.test.data import TESTDATA_moldb_VALD
+
 
 def mock_mdbExomol():
     """default mock mdb of the ExoMol form for unit test   
     Returns:
         mdbExomol instance  
     """
-    filename = pkg_resources.resource_filename('exojax', 'data/testdata/'+TESTDATA_moldb_CO_EXOMOL)
+    filename = pkg_resources.resource_filename(
+        'exojax', 'data/testdata/' + TESTDATA_moldb_CO_EXOMOL)
     with open(filename, 'rb') as f:
         mdb = pickle.load(f)
     return mdb
 
-def mock_mdbHitemp():
+
+def mock_mdbHitemp(multi_isotope=False):
     """default mock mdb of the Hitemp form for unit test   
+    
+    Args:
+        multi isotope: if True, use multi isotope mdb
+    
     Returns:
         mdbHitemp instance  
     """
-    filename = pkg_resources.resource_filename('exojax', 'data/testdata/'+TESTDATA_moldb_CO_HITEMP)
+    if multi_isotope:
+        filename = TESTDATA_moldb_CO_HITEMP
+    else:
+        filename = TESTDATA_moldb_CO_HITEMP_SINGLE_ISOTOPE
+
+    filename = pkg_resources.resource_filename('exojax',
+                                               'data/testdata/' + filename)
     with open(filename, 'rb') as f:
         mdb = pickle.load(f)
     return mdb
@@ -32,8 +46,8 @@ def mock_mdbVALD():
     Returns:
         AdbVald instance
     """
-    filename = pkg_resources.resource_filename('exojax', 'data/testdata/'+TESTDATA_moldb_VALD)
+    filename = pkg_resources.resource_filename(
+        'exojax', 'data/testdata/' + TESTDATA_moldb_VALD)
     with open(filename, 'rb') as f:
         mdb = pickle.load(f)
     return mdb
-
