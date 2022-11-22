70ae0fa7f1ebdda81985f091ed88800ae7902375
Hajime Kawahara <divrot@gmail.com>
2022-11-22 21:33:02 +0900

    jaxopt spectrum bug fixed

--- src/exojax/spec/response.py
+++ src/exojax/spec/response.py
@@ -11,16 +11,20 @@ from jax.lax import scan
 from jax import jit
 
 from exojax.spec.spin_rotation import convolve_rigid_rotation
-from exojax.utils.grids import velocity_grid 
+from exojax.utils.grids import velocity_grid
 from exojax.utils.instfunc import resolution_eslog
+import warnings
+
 
 def rigidrot(nus, F0, vsini, u1, u2, vsinimax=100.0):
     """(deprecated) rigid rotation. This will be removed.
     """
+    warnings.warn(
+        "rigidrot is deprecated and do not work for VJP. Use convolve_rigid_rotation instead.",
+        UserWarning)
     resolution = resolution_eslog(nus)
     vr_array = velocity_grid(resolution, vsinimax)
     return convolve_rigid_rotation(F0, vr_array, vsini, u1, u2)
-    
 
 
 @jit
@@ -41,11 +45,10 @@ def ipgauss_sampling(nusd, nus, F0, beta, RV):
         dvgrid = c * (jnp.log1p(1.0 - nus / nusd_each))
         kernel = jnp.exp(-(dvgrid + RV)**2 / (2.0 * beta**2))
         kernel = kernel / jnp.sum(kernel)
-        return carry, kernel@F0
+        return carry, kernel @ F0
 
     _, F_convolved = scan(convolve_ipgauss_scan, 0, nusd)
     return F_convolved
-
 
 
 @jit
