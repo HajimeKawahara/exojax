ded87e9ce4b3bb9670989d065a623152d0238a0d
Hajime Kawahara <divrot@gmail.com>
2023-02-12 13:25:21 +0900

    test vaex in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ CLASSIFIERS = [
 ]
 INSTALL_REQUIRES = [
     'numpy<=1.22.3', 'pandas', 'tqdm', 'scipy', 'jax>=0.2.22', 'numpyro',
-    'pyarrow', 'matplotlib <= 3.6.0', 'hitran-api', 'bs4', 'radis', 'jaxopt'
+    'pyarrow', 'matplotlib <= 3.6.0', 'hitran-api', 'bs4', 'vaex==4.9.2',
+    'radis', 'jaxopt'
 ]
 
 # END PROJECT SPECIFIC
