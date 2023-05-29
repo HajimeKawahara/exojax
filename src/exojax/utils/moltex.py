5950143aefb1c7d43c60afaaf308d78cb45b5f6e
Hajime Kawahara <divrot@gmail.com>
2023-05-29 20:18:39 +0900

    for list

--- src/exojax/utils/moltex.py
+++ src/exojax/utils/moltex.py
@@ -9,7 +9,7 @@ def format_molecule(simple_molecule_name):
     and the string surrounded by LaTeX math mode and "mathrm" syntax.
 
     Args:
-        simple_molecule_name (str): A string representation of a molecule.
+        molecule (str): A string representation of a molecule.
 
     Returns:
         str: A LaTeX-formatted string representation of the molecule.
@@ -26,9 +26,30 @@ def format_molecule(simple_molecule_name):
     latex_molecule = f"$\\mathrm{{{formatted_molecule}}}$"
     return latex_molecule
 
+def format_molecules_list(molecules):
+    """
+    Format a list of molecule strings with subscript numbers and convert them to LaTeX syntax.
+    
+    This function takes in a list of molecule strings, where elements are represented by their symbols and
+    the number of atoms is represented by a subscript number following the symbol. The function
+    converts each molecule string to a LaTeX string, with subscript numbers formatted correctly
+    and the string surrounded by LaTeX math mode and "mathrm" syntax.
+
+    Args:
+        molecules (list of str): A list of string representations of molecules.
+
+    Returns:
+        list of str: A list of LaTeX-formatted string representations of the molecules.
+
+    Examples:
+        >>> format_molecules_list(["H2O", "CH4", "CO"])
+        ["$\\mathrm{H_2O}$", "$\\mathrm{CH_4}$", "$\\mathrm{CO}$"]
+    """
+    return [format_molecule(molecule) for molecule in molecules]
+
 if __name__ == "__main__":
     molecules = ["H2O", "CH4", "CO"]
 
     for molecule in molecules:
         print(format_molecule(molecule))
-
+    print(format_molecules_list(molecules))
--- tests/unittests/utils/moltex_test.py
+++ tests/unittests/utils/moltex_test.py
@@ -1,8 +1,12 @@
 import pytest
-from exojax.utils.moltex import format_molecule
+from exojax.utils.moltex import format_molecule, format_molecules_list
 
 def test_format_molecule():
     assert format_molecule("H2O") == "$\\mathrm{H_2O}$"
     assert format_molecule("CH4") == "$\\mathrm{CH_4}$"
     assert format_molecule("CO") == "$\\mathrm{CO}$"
     assert format_molecule("NH3") == "$\\mathrm{NH_3}$"
+
+def test_format_molecules_list():
+    assert format_molecules_list(["H2O", "CH4", "CO"]) == ["$\\mathrm{H_2O}$", "$\\mathrm{CH_4}$", "$\\mathrm{CO}$"]
+    assert format_molecules_list(["NH3", "NO2"]) == ["$\\mathrm{NH_3}$", "$\\mathrm{NO_2}$"]
