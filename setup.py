afe40f2079cc9521e3f0c368a0d5c2eae9277947
Hajime Kawahara <divrot@gmail.com>
2023-06-05 18:16:08 +0900

    setup version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             'write_to_template':
             '__version__ = "{version}"\n',
         },
-        version='1.3',
+        version='1.3.1',
         author=find_meta('author'),
         author_email=find_meta('email'),
         maintainer=find_meta('author'),
