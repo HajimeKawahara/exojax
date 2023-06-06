891c89d8259da514a965654cf3d3cb68e310c598
Hajime Kawahara <divrot@gmail.com>
2023-06-06 22:09:59 +0900

    Update src/exojax/spec/__init__.py
    Co-authored-by: ykawashima <44265645+ykawashima@users.noreply.github.com>

--- src/exojax/spec/__init__.py
+++ src/exojax/spec/__init__.py
@@ -7,7 +7,7 @@ __email__ = 'divrot@gmail.com'
 __license__ = ''
 __description__ = 'auto-differentiable spectral modules in exojax'
 
-from exojax.spec.hitran import (line_strength, line_strength, doppler_sigma,
+from exojax.spec.hitran import (line_strength, doppler_sigma,
                                 gamma_natural, normalized_doppler_sigma)
 
 from exojax.spec.lpf import (
