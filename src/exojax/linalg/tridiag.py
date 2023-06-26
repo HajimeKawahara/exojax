13693ad6d6477715caeb024ae3f288ce20304dfc
Hajime Kawahara <divrot@gmail.com>
2023-06-26 12:06:12 +0900

    imported rtjax tridiag

---
+++ src/exojax/linalg/tridiag.py
@@ -0,0 +1,63 @@
+""" Tridiagonal Solver 
+
+- The original code of solve_tridiag was taken from lineax (https://github.com/google/lineax), under Apache 2.0 License (See LICENSES_bundled.txt).
+
+"""
+import jax.numpy as jnp
+from jax.lax import scan
+
+
+def solve_tridiag(diagonal, lower_diagonal, upper_diagonal, vector):
+    """Tridiagonal Linear Solver for A x = b, using Thomas Algorithm  
+    
+    the original code was taken from lineax (https://github.com/google/lineax), under Apache 2.0 License (See LICENSES_bundled.txt).
+
+    Args:
+        diagonal (1D array): the diagonal component vector of a matrix A
+        lower_diagonal (1D array): the lower diagonal component vector of a matrix A
+        upper_diagonal (1D array): the upper diagonal component vector of a matrix A
+        vector (1D array): the vector b
+
+    Notes:
+        notation from: https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+        _p indicates prime, ie. `d_p` is the variable name for d' on wikipedia
+        
+
+
+    Returns:
+        1D array: solution vector (x)
+    """
+
+    size = len(diagonal)
+
+    def thomas_scan(prev_cd_carry, bd):
+        c_p, d_p, step = prev_cd_carry
+        # the index of `a` doesn't matter at step 0 as
+        # we won't use it at all. Same for `c` at final step
+        a_index = jnp.where(step > 0, step - 1, 0)
+        c_index = jnp.where(step < size, step, 0)
+
+        b, d = bd
+        a, c = lower_diagonal[a_index], upper_diagonal[c_index]
+        denom = b - a * c_p
+        new_d_p = (d - a * d_p) / denom
+        new_c_p = c / denom
+        return (new_c_p, new_d_p, step + 1), (new_c_p, new_d_p)
+
+    def backsub(prev_x_carry, cd_p):
+        x_prev, step = prev_x_carry
+        c_p, d_p = cd_p
+        x_new = d_p - c_p * x_prev
+        return (x_new, step + 1), x_new
+
+    # not a dummy init! 0 is the proper value for all of these
+    init_thomas = (0, 0, 0)
+    init_backsub = (0, 0)
+    diag_vec = (diagonal, vector)
+    _, cd_p = scan(thomas_scan, init_thomas, diag_vec, unroll=32)
+    _, solution = scan(backsub, init_backsub, cd_p, reverse=True, unroll=32)
+
+    return solution
+
+
+
---
+++ tests/unittests/special/tridiag_test.py
@@ -0,0 +1,54 @@
+import jax.numpy as jnp
+from exojax.linalg.tridiag import solve_tridiag
+
+def test_solve_tridiag():
+    from jax.config import config
+    config.update("jax_enable_x64", True)
+    mat = jnp.array([[1., 8., 0., 0.], [5., 2., 9., 0.], [0., 6., 3., 10.],
+                     [0., 0., 7., 4.]])
+    ans = jnp.array([4., 3., 2., 1.])
+    vector = mat @ ans
+    diag = jnp.diag(mat)
+    lower_diag = jnp.diag(mat, k=-1)
+    upper_diag = jnp.diag(mat, k=1)
+
+    x = solve_tridiag(diag, lower_diag, upper_diag, vector)
+    print(x, "tridiag")
+
+    assert jnp.sum((mat @ x - vector)**2) == 0.0
+
+
+def test_solve_vmaptridiag():
+    from jax.config import config
+    config.update("jax_enable_x64", True)
+    from jax import vmap
+    nlayer = 3
+    mat = jnp.array([[1., 8., 0., 0.], [5., 2., 9., 0.], [0., 6., 3., 10.],
+                     [0., 0., 7., 4.]])
+    ans = jnp.array([4., 3., 2., 1.])
+    vector = mat @ ans
+
+    vvector = jnp.array([vector for _ in range(nlayer)])
+    diag = jnp.diag(mat)
+    lower_diag = jnp.diag(mat, k=-1)
+    upper_diag = jnp.diag(mat, k=1)
+    vdiag = jnp.array([diag for _ in range(nlayer)])
+    vlower_diag = jnp.array([lower_diag for _ in range(nlayer)])
+    vupper_diag = jnp.array([upper_diag for _ in range(nlayer)])
+
+    vsolve_tridiag = vmap(solve_tridiag, (0, 0, 0, 0), 0)
+
+    x = vsolve_tridiag(vdiag, vlower_diag, vupper_diag, vvector)
+
+    print(x, "vmap tridiag")
+
+    for i in range(nlayer):
+        assert jnp.sum((mat @ x[i] - vector)**2) == 0.0
+
+
+
+
+
+if __name__ == "__main__":
+    test_solve_tridiag()
+    test_solve_vmaptridiag()
