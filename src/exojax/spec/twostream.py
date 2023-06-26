c9d1c756813072075addd696507dfd0fee3460dc
Hajime Kawahara <divrot@gmail.com>
2023-06-26 15:49:40 +0900

    coefficient tested

--- src/exojax/spec/twostream.py
+++ src/exojax/spec/twostream.py
@@ -30,22 +30,39 @@ def compute_tridiag_diagonals(coeff_scattering, coeff_transmission,
 
     diagonal = coeff_scattering * (Sn_minus_one**2 - Tn_minus_one**2)
     upper_diagonal = Sn_minus_one * coeff_transmission
+
     lower_diagonal = Sn_plus_one * coeff_transmission
 
     #boundary setting
     upper_diagonal = upper_diagonal.at[0].set(upper_diagonal_top)
     diagonal = diagonal.at[0].set(diagonal_top)
     diagonal = diagonal.at[-1].set(diagonal_btm)
-    lower_diagonal = diagonal.at[-2].set(lower_diagonal_btm)
+    lower_diagonal = lower_diagonal.at[-2].set(lower_diagonal_btm)
 
     return diagonal, lower_diagonal, upper_diagonal
 
 
 def test_tridiag_coefficients():
-    N = 10
-    S = jnp.array(range(0, N)) * 0.1
-    T = jnp.array(range(0, N))
-    print(compute_tridiag_diagonals(S, T, 0.0, 0.0, 0.0, 0.0))
+    import numpy as np
+    N = 4
+    S = jnp.array(range(0, N)) + 1
+    T = (jnp.array(range(0, N)) + 1) * 2
+    boundaries = (1.0, 2.0, 3.0, 4.0)
+    upper_diagonal_top, diagonal_top, diagonal_btm, lower_diagonal_btm = boundaries
+    
+    diag, lower, upper = compute_tridiag_diagonals(S, T, upper_diagonal_top,
+                                                   diagonal_top, diagonal_btm,
+                                                   lower_diagonal_btm)
+
+    ref_diag = jnp.array([
+        diagonal_top, S[1] * (S[0]**2 - T[0]**2), S[2] * (S[1]**2 - T[1]**2),
+        diagonal_btm
+    ])
+    assert np.all(ref_diag - diag) == 0.0
+    ref_lower = jnp.array([S[1] * T[0], S[2] * T[1], lower_diagonal_btm])
+    assert np.all(ref_lower - lower[:-1]) == 0.0
+    ref_upper = jnp.array([upper_diagonal_top, S[0] * T[1], S[1] * T[2]])
+    assert np.all(ref_upper - upper[:-1]) == 0.0
 
 
 if __name__ == "__main__":
