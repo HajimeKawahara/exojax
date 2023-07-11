ad4d6ca703a6a317169599dc19e1729bf977c3a9
Hajime Kawahara <divrot@gmail.com>
2023-07-12 07:01:26 +0900

    twostream_test fixed

--- tests/unittests/spec/twostream/twostream_test.py
+++ tests/unittests/spec/twostream/twostream_test.py
@@ -7,19 +7,18 @@ def test_tridiag_coefficients():
     Nlayer = 4
     S = jnp.array(range(0, Nlayer)) + 1.
     T = (jnp.array(range(0, Nlayer)) + 1) * 2.
-    boundaries = (1.0, 2.0, 3.0, 4.0)
-    upper_diagonal_top, diagonal_top, diagonal_btm, lower_diagonal_btm = boundaries
+    boundaries = (1.0, 2.0)
+    upper_diagonal_top, diagonal_top = boundaries
 
     diag, lower, upper = compute_tridiag_diagonals(S, T, upper_diagonal_top,
-                                                   diagonal_top, diagonal_btm,
-                                                   lower_diagonal_btm)
-
+                                                   diagonal_top)#, diagonal_btm,
+                                                   #lower_diagonal_btm)
     ref_diag = jnp.array([
         diagonal_top, S[1] * (S[0]**2 - T[0]**2) -S[0] , S[2] * (S[1]**2 - T[1]**2) -S[1],
-        diagonal_btm
+        S[3] * (S[2]**2 - T[2]**2) -S[2]
     ])
     assert np.array_equal(ref_diag,diag) 
-    ref_lower = jnp.array([S[1] * T[0], S[2] * T[1], lower_diagonal_btm])
+    ref_lower = jnp.array([S[1] * T[0], S[2] * T[1], S[3] * T[2]])
     assert np.array_equal(ref_lower, lower[:-1])
     ref_upper = jnp.array([upper_diagonal_top, S[0] * T[1], S[1] * T[2]])
     assert np.array_equal(ref_upper, upper[:-1])
