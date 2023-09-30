4043e51ffa93f0b4af827931788aaf272592f09d
Hajime Kawahara <divrot@gmail.com>
2023-09-30 16:14:38 +0900

    moved simpson to integrate.py

--- tests/unittests/signal/simpson_integral_test.py
+++ tests/unittests/signal/simpson_integral_test.py
@@ -1,10 +1,11 @@
 import pytest
 import jax.numpy as jnp
+from exojax.signal.integrate import simpson
 from jax.config import config
 
 config.update('jax_enable_x64', True)
 
-def test_simpson():
+def test_compare_simpson_with_manual_computation():
     """ test simpson integral
 
     f = 0.01, (0.3), 1.0, (1.3), 2.0, (2.7), 3.0
@@ -24,13 +25,7 @@ def test_simpson():
 
     assert integral[0] == pytest.approx(ref_integral[0])
 
-def simpson(f, f_lower, f_top, h):
-    N=len(f)
-    hh = jnp.roll(h, -1) + h  # h_{n+1} + h_n
-    fac = hh[:N-1,None]*f_lower[:N-1,:]
-    return 2.0/3.0*jnp.sum(h[:,None]*f) + h[0]*f_top/6.0 + h[-1]*f_lower[-1,:]/6.0 + jnp.sum(fac)/6.0
-    
 
 if __name__ == "__main__":
-    test_simpson()
+    test_compare_simpson_with_manual_computation()
 
