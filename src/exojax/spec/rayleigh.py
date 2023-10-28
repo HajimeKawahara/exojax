c6f8f51c6c607e433329afd5b797b2da2687567e
Hajime Kawahara <divrot@gmail.com>
2023-10-28 23:00:20 +0900

    polarizability

--- src/exojax/spec/rayleigh.py
+++ src/exojax/spec/rayleigh.py
@@ -1,16 +1,14 @@
 import jax.numpy as jnp
-
+from exojax.atm.polarizability import n_ref_refractive
 
 def cross_section_rayleigh_gas(wavenumber,
                                refractive_index,
-                               number_density,
                                king_factor=1.0):
     """ Computes Rayleigh scattering cross-section of gas from real refractive index
     
         Args:
             wavenumber: wavenumber (cm-1)
             refractive_index: real refractive index (hint: one can compute the refractive index using the Lorentz-Lonrez formula (atm.lorentz_lorenz) from polarizability)
-            number_density: gas number density of the molecule (cm-3)
             king_factor: King correction factor which accounts for the depolarization effect (default=1.0)
 
         Returns:
@@ -20,5 +18,18 @@ def cross_section_rayleigh_gas(wavenumber,
             References=
 
     """
-    fac = (refractive_index**2 - 1.0) / (refractive_index**2 + 2.0)
-    return 24.0 * jnp.pi**3 * wavenumber**4 / number_density**2 * fac**2 * king_factor
+    fac = (refractive_index**2 - 1.0) / (n_ref_refractive*(refractive_index**2 + 2.0))
+    print(fac)
+    return 24.0 * jnp.pi**3 * (wavenumber**2 * fac)**2 * king_factor
+
+
+if __name__ == "__main__":
+    from exojax.atm.polarizability import polarizability
+    from exojax.atm.lorentz_lorenz import refractive_index_Lorentz_Lorenz
+    p = polarizability["H2"]
+    refractive_index = refractive_index_Lorentz_Lorenz(p, n_ref_refractive)
+    print(refractive_index)
+    from exojax.utils.grids import wavenumber_grid
+    nus,wav,res = wavenumber_grid(3000.0, 3100.0, 128, xsmode="premodit", wavelength_order="descending", unit='nm')
+    sigma = cross_section_rayleigh_gas(nus,refractive_index)
+    print(sigma)
