93ee60cf16ee864c44f3dc5e48cd100ff21e3f2d
HajimeKawahara <divrot@gmail.com>
2023-11-16 10:15:29 +0900

    temp

--- src/exojax/atm/vterm.py
+++ src/exojax/atm/vterm.py
@@ -86,16 +86,17 @@ def vf_largeNre(r, g, eta, drho, rho):
     return eta/(2.0*rho*r)*jnp.sqrt(ND/Cd)
 
 
+
 @jit
-def vf(r, g, eta, rhoc, rho, Nkn=0.0):
-    """terminal velocity.
+def terminal_velocity(r, gravity, dynamic_viscosity, rho_cloud, rho_atm, Nkn=0.0):
+    """computes terminal velocity in a wide particles size range.
 
     Args:
         r: particle size (cm)
         g: gravity (cm/s2)
-        eta: dynamic viscosity (g/s/cm)
-        rhoc: condensate density (g/cm3)
-        rho: atmosphere density (g/cm3)
+        dynamic_viscosity: dynamic viscosity (g/s/cm)
+        rho_cloud: condensate density (g/cm3)
+        rho_atm: atmosphere density (g/cm3)
         Nkn: Knudsen number
 
     Return:
@@ -110,16 +111,22 @@ def vf(r, g, eta, rhoc, rho, Nkn=0.0):
         >>> vfactor,Tr=vc.calc_vfactor(atm="Air")
         >>> eta=vc.eta_Rosner(300.0,vfactor)
         >>> r=jnp.logspace(-5,0,70)
-        >>> vf(r,g,eta,drho,rho) #terminal velocity (cm/s)
+        >>> terminal_velocity(r,g,eta,drho,rho) #terminal velocity (cm/s)
     """
-    drho = rhoc-rho
-    ND = Ndavies(r, g, eta, drho, rho)
+    drho = rho_cloud-rho_atm
+    ND = Ndavies(r, gravity, dynamic_viscosity, drho, rho_atm)
     cond = [ND < 42.877543, (ND >= 42.877543) *
             (ND < 119643.38), ND >= 119643.38]
-    choice = [vf_stokes(r, g, eta, drho, Nkn), vf_midNre(
-        r, g, eta, drho, rho), vf_largeNre(r, g, eta, drho, rho)]
+    choice = [vf_stokes(r, gravity, dynamic_viscosity, drho, Nkn), vf_midNre(
+        r, gravity, dynamic_viscosity, drho, rho_atm), vf_largeNre(r, gravity, dynamic_viscosity, drho, rho_atm)]
     vft = jnp.select(cond, choice)
     return vft
+
+#deprecated 
+import warnings
+def vf(r, gravity, dynamic_viscosity, rho_cloud, rho_atm, Nkn):
+    warnings.warn("vf will be replaced to terminal_velocity.")
+    return terminal_velocity(r, gravity, dynamic_viscosity, rho_cloud, rho_atm, Nkn)
 
 
 if __name__ == '__main__':
@@ -133,7 +140,7 @@ if __name__ == '__main__':
     eta = vc.eta_Rosner(300.0, vfactor)
     r = jnp.logspace(-5, 0, 70)
     plt.figure(figsize=(5, 3))
-    plt.plot(r*1.e4, vf(r, g, eta, drho, rho), color='black')
+    plt.plot(r*1.e4, terminal_velocity(r, g, eta, drho, rho), color='black')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('r (micron)')
