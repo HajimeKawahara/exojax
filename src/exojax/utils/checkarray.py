d67d9ba391cc44b3671551df83251c74a4ea0270
Hajime Kawahara <divrot@gmail.com>
2024-01-07 18:38:11 +0900

    implemented

--- src/exojax/utils/checkarray.py
+++ src/exojax/utils/checkarray.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def is_sorted(x):
     """Check if a list is sorted in ascending or descending order.
 
@@ -14,8 +17,31 @@ def is_sorted(x):
     if isinstance(x, (int, float, str, bool)):
         return "single"
     elif all(a <= b for a, b in zip(x, x[1:])):
-        return 'ascending'
+        return "ascending"
     elif all(a >= b for a, b in zip(x, x[1:])):
-        return 'descending'
+        return "descending"
     else:
-        return 'unordered'
+        return "unordered"
+
+
+def is_outside_range(xarr, xs, xe):
+    """
+    Check if all elements in the array are outside the specified range.
+
+    Args:
+        xarr (numpy.ndarray): An array of numerical values.
+        xs (float): The start of the range (exclusive).
+        xe (float): The end of the range (exclusive).
+
+    Returns:
+        bool: True if all elements in xarr are outside the range (xs, xe), False otherwise.
+
+    Examples:
+        >>> xarr = np.array([1.2, 1.4, 1.7, 1.3, 1.0])
+        >>> xs = 0.7
+        >>> xe = 0.8
+        >>> result = is_outside_range(xarr, xs, xe) #-> True
+
+        
+    """
+    return not np.any((xarr > xs) & (xarr < xe))
---
+++ tests/integration/api/inconsistent_nu_error_test.py
@@ -0,0 +1,42 @@
+from exojax.spec.api import MdbHitran
+from exojax.spec.opacalc import OpaPremodit
+from exojax.utils.grids import wavenumber_grid
+from exojax.utils.checkarray import is_outside_range
+import numpy as np
+import pytest
+
+def test_raise_error_in_premodit_when_no_mdblines_in_nu_grid():
+    wavelength_start = 7100.0 #AA
+    wavelength_end = 7450.0 #AA
+
+    wavenumber_start = 7100.0 #cm-1
+    wavenumber_end = 7450.0 #cm-1
+
+    N=40000
+    mdb_water = MdbHitran("H2O", nurange=[1.e8/wavelength_end, 1.e8/wavelength_start], isotope=0)
+    nus, wav, res = wavenumber_grid(wavenumber_start, wavenumber_end, N, xsmode="premodit")
+
+    print("opa nu range", nus[0],nus[-1])
+    print("mdb nu range", np.min(mdb_water.nu_lines), np.max(mdb_water.nu_lines))
+    print(is_outside_range(mdb_water.nu_lines,nus[0],nus[-1]))
+
+    with pytest.raises(ValueError):
+        opa = OpaPremodit(mdb_water, nu_grid=nus, allow_32bit=True, auto_trange=[150.0,250.0])
+
+
+def test_no_error_in_premodit_when_mdblines_in_nu_grid():
+    wavelength_start = 7100.0 #AA
+    wavelength_end = 7450.0 #AA
+
+    wavenumber_start = 1.e8/wavelength_end
+    wavenumber_end = 1.e8/wavelength_start 
+
+    N=40000
+    mdb_water = MdbHitran("H2O", nurange=[1.e8/wavelength_end, 1.e8/wavelength_start], isotope=0)
+    nus, wav, res = wavenumber_grid(wavenumber_start, wavenumber_end, N, xsmode="premodit")
+
+    print("opa nu range", nus[0],nus[-1])
+    print("mdb nu range", np.min(mdb_water.nu_lines), np.max(mdb_water.nu_lines))
+    print(is_outside_range(mdb_water.nu_lines,nus[0],nus[-1]))
+
+    opa = OpaPremodit(mdb_water, nu_grid=nus, allow_32bit=True, auto_trange=[150.0,250.0])
