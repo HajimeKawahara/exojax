290533a21de4b738c3f9939a93c164cd8dd78831
Hajime Kawahara <divrot@gmail.com>
2024-01-20 11:56:02 +0900

    adde exomol broadening a1

--- src/exojax/spec/api.py
+++ src/exojax/spec/api.py
@@ -197,7 +197,7 @@ class MdbExomol(CapiMdbExomol):
 
     def compute_load_mask(self, df):
         # wavelength
-        print(df)
+        # print(df)
         mask = (df.nu_lines > self.nurange[0]) * (df.nu_lines < self.nurange[1])
         QTtyp = np.array(self.QT_interp(self.Ttyp))
         QTref_original = np.array(self.QT_interp(Tref_original))
