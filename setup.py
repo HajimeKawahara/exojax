9dfdae29d64e9cb1ab69679b1d0ffa0a3c65c911
Hajime Kawahara <divrot@gmail.com>
2024-07-17 19:00:19 +0900

    1.5 -> 1.5.1 in setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
             'write_to_template':
             '__version__ = "{version}"\n',
         },
-        version='1.5',
+        version='1.5.1',
         author=find_meta('author'),
         author_email=find_meta('email'),
         maintainer=find_meta('author'),
