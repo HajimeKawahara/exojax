04fb330df0db024290f1cfea2f2ff98da8437c78
Hajime Kawahara <divrot@gmail.com>
2024-08-10 23:40:06 +0900

    hitemp and exomol

--- tests/unittests/spec/opa/sideeffect_test.py
+++ tests/unittests/spec/opa/sideeffect_test.py
@@ -22,5 +22,20 @@ def test_sideeffect_call():
     print(opa1 == opa1_orig)
     assert opa1 == opa1_orig
 
+def test_sideeffect_call_exomol():
+    nus, wav, res = wavenumber_grid(
+        22920.0, 23100.0, 20000, unit="AA", xsmode="premodit"
+    )
+    mdb = mock_mdbExomol()
+    opa1 = OpaPremodit(mdb, nu_grid=nus, allow_32bit=True, auto_trange=[500.0, 1000])
+    opa1_orig = copy.deepcopy(opa1)
+    opa2 = OpaPremodit(
+        mdb, nu_grid=nus, allow_32bit=True, auto_trange=[500.0, 1200]
+    )  # used the same mdb used in opa1
+    print(opa1 == opa1_orig)
+    assert opa1 == opa1_orig
+
+
 if __name__ == "__main__":
     test_sideeffect_call()
+    test_sideeffect_call_exomol()
