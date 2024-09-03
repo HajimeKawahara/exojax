352e48ab46582b812fd8b396643da64aa58f5ffd
Hajime Kawahara <divrot@gmail.com>
2024-09-03 10:08:00 +0900

    remove _fast from the original optgrid long tset

--- tests/integration/unittests_long/premodit/optgrid_test.py
+++ tests/integration/unittests_long/premodit/optgrid_test.py
@@ -4,20 +4,6 @@ from jax import config
 config.update("jax_enable_x64", True)
 
 
-def test_optelower_exomol_fast():
-    from exojax.test.emulate_mdb import mock_mdbExomol
-    from exojax.test.emulate_mdb import mock_wavenumber_grid
-    
-    nu_grid, wav, reso = mock_wavenumber_grid()
-    Tmax = 1020.0  #K
-    Pmin = 0.1  #bar
-    mdb = mock_mdbExomol(crit=1.e-37)
-    Eopt = optelower(mdb, nu_grid, Tmax, Pmin, accuracy=0.0)
-    print("optimal elower_max=",Eopt,"cm-1")
-    assert Eopt == pytest.approx(11615.5075)
-
-
-
 def test_optelower_exomol():
     from exojax.test.emulate_mdb import mock_mdbExomol
     from exojax.test.emulate_mdb import mock_wavenumber_grid
