073b88979470838fd3e7b9fe092bfcab6a951eac
Hajime Kawahara <divrot@gmail.com>
2024-09-03 10:02:51 +0900

    optgrid unittest and find a bug

--- tests/integration/unittests_long/premodit/optgrid_test.py
+++ tests/integration/unittests_long/premodit/optgrid_test.py
@@ -4,6 +4,20 @@ from jax import config
 config.update("jax_enable_x64", True)
 
 
+def test_optelower_exomol_fast():
+    from exojax.test.emulate_mdb import mock_mdbExomol
+    from exojax.test.emulate_mdb import mock_wavenumber_grid
+    
+    nu_grid, wav, reso = mock_wavenumber_grid()
+    Tmax = 1020.0  #K
+    Pmin = 0.1  #bar
+    mdb = mock_mdbExomol(crit=1.e-37)
+    Eopt = optelower(mdb, nu_grid, Tmax, Pmin, accuracy=0.0)
+    print("optimal elower_max=",Eopt,"cm-1")
+    assert Eopt == pytest.approx(11615.5075)
+
+
+
 def test_optelower_exomol():
     from exojax.test.emulate_mdb import mock_mdbExomol
     from exojax.test.emulate_mdb import mock_wavenumber_grid
@@ -29,6 +43,7 @@ def test_optelower_hitemp():
     print("optimal elower_max=",Eopt,"cm-1")
     assert Eopt == pytest.approx(11659.3718)
 
+
 if __name__ == "__main__":
-    #test_optelower_exomol()
+    test_optelower_exomol()
     test_optelower_hitemp()
