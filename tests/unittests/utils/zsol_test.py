e7e3ea5f00071a9992abfa2dffffdee02970e7e8
Hajime Kawahara <divrot@gmail.com>
2024-09-15 13:04:57 +0900

    temp

--- tests/unittests/utils/zsol_test.py
+++ tests/unittests/utils/zsol_test.py
@@ -5,3 +5,20 @@ def test_check_sum_nsol():
     n = nsol()
     sum_n = sum([n[atom] for atom in n])
     assert sum_n == pytest.approx(1.0)
+
+from exojax.spec.molinfo import element_mass 
+
+def mmr_metal_solar():
+    n = nsol()
+    hhe = element_mass["H"]*n["H"] + element_mass["He"]*n["He"]
+    sum_element = sum([element_mass[atom]*n[atom] for atom in n])
+    mmr_metal = (sum_element - hhe)/sum_element
+    
+    
+    print(element_mass["H"]*n["H"]/sum_element)
+    print(element_mass["He"]*n["He"]/sum_element)
+    print(mmr_metal)
+
+if __name__ == "__main__":
+    test_check_sum_nsol()
+    mmr_metal_solar()
