d3bc4bcf01e4cabc5ce595671b923903dbb9a959
Hajime Kawahara <divrot@gmail.com>
2024-09-16 10:00:45 +0900

    fig

--- documents/tutorials/jupiters/postprocess.py
+++ documents/tutorials/jupiters/postprocess.py
@@ -17,29 +17,33 @@ wav_obs = nu2wav(nus_obs, unit="AA")
 spectra = unmask_spectra[mask]
 
 
+# PLOT: corner plot
 azdata = arviz.from_netcdf("output/samples.nc")
 var = ["fsed", "vr", "mmr_ch4", "factor", "sigma"]
 label = ["$f_\mathrm{sed}$", "$v_r$", "MMR (CH4)", "$A$", "$\sigma$"]
-ax = arviz.plot_pair(azdata, kind="kde", divergences=False, marginals=True, 
-                var_names=var, textsize=30)
+ax = arviz.plot_pair(
+    azdata, kind="kde", divergences=False, marginals=False, var_names=var, textsize=30
+)
+# label change
 for i, ax_ in enumerate(ax.flatten()):
     if ax_.get_xlabel():
-        ax_.set_xlabel(label[i-20])
+        ax_.set_xlabel(label[i - 12])
+        #ax_.set_xlabel(label[i - 20]) #when marginals=True
     if ax_.get_ylabel():
-        ax_.set_ylabel(label[int(i/5)])
+        ax_.set_ylabel(label[int(i / 4)+1])
+        #ax_.set_ylabel(label[int(i / 5)]) #when marginals=True
 plt.tick_params(labelsize=30)
-plt.savefig("output/pairplot.png")
+plt.savefig("output/pairplot.png", bbox_inches="tight", pad_inches=0.1)
 
+# PLOT: posterior plot
 predictions = np.load("output/predictions.npz", allow_pickle=True)["arr_0"]
 predictions = predictions.item()
 median_mu1 = jnp.median(predictions["y1"], axis=0)
 hpdi_mu1 = hpdi(predictions["y1"], 0.95)
-
-# prediction plot
 plt = plotjupiter.plot_prediction(wav_obs, spectra, median_mu1, hpdi_mu1)
 
-#unmask_wav_obs = nu2wav(unmask_nus_obs, unit="AA")
-#plt.plot(unmask_wav_obs, unmask_spectra, ".", alpha=0.3, color="gray")
+# unmask_wav_obs = nu2wav(unmask_nus_obs, unit="AA")
+# plt.plot(unmask_wav_obs, unmask_spectra, ".", alpha=0.3, color="gray")
 plt.savefig("output/Jupiter_fit_wav.png", bbox_inches="tight", pad_inches=0.1)
 
 from exojax.utils.zsol import nsol
@@ -47,6 +51,9 @@ from exojax.utils.zsol import mass_fraction
 
 n = nsol("AG89")
 Xc = mass_fraction("C", n)
-mmr_sol = Xc*16/12
-print("MMR_CH4 for 3 Zsol (AG89) = ",3*mmr_sol)
-print(10**(0.04)*0.01/mmr_sol,"-", 10**(0.22)*0.01/mmr_sol)
+mmr_sol = Xc * 16 / 12
+print("MMR_CH4 for 3 Zsol (AG89) = ", 3 * mmr_sol)
+MMR5 = 10 ** (0.04)
+MMR95 = 10 ** (0.22)
+print("MMR (5-95%):", MMR5, "-", MMR95)
+print("Z:", MMR5 * 0.01 / mmr_sol, "-", MMR95 * 0.01 / mmr_sol)
