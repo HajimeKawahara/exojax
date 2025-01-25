79132e0ac707d0759b46c4407d3a62d87bab3a40
Hajime Kawahara <divrot@gmail.com>
2025-01-25 11:32:15 +0900

    comments for CIA

--- src/exojax/spec/hitrancia.py
+++ src/exojax/spec/hitrancia.py
@@ -78,19 +78,19 @@ def interp_logacia_matrix(Tarr, nu_grid, nucia, tcia, logac):
     """interpolated function of log10(alpha_CIA)
 
     Args:
-        Tarr (1D array): temperature array (K)
-        nu_grid (1D array): wavenumber array (cm-1)
+        Tarr (1D array): temperature array (K) [Nlayer]
+        nu_grid (1D array): wavenumber array (cm-1) [Nnus]
         nucia: CIA wavenumber (cm-1)
         tcia: CIA temperature (K)
         logac: log10 cia coefficient
 
     Returns:
-        logac(Tarr, nus)
+        logarithm of absorption coefficient [Nlayer, Nnus] in the unit of cm5
 
     Example:
-        >>> nucia,tcia,ac=read_cia("../../data/CIA/H2-H2_2011.cia",nus[0]-1.0,nus[-1]+1.0)
-        >>> logac=jnp.array(np.log10(ac))
-        >>> interp_logacia_matrix(Tarr,nus,nucia,tcia,logac)
+        nucia,tcia,ac=read_cia("../../data/CIA/H2-H2_2011.cia",nus[0]-1.0,nus[-1]+1.0)
+        logac=jnp.array(np.log10(ac))
+        interp_logacia_matrix(Tarr,nus,nucia,tcia,logac)
     """
 
     def fcia(x, i):
@@ -114,7 +114,8 @@ def interp_logacia_vector(T, nu_grid, nucia, tcia, logac):
         logac: log10 cia coefficient
 
     Returns:
-        vector logac(T, nus)
+        logarithm of absorption coefficient [Nnus] at T in the unit of cm5
+        
 
     """
 
