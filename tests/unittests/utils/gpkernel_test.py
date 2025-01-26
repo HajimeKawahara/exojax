fdc401bb6ea235a280d1670f9c3831952f65a08d
Hajime Kawahara <divrot@gmail.com>
2025-01-26 18:54:20 +0900

    png and abs in pytest

--- tests/unittests/utils/gpkernel_test.py
+++ tests/unittests/utils/gpkernel_test.py
@@ -14,7 +14,7 @@ def test_gpkernel_RBF():
     amplitude = 1.0
     err = jnp.array([0.1, 0.1, 0.1])
     cov = gpkernel_RBF(x, scale, amplitude, err)
-    assert np.sum(cov) == pytest.approx(5.7267933, 1e-5)
+    assert np.sum(cov) == pytest.approx(5.7267933, abs=1e-5)
 
 
 def test_gpkernel_RBF_cross():
@@ -25,7 +25,7 @@ def test_gpkernel_RBF_cross():
     scale = 1.0
     amplitude = 1.0
     cov = gpkernel_RBF_cross(x, y, scale, amplitude)
-    assert np.sum(cov) == pytest.approx(5.801156, 1e-5)
+    assert np.sum(cov) == pytest.approx(5.801156, abs=1e-5)
 
 
 def test_average_covariance_gpmodel():
@@ -40,8 +40,8 @@ def test_average_covariance_gpmodel():
     average, covariance = average_covariance_gpmodel(
         x, data, model, scale, amplitude, err
     )
-    assert np.sum(average) == pytest.approx(6.0979223, 1e-5)
-    assert np.sum(covariance) == pytest.approx(0.059825122, 1e-5)
+    assert np.sum(average) == pytest.approx(6.0979223, abs=1e-5)
+    assert np.sum(covariance) == pytest.approx(0.059825122, abs=1e-5)
 
 
 def test_average_covariance_gpmodel_cross():
@@ -60,8 +60,8 @@ def test_average_covariance_gpmodel_cross():
     average, covariance = average_covariance_gpmodel_cross(
         x, y, data, model_x, model_y, scale, amplitude, err_x, err_y
     )
-    assert np.sum(average) == pytest.approx(12.213027, 1e-5)
-    assert np.sum(covariance) == pytest.approx(1.5004411, 1e-5)
+    assert np.sum(average) == pytest.approx(12.213027, abs=1e-5)
+    assert np.sum(covariance) == pytest.approx(1.5004411, abs=1e-5)
 
 
 if __name__ == "__main__":
