08cea3b87f00894e1479f4d8f56197a00021a450
Hajime Kawahara <divrot@gmail.com>
2025-02-05 10:42:03 +0900

    kernel for comparison (tildevk)

--- tests/unittests/spec/dit/ditkernel_test.py
+++ tests/unittests/spec/dit/ditkernel_test.py
@@ -1,32 +1,29 @@
 import pytest
 import jax.numpy as jnp
-from exojax.test.emulate_mdb import mock_mdbExomol
+import numpy as np
 from exojax.test.emulate_mdb import mock_wavenumber_grid
-from exojax.spec.set_ditgrid import ditgrid_log_interval
-from exojax.spec.exomol import gamma_exomol
 from exojax.spec.hitran import normalized_doppler_sigma
 from exojax.spec.ditkernel import fold_voigt_kernel_logst
-from exojax.spec.initspec import init_modit
 from jax import config
 
 
 def test_fold_voigt_kernel_logst():
-    
+
     config.update("jax_enable_x64", True)
-    mdb = mock_mdbExomol()
-    nu_grid, wav, res = mock_wavenumber_grid()
+    nu_grid, wav, resolution = mock_wavenumber_grid()
     Ttest = 1200.0
-    P = 1.0
-    # MODIT manual
-    cont, index, R, pmarray = init_modit(mdb.nu_lines, nu_grid)
-    nsigmaD = normalized_doppler_sigma(Ttest, mdb.molmass, R)
-    # log ngamma grid construction
-    gammaL = gamma_exomol(P, Ttest, mdb.n_Texp, mdb.alpha_ref)
-    dv_lines = mdb.nu_lines / R
-    ngammaL = gammaL / dv_lines
-    ngammaL_grid = ditgrid_log_interval(ngammaL, dit_grid_resolution=0.2)
+    pmarray = np.ones(len(nu_grid) + 1)
+    pmarray[1::2] = pmarray[1::2] * -1.0
+    #### molmass and ngammaL_grid are based on the following code ###
+    # mdb = mock_mdbExomol()
+    # gammaL = gamma_exomol(P, Ttest, mdb.n_Texp, mdb.alpha_ref)
+    # dv_lines = mdb.nu_lines / resolution
+    # ngammaL = gammaL / dv_lines
+    # ngammaL_grid = ditgrid_log_interval(ngammaL, dit_grid_resolution=0.2)
+    molmass = 28.0101  # mdb.molmass
+    nsigmaD = normalized_doppler_sigma(Ttest, molmass, resolution)
+    ngammaL_grid = jnp.array([16.23074604, 18.25429762, 20.53013342])
     log_ngammaL_grid = jnp.log(ngammaL_grid)
-    _, _, R, pmarray = init_modit(mdb.nu_lines, nu_grid)
     Ng_nu = len(nu_grid)
 
     vk = fold_voigt_kernel_logst(
@@ -36,19 +33,19 @@ def test_fold_voigt_kernel_logst():
         Ng_nu,
         pmarray,
     )
-    
-    ref = [362.08269655,326.81894669,294.27354816] # feb 5th 2025
-    assert vk.shape == (len(nu_grid)+1, len(log_ngammaL_grid))
+
+    ref = [362.08269655, 326.81894669, 294.27354816]  # feb 5th 2025
+    assert vk.shape == (len(nu_grid) + 1, len(log_ngammaL_grid))
     assert jnp.all(jnp.sum(vk, axis=0) == pytest.approx(ref))
     return vk
 
+
 if __name__ == "__main__":
-    vk = test_fold_voigt_kernel_logst()
     import matplotlib.pyplot as plt
-    plt.plot(vk[:,0],label="0",alpha=0.3)
-    plt.plot(vk[:,1],label="1",alpha=0.2)
-    plt.plot(vk[:,2],label="2",alpha=0.1)
+    vk = test_fold_voigt_kernel_logst()
+    plt.plot(vk[:, 0], label="0", alpha=0.3)
+    plt.plot(vk[:, 1], label="1", alpha=0.2)
+    plt.plot(vk[:, 2], label="2", alpha=0.1)
     plt.yscale("log")
     plt.legend()
     plt.savefig("fold_voigt_kernel_logst.png")
-
