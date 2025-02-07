5c93c829bbae44ebd0ce36714a3ecd1d60498d8b
Hajime Kawahara <divrot@gmail.com>
2025-02-07 17:18:44 +0900

    change opamodit default

--- src/exojax/spec/modit_scanfft.py
+++ src/exojax/spec/modit_scanfft.py
@@ -1,6 +1,6 @@
 """Line profile computation using Discrete Integral Transform using zeroscan, scan+fft (#277).
 
-* Currently, zeroscan is the default method
+* Currently, zeroscan is the default method, scanfft is deprecated.
 
 * MODIT using zeroscan/scan+fft allows > 4GB fft memory
 
@@ -92,6 +92,44 @@ def xsvector_zeroscan(
     )
     return xs
 
+def xsmatrix_zeroscan(
+    cnu, indexnu, R, pmarray, nsigmaDl, ngammaLM, SijM, nu_grid, dgm_ngammaL
+):
+    """Cross section matrix for xsvector (MODIT), zeroscan
+
+    Args:
+       cnu: contribution by npgetix for wavenumber
+       indexnu: index by npgetix for wavenumber
+       R: spectral resolution
+       pmarray: (+1,-1) array whose length of len(nu_grid)+1
+       nu_lines: line center (Nlines)
+       nsigmaDl: normalized doppler sigma in layers in R^(Nlayer x 1)
+       ngammaLM: gamma factor matrix in R^(Nlayer x Nline)
+       SijM: line strength matrix in R^(Nlayer x Nline)
+       nu_grid: linear wavenumber grid
+       dgm_ngammaL: DIT Grid Matrix for normalized gammaL R^(Nlayer, NDITgrid)
+
+    Return:
+       cross section matrix in R^(Nlayer x Nwav)
+    """
+    NDITgrid = jnp.shape(dgm_ngammaL)[1]
+    Nline = len(cnu)
+    Mat = jnp.hstack([nsigmaDl, ngammaLM, SijM, dgm_ngammaL])
+
+    def fxs(x, arr):
+        carry = 0.0
+        nsigmaD = arr[0:1]
+        ngammaL = arr[1 : Nline + 1]
+        Sij = arr[Nline + 1 : 2 * Nline + 1]
+        ngammaL_grid = arr[2 * Nline + 1 : 2 * Nline + NDITgrid + 1]
+        arr = xsvector_zeroscan(
+            cnu, indexnu, R, pmarray, nsigmaD, ngammaL, Sij, nu_grid, ngammaL_grid
+        )
+        return carry, arr
+
+    val, xsm = scan(fxs, 0.0, Mat)
+    return xsm
+
 
 def calc_xsection_from_lsd_scanfft(
     Slsd, R, pmarray, nsigmaD, nu_grid, log_ngammaL_grid
@@ -134,6 +172,7 @@ def calc_xsection_from_lsd_scanfft(
     # convolves
     fftvalsum = jnp.sum(fftval * vk, axis=(1,))
     return jnp.fft.irfft(fftvalsum)[:Ng_nu] * R / nu_grid
+
 
 
 @jit
