35fee3e397bca67cc3ecd2c3f75ee77771cf144a
Hajime Kawahara <divrot@gmail.com>
2025-02-11 11:52:35 +0900

    open filter setting done

--- src/exojax/spec/lpffilter.py
+++ src/exojax/spec/lpffilter.py
@@ -13,7 +13,8 @@ def generate_closed_lpffilter(filter_length, nsigmaD, ngammaL):
         The filter structure is filter[1:M] = vkfilter[M+1:][::-1]m where M=N/2
         filter[0] is the DC component, Nyquist component.
         filter[M] is the Nyquist component. 
-
+        The dimension of the closed lpf filter is even number.
+        
     Returns:
         array: closed lpf filter [2*filter_length]
     """
@@ -25,14 +26,17 @@ def generate_closed_lpffilter(filter_length, nsigmaD, ngammaL):
 def generate_open_lpffilter(nfilter, nsigmaD, ngammaL):
     """Generates the open form LPF filter
 
+    Notes:
+        The dimension of the open lpf filter is odd number to ensure asymmetry. This forces fft_length to be even number when the dimension of signal is even number.
+
     Args:
         nfilter (int): length of the wavenumber grid of lpffilter
         nsigmaD (float): normalized gaussian standard deviation, resolution*betaT/nu betaT is the STD of Doppler broadening
         ngammaL (float): normalized Lorentzian half width
 
     Returns:
-        array: open lpf filter [2*filter_length]
+        array: open lpf filter [2*filter_length+1]
     """
-    dq = jnp.arange(-nfilter, nfilter)
+    dq = jnp.arange(-nfilter, nfilter+1)
     return voigt(dq, nsigmaD, ngammaL)
     
