04c762c757a1c28237dfa179c353c331144cd644
Hajime Kawahara <divrot@gmail.com>
2025-02-22 19:12:54 +0900

    two species

--- src/exojax/test/generate.py
+++ src/exojax/test/generate.py
@@ -1,62 +1,74 @@
 """generate test data for exomol
 
 """
+
 from exojax.spec import api
 from exojax.test.emulate_mdb import mock_wavenumber_grid
 import pathlib
+import numpy as np
 import os
 from jax import config
 
 config.update("jax_enable_x64", True)
 
+sample_directory_name = {"CO": "CO/12C-16O/SAMPLE", "H2O": "H2O/1H2-16O/SAMPLE"}
 
-def gendata_moldb():
-    """generate test data for CO exomol
-    """
+
+def gendata_moldb(molecules="CO"):
+    """generate test data for CO exomol"""
     nus, wav, res = mock_wavenumber_grid()
-    mdbCO = api.MdbExomol('.database/CO/12C-16O/Li2015',
-                          nus,
-                          inherit_dataframe=True,
-                          gpu_transfer=True)
 
-    trans_filename = "12C-16O__SAMPLE.trans"
-    mask = (mdbCO.df["nu_lines"] <= nus[-1]) * (mdbCO.df["nu_lines"] >= nus[0])
-    maskeddf = mdbCO.df[mask]
-    directory_name = "CO/12C-16O/SAMPLE"
-    if not os.path.exists(directory_name):
-        os.makedirs(directory_name)
+    exomoldb = {
+        "CO": ".database/CO/12C-16O/Li2015",
+        "H2O": ".database/H2O/1H2-16O/POKAZATEL",
+    }
+    line_strength_criterion = {"CO": 0.0, "H2O": 1.0e-30}
+    mdb = api.MdbExomol(
+        exomoldb[molecules], nus, inherit_dataframe=True, gpu_transfer=True
+    )
+    line_mask = mdb.df["Sij0"] > line_strength_criterion[molecules]
+    trans_filename = {"CO": "12C-16O__SAMPLE.trans", "H2O": "1H2-16O__SAMPLE.trans"}
+    mask = (mdb.df["nu_lines"] <= nus[-1]) * (mdb.df["nu_lines"] >= nus[0]) * line_mask
+    maskeddf = mdb.df[mask]
+    if not os.path.exists(sample_directory_name[molecules]):
+        os.makedirs(sample_directory_name[molecules])
     cdir = os.getcwd()
-    os.chdir(directory_name)
-    save_trans(trans_filename, maskeddf)
+    os.chdir(sample_directory_name[molecules])
+    save_trans(trans_filename[molecules], maskeddf)
     os.chdir(cdir)
 
 
-def make_hdf():
-    path = "CO/12C-16O/SAMPLE"
-    path = pathlib.Path(path).expanduser()
+def make_hdf(molecule):
+    path = pathlib.Path(sample_directory_name[molecule]).expanduser()
     nus, wav, res = mock_wavenumber_grid()
-    mdbCO = api.MdbExomol(str(path),
-                          nus,
-                          inherit_dataframe=True,
-                          gpu_transfer=True)
+    mdb = api.MdbExomol(str(path), nus, inherit_dataframe=True, gpu_transfer=True)
 
 
 def save_trans(trans_filename, maskeddf):
-    maskeddf.export_csv(trans_filename,
-                        columns=["i_upper", "i_lower", "A", "nu_lines"],
-                        sep="\t",
-                        header=False)
+    maskeddf.export_csv(
+        trans_filename,
+        columns=["i_upper", "i_lower", "A", "nu_lines"],
+        sep="\t",
+        header=False,
+    )
     import bz2
-    with open(trans_filename, 'rb') as f_in:
-        with bz2.open(trans_filename + ".bz2", 'wb') as f_out:
+
+    with open(trans_filename, "rb") as f_in:
+        with bz2.open(trans_filename + ".bz2", "wb") as f_out:
             f_out.writelines(f_in)
 
 
 if __name__ == "__main__":
     # regenerate hdf for exomol
-    gendata_moldb()
+
+    molecule = "H2O"
+    gendata_moldb(molecule)
     print(
-        "cp some files from data/testdata/CO/12C-16O/SAMPLE/ to CO/12C-16O/SAMPLE/, then do make_hdf"
+        "cp some files from data/testdata/"
+        + sample_directory_name[molecule]
+        + " to "
+        + sample_directory_name[molecule]
+        + ", then do make_hdf"
     )
-    make_hdf()
-    print(">cp -rf CO ../data/testdata/")
+    make_hdf(molecule)
+    print(">cp -rf " + molecule + " ../data/testdata/")
