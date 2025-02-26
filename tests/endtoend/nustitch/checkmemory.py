276ff2ede79af390de3adc431333ef57ab96369a
Hajime Kawahara <divrot@gmail.com>
2025-02-26 21:51:39 +0900

    checkmemory.py

--- tests/endtoend/nustitch/checkmemory.py
+++ tests/endtoend/nustitch/checkmemory.py
@@ -1,3 +1,9 @@
+"""
+Example that OpaPremoditStitch can compute cross section matrix, but ends up OoM when using PreModit
+tested using A100 40GB
+"""
+
+
 from exojax.spec.opastitch import OpaPremoditStitch
 from exojax.spec.opacalc import OpaPremodit
 from exojax.utils.grids import wavenumber_grid
@@ -15,13 +21,17 @@ N=30000
 nus, wav, res = wavenumber_grid(22000.0, 22500.0, N, unit="AA", xsmode="premodit")
 mdb = MdbExomol(".database/H2O/1H2-16O/POKAZATEL", nus)
 
-nlayer = 10
+nlayer = 10000
 Tarr = jnp.ones(nlayer)*1000.0
 Parr = jnp.ones(nlayer)
+print(Tarr.shape)
 
-opa = OpaPremodit(mdb, nus, auto_trange=[500,1300], alias="close", dit_grid_resolution=1.0)
-xsm = opa.xsmatrix(Tarr, Parr)
-exit()
+# OoM
+#opa = OpaPremodit(mdb, nus, auto_trange=[500,1300], alias="close", dit_grid_resolution=1.0)
+#xsm = opa.xsmatrix(Tarr, Parr)
+
+# can compute
+ndiv=20
 opas = OpaPremoditStitch(mdb, nus, ndiv, auto_trange=[500,1300], cutwing = 0.5)
 Tarr = jnp.array([1000.0, 1100.0])
 Parr = jnp.array([1.0, 1.5])
