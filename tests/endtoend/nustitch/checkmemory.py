bc223e9454fc3e0f69401c092b8529a4f54245c2
Hajime Kawahara <divrot@gmail.com>
2025-02-26 18:10:31 +0900

    checkmemory

--- tests/endtoend/nustitch/checkmemory.py
+++ tests/endtoend/nustitch/checkmemory.py
@@ -5,17 +5,21 @@ from exojax.spec.api import MdbExomol
 import numpy as np
 import jax.numpy as jnp
 
+from jax_smi import initialise_tracking
+initialise_tracking()
+
 from jax import config 
 config.update("jax_enable_x64", True)
-N=100000
-nus, wav, res = wavenumber_grid(22000.0, 24000.0, N, unit="AA", xsmode="premodit")
+
+N=30000
+nus, wav, res = wavenumber_grid(22000.0, 22500.0, N, unit="AA", xsmode="premodit")
 mdb = MdbExomol(".database/H2O/1H2-16O/POKAZATEL", nus)
 
-nlayer = 200
+nlayer = 10
 Tarr = jnp.ones(nlayer)*1000.0
 Parr = jnp.ones(nlayer)
 
-opa = OpaPremodit(mdb, nus, auto_trange=[500,1300], alias="close")
+opa = OpaPremodit(mdb, nus, auto_trange=[500,1300], alias="close", dit_grid_resolution=1.0)
 xsm = opa.xsmatrix(Tarr, Parr)
 exit()
 opas = OpaPremoditStitch(mdb, nus, ndiv, auto_trange=[500,1300], cutwing = 0.5)
