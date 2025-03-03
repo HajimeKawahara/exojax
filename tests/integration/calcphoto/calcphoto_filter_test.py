5b5a8533732bbb42af5e0b64abfdc2475a585d43
Hajime Kawahara <divrot@gmail.com>
2025-03-03 09:27:46 +0900

    wav2nu, nu2wav, with values option

---
+++ tests/integration/calcphoto/calcphoto_filter_test.py
@@ -0,0 +1,69 @@
+import jax.numpy as jnp
+import numpy as np
+from exojax.utils.grids import wavenumber_grid
+from exojax.spec.unitconvert import wav2nu
+#from astropy import units as u
+
+#http://svo2.cab.inta-csic.es/theory/fps/
+up_resolution_factor = 2**5
+filter_name = '2MASS/2MASS.H'
+from astroquery.svo_fps import SvoFps
+data = SvoFps.get_transmission_data(filter_name)
+unit = str(data['Wavelength'].unit)
+wl_ref = data['Wavelength']
+nu_ref, transmission_ref = wav2nu(np.array(wl_ref), unit=unit, values=np.array(data['Transmission']))
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(211)
+plt.plot(data['Wavelength'], data['Transmission'])
+ax = fig.add_subplot(212)
+plt.plot(nu_ref, transmission_ref)
+plt.show()
+
+
+print(nu_ref)
+exit()
+#####
+wl_min = np.min(wl_ref)
+wl_max = np.max(wl_ref)
+dlmd = (wl_max - wl_min) / len(wl_ref)
+Rinst_p = 0.5 * (wl_min + wl_max) / dlmd
+R = Rinst_p * up_resolution_factor
+
+nu_min = 1.0e8/(wl_max + 5.0)
+nu_max = 1.0e8/(wl_min - 5.0)
+Nx = np.ceil(R * np.log(nu_max/nu_min)) + 1 # ueki                                                                
+Nx = np.ceil(Nx/2.) * 2 # make even  
+nus_k,wav_k,res_k = wavenumber_grid(wl_min-5.,wl_max+5.,Nx,unit="AA",xsmode="premodit")
+
+#from scipy import interpolate
+#f = interpolate.interp1d(data['Wavelength'], data['Transmission'])
+#tr = f(wavd_p)
+
+
+tr = np.interp(wav_k, wl_ref, transmission_ref)
+
+
+
+
+def calc_photo(mu, f_ref=1.0):
+    mu = jnp.concatenate(mu)
+    mu = mu * f_ref # [erg/s/cm^2/cm^{-1}]                                                                              
+    # [erg/s/cm^2/cm^{-1}] => [erg/s/cm^2/cm]                                                                           
+    mu = mu / (jnp.concatenate(wavd_p)*1.0e-8)**2.0e0
+    # [erg/s/cm^2/cm] => [W/m^2/um]                                                                                     
+    mu = mu * 1.0e-7 * 1.0e4 * 1.0e-4
+
+    fdl = jnp.trapz(mu*jnp.concatenate(tr), jnp.concatenate(wavd_p))
+    dl = jnp.trapz(jnp.concatenate(tr), jnp.concatenate(wavd_p))
+    f = fdl / dl
+
+    H_mag = -2.5 * jnp.log10(f / f0)
+
+    return H_mag
+
+
+
+
--- tests/unittests/utils/unitconvert_test.py
+++ tests/unittests/utils/unitconvert_test.py
@@ -4,36 +4,54 @@ from exojax.spec.unitconvert import nu2wav
 import pytest
 import numpy as np
 
+
 def test_wav2nu_single():
     wav = 20000.0  # AA
     assert wav2nu(wav, unit="AA") == pytest.approx(5000.0)
 
+
 def test_wav2nu_check_nus_is_in_ascending():
-    wav_ascending = np.array([10000.0,20000.0])  # AA
-    wav_descending = np.array([20000.0,10000.0])  # AA
-    
-    nus_ref = np.array([5000.0,10000.0])  # cm-1
-    
+    wav_ascending = np.array([10000.0, 20000.0])  # AA
+    wav_descending = np.array([20000.0, 10000.0])  # AA
+
+    nus_ref = np.array([5000.0, 10000.0])  # cm-1
+
     assert np.all(wav2nu(wav_ascending, unit="AA") == nus_ref)
     assert np.all(wav2nu(wav_descending, unit="AA") == nus_ref)
-    
+
+
 def test_nu2wav_single():
     nus_single = 5000.0
     assert nu2wav(nus_single) == pytest.approx(20000.0)
 
+
+def test_nu2wav_single_with_values():
+    nus_single = 5000.0
+    _, vals = nu2wav(nus_single, values=1.0)
+    assert vals == 1.0 
+
+
+def test_wav2nus_single_with_values():
+    wav_single = 20000.0
+    _, vals = nu2wav(wav_single, values=1.0)
+    assert vals == 1.0 
+
+
 def test_nu2wav_ascending():
-    nus_ascending = np.array([5000.0,10000.0])  # cm-1
-    wav_ascending = np.array([10000.0,20000.0])  # AA
-    wav_descending = np.array([20000.0,10000.0])  # AA
-    
+    nus_ascending = np.array([5000.0, 10000.0])  # cm-1
+    wav_ascending = np.array([10000.0, 20000.0])  # AA
+    wav_descending = np.array([20000.0, 10000.0])  # AA
+
     assert np.all(nu2wav(nus_ascending, wavelength_order="ascending") == wav_ascending)
-    assert np.all(nu2wav(nus_ascending, wavelength_order="descending") == wav_descending)
+    assert np.all(
+        nu2wav(nus_ascending, wavelength_order="descending") == wav_descending
+    )
 
 
 def test_nu2wav_when_nus_input_is_ascending_or_single_then_value_error():
-    nus_descending = np.array([10000.0,5000.0])  # cm-1
-    nus_disorder = np.array([10000.0,20000.0, 5000.0])  # cm-1
-    
+    nus_descending = np.array([10000.0, 5000.0])  # cm-1
+    nus_disorder = np.array([10000.0, 20000.0, 5000.0])  # cm-1
+
     with pytest.raises(ValueError):
         nu2wav(nus_descending, wavelength_order="ascending")
     with pytest.raises(ValueError):
@@ -44,7 +62,34 @@ def test_nu2wav_when_nus_input_is_ascending_or_single_then_value_error():
         nu2wav(nus_disorder, wavelength_order="descending")
 
 
+def test_nu2wav_ascending_with_values():
+    nus_ascending = np.array([5000.0, 10000.0])  # cm-1
+    vals = np.array([1.0, 2.0])
+
+    _, vals_as = nu2wav(nus_ascending, wavelength_order="ascending", values=vals)
+    _, vals_des = nu2wav(nus_ascending, wavelength_order="descending", values=vals)
+
+    assert np.all(vals_as == vals[::-1])
+    assert np.all(vals_des == vals)
+
+
+def test_wav2nu_with_values():
+    wav_ascending = np.array([10000.0, 20000.0])  # AA
+    wav_descending = np.array([20000.0, 10000.0])  # AA
+    vals = np.array([1.0, 2.0])
+
+    _, vals_as = wav2nu(wav_ascending, unit="AA", values=vals)
+    _, vals_des = wav2nu(wav_descending, unit="AA", values=vals)
+
+    assert np.all(vals_as == vals[::-1])
+    assert np.all(vals_des == vals)
+
+
 if __name__ == "__main__":
     test_wav2nu_single()
     test_wav2nu_check_nus_is_in_ascending()
     test_nu2wav_when_nus_input_is_ascending_or_single_then_value_error()
+    test_nu2wav_ascending_with_values()
+    test_wav2nu_with_values()
+    test_nu2wav_single_with_values()
+    test_wav2nus_single_with_values()
