46861317b10cdab60ed1a7f77544f439b6b6ee19
HajimeKawahara <divrot@gmail.com>
2025-06-06 10:58:55 +0900

    temp

--- tests/integration/corrk/corrk_test.py
+++ tests/integration/corrk/corrk_test.py
@@ -12,3 +12,12 @@ nus, wav, res = wavenumber_grid(6400.0, 6800.0, N, unit="cm-1", xsmode = "premod
 print("resolution = ", res)
 mdb = MdbExomol(".databases/H2O/1H2-16O/POKAZATEL/",nus)
 opa = OpaPremodit(mdb, nus, auto_trange=[1000.0,1100.0])
+
+T=1050.0
+P=0.1
+xsv = opa.xsvector(T, P)
+
+import matplotlib.pyplot as plt
+plt.plot(wav, xsv)
+plt.savefig("corrk_test.png")
+plt.close()
