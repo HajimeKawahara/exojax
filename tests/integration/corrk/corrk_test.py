c845978d8bfd7ddbb61db42de7d238e16715b6f9
HajimeKawahara <divrot@gmail.com>
2025-06-10 08:51:31 +0900

    fig update

--- tests/integration/corrk/corrk_test.py
+++ tests/integration/corrk/corrk_test.py
@@ -32,7 +32,7 @@ idx = jnp.argsort(xsv)
 k_g = xsv[idx]
 g = jnp.arange(xsv.size, dtype=xsv.dtype) / xsv.size
 
-#segments
+# segments
 Ng = 10
 edges = jnp.linspace(0.0, 1.0, Ng + 1)  # 0,1/Ng,…,1
 cut_idx = jnp.searchsorted(g, edges)  # shape (Ng+1,)
@@ -47,8 +47,9 @@ k_high = k_g[cut_idx[j + 1] - 1]
 k_med = k_low + (k_high - k_low) * 0.5
 mask = (xsv >= k_low) & (xsv < k_high)
 y_base = xsv.min() * 0.5 * jnp.ones_like(k_low)
+y_base_ = xsv.min() * 0.5 * jnp.ones(2)
 
-fig = plt.figure(figsize=(10, 3.5))
+fig = plt.figure(figsize=(13, 3.5))
 plt.subplot(1, 2, 1)
 plt.plot(nus, xsv, label="xsv")
 plt.fill_between(
@@ -61,25 +62,47 @@ plt.fill_between(
     color="tab:orange",
     alpha=0.35,
 )
-plt.plot(nus_segments[j], xsv_segments[j], ".")
+plt.plot(nus_segments[j], xsv_segments[j], ".", lw=0.5)
 plt.axhline(xsv_segments[j].max(), alpha=0.3, color="gray")
 plt.axhline(xsv_segments[j].min(), alpha=0.3, color="gray")
 
 plt.ylim(xsv.min() * 0.5, xsv.max() * 2)
 plt.yscale("log")
 plt.xlabel("$\\nu$ (cm-1)")
-plt.ylabel("k($\\nu$)")
+plt.ylabel("$\\sigma(\\nu) (\\mathrm{cm}^{2})$")
 
 plt.subplot(1, 2, 2)
 plt.plot(g, k_g, label="xsv sorted")
+plt.plot(g[cut_idx[j] : cut_idx[j + 1]], xsv_segments[j], ".", lw=0.5)
+
 plt.ylim(xsv.min() * 0.5, xsv.max() * 2)
 plt.axhline(xsv_segments[j].max(), alpha=0.3, color="gray")
 plt.axhline(xsv_segments[j].min(), alpha=0.3, color="gray")
 plt.axvline(edges[j], alpha=0.3, color="gray")
-plt.axvline(edges[j+1], alpha=0.3, color="gray")
+plt.axvline(edges[j + 1], alpha=0.3, color="gray")
 
+plt.fill_between(
+    # nus, k_low, k_high,
+    [edges[j], edges[j + 1]],
+    y_base,
+    k_med,
+    step="mid",
+    color="tab:orange",
+    alpha=0.35,
+)
+
+plt.text(
+    (edges[j] + edges[j + 1]) / 2,
+    y_base,
+    "$\\Delta g_j$",
+    horizontalalignment="center",
+    verticalalignment="bottom",
+    fontsize=12,
+    color="k"
+)
+plt.plot(edges[j : j + 2], y_base_, color="k", lw=3)
 plt.yscale("log")
 plt.xlabel("g")
-plt.ylabel("k(g)")
+plt.ylabel("$\\sigma(g) (\\mathrm{cm}^{2})$")
 plt.savefig("corrk_test.png", dpi=300, bbox_inches="tight")
 plt.show()
