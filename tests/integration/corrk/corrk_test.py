06335038d3ad627326826e10f5a728dc6295a6d0
Hajime Kawahara <divrot@gmail.com>
2025-06-10 10:56:49 +0900

    inter layer figure

--- tests/integration/corrk/corrk_test.py
+++ tests/integration/corrk/corrk_test.py
@@ -1,5 +1,6 @@
 # correlated k distribution tests
 
+from tkinter import font
 from exojax.utils.grids import wavenumber_grid
 from exojax.spec.api import MdbExomol
 import numpy as np
@@ -23,36 +24,123 @@ fig = False
 nus, wav, res = mock_wavenumber_grid(lambda0=22930.0, lambda1=22940.0, Nx=2000)
 mdb = mock_mdbExomol("H2O")
 
+opa = OpaPremodit(mdb, nus, auto_trange=[500.0, 1500.0])
+
 T = 1000.0
 P = 1.0e-2
-opa = OpaPremodit(mdb, nus, auto_trange=[1000.0, 1100.0])
-
 xsv = opa.xsvector(T, P)
-idx = jnp.argsort(xsv)
-k_g = xsv[idx]
-g = jnp.arange(xsv.size, dtype=xsv.dtype) / xsv.size
+
+def computeg(nus, xsv, Ng = 10):
+    idx = jnp.argsort(xsv)
+    k_g = xsv[idx]
+    g = jnp.arange(xsv.size, dtype=xsv.dtype) / xsv.size
 
 # segments
-Ng = 10
-edges = jnp.linspace(0.0, 1.0, Ng + 1)  # 0,1/Ng,…,1
-cut_idx = jnp.searchsorted(g, edges)  # shape (Ng+1,)
+    edges = jnp.linspace(0.0, 1.0, Ng + 1)  # 0,1/Ng,…,1
+    cut_idx = jnp.searchsorted(g, edges)  # shape (Ng+1,)
 
-nus_segments = [nus[idx[cut_idx[i] : cut_idx[i + 1]]] for i in range(Ng)]
-xsv_segments = [xsv[idx[cut_idx[i] : cut_idx[i + 1]]] for i in range(Ng)]
-cut_idx = np.linspace(0, len(k_g), Ng + 1, dtype=int)
+    nus_segments = [nus[idx[cut_idx[i] : cut_idx[i + 1]]] for i in range(Ng)]
+    xsv_segments = [xsv[idx[cut_idx[i] : cut_idx[i + 1]]] for i in range(Ng)]
+    cut_idx = np.linspace(0, len(k_g), Ng + 1, dtype=int)
 
-j = 6
-k_low = k_g[cut_idx[j]]
-k_high = k_g[cut_idx[j + 1] - 1]
-k_med = k_low + (k_high - k_low) * 0.5
-mask = (xsv >= k_low) & (xsv < k_high)
-y_base = xsv.min() * 0.5 * jnp.ones_like(k_low)
-y_base_ = xsv.min() * 0.5 * jnp.ones(2)
+    j = 6
+    k_low = k_g[cut_idx[j]]
+    k_high = k_g[cut_idx[j + 1] - 1]
+    k_med = k_low + (k_high - k_low) * 0.5
+    mask = (xsv >= k_low) & (xsv < k_high)
+    y_base = xsv.min() * 0.5 * jnp.ones_like(k_low)
+    y_top = xsv.max() * 2.0 * jnp.ones_like(k_low)
+    y_base_ = xsv.min() * 0.5 * jnp.ones(2)
+    return k_g,g,edges,cut_idx,nus_segments,xsv_segments,j,k_med,mask,y_base,y_base_,y_top
+
+
+def plotxsv_1(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base, text, ax):
+    plt.plot(nus, xsv, label="xsv", alpha=0.3)
+    plt.fill_between(
+    # nus, k_low, k_high,
+    nus,
+    y_base,
+    y_top,
+    where=mask,
+    step="mid",
+    color="tab:orange",
+    alpha=0.35,
+)
+    plt.plot(nus_segments[j], xsv_segments[j], ".", lw=0.5)
+    plt.axhline(xsv_segments[j].max(), alpha=0.3, color="gray")
+    plt.axhline(xsv_segments[j].min(), alpha=0.3, color="gray")
+    ax.set_title(text, fontsize=8, loc="left", pad=0.5)
+    plt.ylim(xsv.min() * 0.5, xsv.max() * 2)
+    plt.yscale("log")
+    plt.ylabel("$\\sigma(\\nu) (\\mathrm{cm}^{2})$")
+
 
 fig = plt.figure(figsize=(13, 3.5))
-plt.subplot(1, 2, 1)
-plt.plot(nus, xsv, label="xsv")
-plt.fill_between(
+
+ax = plt.subplot(3, 2, 1)
+T = 700.0
+P = 1.0e-1
+xsv = opa.xsvector(T, P)
+k_g, g, edges, cut_idx, nus_segments, xsv_segments, j, k_med, mask, y_base, y_base_, y_top = computeg(nus, xsv)
+plotxsv_1(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base, "T=700 K, P=0.1 bar", ax)
+ax.xaxis.set_ticklabels([])
+ax.axes.get_xaxis().set_ticks([])
+
+
+ax = plt.subplot(3, 2, 3)
+T = 1000.0
+xsv = opa.xsvector(T, P)
+k_g, g, edges, cut_idx, nus_segments, xsv_segments, j, k_med, mask, y_base, y_base_, y_top = computeg(nus, xsv)
+plotxsv_1(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base, "T=1000 K, P=0.1 bar", ax)
+ax.xaxis.set_ticklabels([])
+ax.axes.get_xaxis().set_ticks([])
+
+
+ax = plt.subplot(3, 2, 5)
+T = 1300.0
+xsv = opa.xsvector(T, P)
+k_g, g, edges, cut_idx, nus_segments, xsv_segments, j, k_med, mask, y_base, y_base_, y_top = computeg(nus, xsv)
+plotxsv_1(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base, "T=1300 K, P=0.1 bar", ax)
+ax.set_xlabel("$\\nu$ (cm-1)")
+
+ax = plt.subplot(3, 2, 2)
+T = 1000.0
+P = 1.0e-2
+xsv = opa.xsvector(T, P)
+k_g, g, edges, cut_idx, nus_segments, xsv_segments, j, k_med, mask, y_base, y_base_, y_top = computeg(nus, xsv)
+plotxsv_1(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base, "T=1000 K, P=0.01 bar", ax)
+ax.xaxis.set_ticklabels([])
+ax.axes.get_xaxis().set_ticks([])
+
+
+ax = plt.subplot(3, 2, 4)
+P = 1.0e-2
+xsv = opa.xsvector(T, P)
+k_g, g, edges, cut_idx, nus_segments, xsv_segments, j, k_med, mask, y_base, y_base_, y_top = computeg(nus, xsv)
+plotxsv_1(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base, "T=1000 K, P=0.1 bar", ax)
+ax.xaxis.set_ticklabels([])
+ax.axes.get_xaxis().set_ticks([])
+
+
+ax = plt.subplot(3, 2, 6)
+P = 1.0
+xsv = opa.xsvector(T, P)
+k_g, g, edges, cut_idx, nus_segments, xsv_segments, j, k_med, mask, y_base, y_base_, y_top = computeg(nus, xsv)
+plotxsv_1(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base, "T=1000 K, P=1 bar", ax)
+ax.set_xlabel("$\\nu$ (cm-1)")
+plt.savefig("corrk_corr.png", dpi=300, bbox_inches="tight")
+plt.show()
+
+
+T = 1000.0
+P = 1.0e-2
+xsv = opa.xsvector(T, P)
+k_g, g, edges, cut_idx, nus_segments, xsv_segments, j, k_med, mask, y_base, y_base_, y_top = computeg(nus, xsv)
+
+
+def plotxsv(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base):
+    plt.plot(nus, xsv, label="xsv")
+    plt.fill_between(
     # nus, k_low, k_high,
     nus,
     y_base,
@@ -62,14 +150,18 @@ plt.fill_between(
     color="tab:orange",
     alpha=0.35,
 )
-plt.plot(nus_segments[j], xsv_segments[j], ".", lw=0.5)
-plt.axhline(xsv_segments[j].max(), alpha=0.3, color="gray")
-plt.axhline(xsv_segments[j].min(), alpha=0.3, color="gray")
+    plt.plot(nus_segments[j], xsv_segments[j], ".", lw=0.5)
+    plt.axhline(xsv_segments[j].max(), alpha=0.3, color="gray")
+    plt.axhline(xsv_segments[j].min(), alpha=0.3, color="gray")
 
-plt.ylim(xsv.min() * 0.5, xsv.max() * 2)
-plt.yscale("log")
+    plt.ylim(xsv.min() * 0.5, xsv.max() * 2)
+    plt.yscale("log")
+    plt.ylabel("$\\sigma(\\nu) (\\mathrm{cm}^{2})$")
+
+fig = plt.figure(figsize=(13, 3.5))
+plt.subplot(1, 2, 1)
+plotxsv(nus, xsv, nus_segments, xsv_segments, j, k_med, mask, y_base)
 plt.xlabel("$\\nu$ (cm-1)")
-plt.ylabel("$\\sigma(\\nu) (\\mathrm{cm}^{2})$")
 
 plt.subplot(1, 2, 2)
 plt.plot(g, k_g, label="xsv sorted")
@@ -106,3 +198,5 @@ plt.xlabel("g")
 plt.ylabel("$\\sigma(g) (\\mathrm{cm}^{2})$")
 plt.savefig("corrk_test.png", dpi=300, bbox_inches="tight")
 plt.show()
+
+
