8d7ac9854808ac7492a1ac32f9dc726018088336
Hajime Kawahara <divrot@gmail.com>
2025-06-24 21:36:12 +0900

    removed unneccesary test from transpure_ckd_test

--- tests/unittests/spec/ckd/transpure_ckd_test.py
+++ tests/unittests/spec/ckd/transpure_ckd_test.py
@@ -5,7 +5,6 @@ import numpy as np
 from exojax.test.emulate_mdb import mock_mdbExomol, mock_wavenumber_grid
 from exojax.opacity import OpaCKD, OpaPremodit
 from exojax.rt import ArtTransPure
-from exojax.test.data import get_testdata_filename, TESTDATA_CO_EXOMOL_PREMODIT_TRANSMISSION_REF
 
 config.update("jax_enable_x64", True)
 
@@ -17,7 +16,7 @@ class TestArtTransPureCKD:
         """Set up test fixtures"""
         nu_grid, _, _ = mock_wavenumber_grid()
         self.nu_grid = nu_grid
-        mdb = mock_mdbExomol("CO")
+        mdb = mock_mdbExomol("H2O")
 
         self.base_art = ArtTransPure(
             pressure_top=1.0e-8, pressure_btm=1.0e2, nlayer=50, integration="simpson"
@@ -32,37 +31,19 @@ class TestArtTransPureCKD:
         self.base_opa = OpaPremodit(mdb, nu_grid, auto_trange=[800.0, 1600.0])
         self.opa_ckd = OpaCKD(self.base_opa, Ng=16, band_width=0.5)
 
-    def test_run_ckd_vs_reference(self):
-        """test run_ckd against reference data"""
-        # Load reference data
-        fn = get_testdata_filename(TESTDATA_CO_EXOMOL_PREMODIT_TRANSMISSION_REF)
-        dat = np.loadtxt(fn, delimiter=",")
-        transit_ref = dat[:, 1]
-
-        # Run standard calculation
-        xsmatrix = self.base_opa.xsmatrix(self.Tarr, self.base_art.pressure)
-        dtau = self.base_art.opacity_profile_xs(xsmatrix, self.mmr_arr, self.base_opa.mdb.molmass, self.gravity)
-        F0 = self.base_art.run(dtau, self.Tarr, self.mean_molecular_weight, self.radius_btm, self.gravity)
-        
-        # Compare with reference
-        relative_diff = np.abs((F0 - transit_ref) / transit_ref)
-        max_relative_diff = np.max(relative_diff)
-        
-        assert max_relative_diff < 1e-4
-
     def test_run_ckd(self):
         """test run_ckd method"""
         # Standard calculation
         xsmatrix = self.base_opa.xsmatrix(self.Tarr, self.base_art.pressure)
         dtau = self.base_art.opacity_profile_xs(xsmatrix, self.mmr_arr, self.base_opa.mdb.molmass, self.gravity)
-        F0 = self.base_art.run(dtau, self.Tarr, self.mean_molecular_weight, self.radius_btm, self.gravity)
+        transit_lbl = self.base_art.run(dtau, self.Tarr, self.mean_molecular_weight, self.radius_btm, self.gravity)
         
-        # Band averages
+        # Band averages 
         transit_avg = []
         band_edges = self.opa_ckd.band_edges
         for band_idx in range(len(self.opa_ckd.nu_bands)):
             mask = (band_edges[band_idx, 0] <= self.nu_grid) * (self.nu_grid < band_edges[band_idx, 1])
-            transit_avg.append(np.mean(F0[mask]))
+            transit_avg.append(np.mean(transit_lbl[mask]))
         transit_avg = np.array(transit_avg)
 
         # CKD calculation
@@ -72,16 +53,30 @@ class TestArtTransPureCKD:
         self.opa_ckd.precompute_tables(T_grid, P_grid)
         xs_ckd = self.opa_ckd.xstensor_ckd(self.Tarr, self.base_art.pressure)
         dtau_ckd = self.base_art.opacity_profile_xs_ckd(xs_ckd, self.mmr_arr, self.base_opa.mdb.molmass, self.gravity)
-        F0_ckd = self.base_art.run_ckd(dtau_ckd, self.Tarr, self.mean_molecular_weight, self.radius_btm, self.gravity,
+        transit_ckd = self.base_art.run_ckd(dtau_ckd, self.Tarr, self.mean_molecular_weight, self.radius_btm, self.gravity,
                                        self.opa_ckd.ckd_info.weights, self.opa_ckd.nu_bands)
 
-        res = np.sqrt(np.sum((F0_ckd - transit_avg)**2)/len(F0_ckd))/np.mean(transit_avg)
-        assert res < 0.05
+        res = np.sqrt(np.sum((transit_ckd - transit_avg)**2)/len(transit_ckd))/np.mean(transit_avg)
+        assert res < 0.01
 
+        """
+        # if plotting needed, uncomment below
+        import matplotlib.pyplot as plt
+        plt.plot(self.nu_grid, transit_lbl, label="Rp2 (LBL by Premodit)", alpha=0.5)
+        plt.plot(self.opa_ckd.nu_bands, transit_ckd, label="Rp2 (CKD)")
+        plt.plot(self.opa_ckd.nu_bands, transit_avg, label="Rp2 (mean of LBL)")
+        plt.legend()
+        plt.xlabel("Wavenumber (cm^-1)")
+        plt.ylabel("squared radius (normalized)")
+        resolution = self.opa_ckd.nu_bands[0]/(band_edges[0, 1] - band_edges[0, 0])
+        print("Resolution:", resolution)
+        plt.title("CKD Spectrum Comparison (Resolution: {:.2f}, error: {:.5f})".format(resolution, res))
+        plt.savefig("ckd_test_transmission"+str(int(resolution))+".png")
+        plt.show()
+        """
 
 if __name__ == "__main__":
     tt = TestArtTransPureCKD()
     tt.setup_method()
-    tt.test_run_ckd_vs_reference()
     tt.test_run_ckd()
     print("Transmission CKD test completed successfully.")
