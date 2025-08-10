1c997278ec36efcfb88c04a1630d61ea333c26ce
Hajime Kawahara <divrot@gmail.com>
2025-08-10 14:10:35 +0900

    __init__.py clean up

--- src/exojax/atm/__init__.py
+++ src/exojax/atm/__init__.py
@@ -1,8 +1,1 @@
-__all__ = []
 
-__version__ = '1.6'
-__uri__ = ''
-__author__ = 'Hajime Kawahara and collaborators'
-__email__ = 'divrot@gmail.com'
-__license__ = ''
-__description__ = 'auto-differentiable spectral modules in exojax'
