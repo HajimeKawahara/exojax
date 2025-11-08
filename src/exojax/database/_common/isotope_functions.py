9e1f38813ca53b02f1f32c0ccec1c0b20a91f252
Hajime Kawahara <divrot@gmail.com>
2025-11-08 13:41:10 +0900

    bug fix in database._common.isotope_functions._list_isotopologues

--- src/exojax/database/_common/isotope_functions.py
+++ src/exojax/database/_common/isotope_functions.py
@@ -44,6 +44,7 @@ def _isotope_index_from_isotope_number(isotope, uniqiso):
 
 
 def _list_isotopologues(
+    simple_molecule_list: list[str],
     max_workers: Optional[int] = None
 
 ) -> dict[str, list[str]]:
