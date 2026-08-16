Add gravity regression and fix precision edge cases

src/exojax/rt/common.py
@@ -66,7 +66,8 @@ class ArtCommon:
                 mean molecular weight profile (float/Nlayer)
             radius_btm (float):
                 the bottom radius of the atmospheric layer
-            gravity_btm (float): the bottom gravity cm2/s at radius_btm, i.e. G M_p/radius_btm
+            gravity_btm (float): the bottom gravity in cm/s2 at radius_btm,
+                i.e. G M_p/radius_btm**2
 
         Returns:
             1D array: height normalized by radius_btm (Nlayer)
@@ -104,17 +105,16 @@ class ArtCommon:
                 mean molecular weight profile (float/Nlayer)
             radius_btm (float): the bottom radius of the atmospheric layer
             gravity_btm (float):
-                the bottom gravity cm2/s at radius_btm, i.e. G M_p/radius_btm
+                the bottom gravity in cm/s2 at radius_btm,
+                i.e. G M_p/radius_btm**2
 
         Returns:
             2D array:
-                gravity in cm2/s (Nlayer, 1), suitable for the input of opacity_profile_lines
+                gravity in cm/s2 (Nlayer, 1), suitable for the input of opacity_profile_lines
         """
         normalized_height, normalized_radius_lower = self.atmosphere_height(
             temperature, mean_molecular_weight, radius_btm, gravity_btm
         )
-#        normalized_radius_layer = normalized_radius_lower + 0.5 * normalized_height
-#        return jnp.array([gravity_btm / normalized_radius_layer]).T
         normalized_radius_layer = normalized_radius_lower + 0.5 * normalized_height
         return jnp.array([gravity_btm / normalized_radius_layer**2]).T
 