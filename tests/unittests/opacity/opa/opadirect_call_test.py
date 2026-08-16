a91cfa12d689e371081da79cad392afbb9e9f344
Hajime Kawahara <divrot@gmail.com>
2026-08-16 16:09:44 +0900

    Fix HITRAN all-isotope opacity calculations

--- tests/unittests/opacity/opa/opadirect_call_test.py
+++ tests/unittests/opacity/opa/opadirect_call_test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from exojax.test.emulate_mdb import mock_mdbHitemp, mock_mdbExomol
 from exojax.utils.grids import wavenumber_grid
 from exojax.opacity import OpaDirect
@@ -6,8 +8,14 @@ def test_opadirect_hitemp_call():
     nus, wav, res = wavenumber_grid(22920.0, 23100.0, 20000, unit="AA", xsmode="premodit")
     mdb = mock_mdbHitemp(multi_isotope=True)
     opa = OpaDirect(mdb, nu_grid=nus)
+    xsv = opa.xsvector(1000.0, 1.0)
+    xsm = opa.xsmatrix(np.array([1000.0, 1200.0]), np.array([1.0, 0.1]))
 
     assert isinstance(opa, OpaDirect)
+    assert xsv.shape == nus.shape
+    assert xsm.shape == (2, len(nus))
+    assert np.all(np.isfinite(xsv))
+    assert np.all(np.isfinite(xsm))
 
 def test_opadirect_exomol_call():
     nus, wav, res = wavenumber_grid(22920.0, 23100.0, 20000, unit="AA", xsmode="premodit")
--- tests/unittests/opacity/opa/opamodit_call_test.py
+++ tests/unittests/opacity/opa/opamodit_call_test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from exojax.test.emulate_mdb import mock_mdbHitemp, mock_mdbExomol
 from exojax.utils.grids import wavenumber_grid
 from exojax.opacity import OpaModit
@@ -6,8 +8,11 @@ def test_opamodit_hitemp_call():
     nus, wav, res = wavenumber_grid(22920.0, 23100.0, 20000, unit="AA", xsmode="premodit")
     mdb = mock_mdbHitemp(multi_isotope=True)
     opa = OpaModit(mdb, nu_grid=nus, allow_32bit=True)
+    xsv = opa.xsvector(1000.0, 1.0)
 
     assert isinstance(opa, OpaModit)
+    assert xsv.shape == nus.shape
+    assert np.all(np.isfinite(xsv))
 
 def test_opamodit_exomol_call():
     nus, wav, res = wavenumber_grid(22920.0, 23100.0, 20000, unit="AA", xsmode="premodit")
